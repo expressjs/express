0d81d0bc882fdeedc2373e6100862b64dd76883b
visionmedia <tj@vision-media.ca>
2009-06-26 11:59:08 -0700

    Setting up specs

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -1,4 +1,8 @@
 
 describe 'Express'
-
+  describe '.version'
+    it 'should be properly formatted'
+      Express.version.should.match /\d+\.\d+\.\d+/
+    end
+  end
 end
--- spec/spec.rhino.js
+++ spec/spec.rhino.js
@@ -1,5 +1,6 @@
 
 load('/Library/Ruby/Gems/1.8/gems/visionmedia-jspec-2.3.1/lib/jspec.js')
+load('lib/express.core.js')
 
 JSpec
 .exec('spec/spec.core.js')
