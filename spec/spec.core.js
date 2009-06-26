4a0706f6fbcb2d448bc89541d450a93267f39bae
visionmedia <tj@vision-media.ca>
2009-06-26 16:49:37 -0700

    Added argsArray()

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -6,6 +6,16 @@ describe 'Express'
     end
   end
   
+  describe '.argsArray()'
+    it 'should return an array of arguments'
+      Express.argsArray(-{ return arguments }('foo', 'bar')).should.eql ['foo', 'bar']
+    end
+    
+    it 'should allow an offset'
+      Express.argsArray(-{ return arguments }('foo', 'bar'), 1).should.eql ['bar']
+    end
+  end
+  
   describe '.escape()'
     it 'should escape html'
       Express.escape('<>&""').should.eql '&lt;&gt;&amp;&quot;&quot;'
