e4350c6a408d002bcd75f81a6921ca861222158b
visionmedia <tj@vision-media.ca>
2009-06-26 16:12:19 -0700

    Added escape()

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -6,6 +6,12 @@ describe 'Express'
     end
   end
   
+  describe '.escape()'
+    it 'should escape html'
+      Express.escape('<>&""').should.eql '&lt;&gt;&amp;&quot;&quot;'
+    end
+  end
+  
   describe '.normalizePath()'
     it 'should strip leading / trailing slashes'
       Express.normalizePath('/foo/bar/').should.eql 'foo/bar'
