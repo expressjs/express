aa01cc2bd81f8e481025a024e27387489deecddf
visionmedia <tj@vision-media.ca>
2009-06-26 12:49:59 -0700

    Added Express.status() and specs

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -18,4 +18,25 @@ describe 'Express'
       Express.header('Content-Type').should.eql 'text/html'
     end
   end
+  
+  describe '.status()'
+    after_each
+      Express.response.status = 200
+    end
+    
+    it 'should set response status code by number'
+      Express.status(404)
+      Express.response.status.should.eql 404
+    end
+    
+    it 'should set using the status string map'
+      Express.status('Not Found')
+      Express.response.status.should.eql 404
+    end
+    
+    it 'should be case insensitive'
+      Express.status('forbidden')
+      Express.response.status.should.eql 403
+    end
+  end
 end
