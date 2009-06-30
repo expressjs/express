9128dc18d5a8356c67a8b136e426811664508630
visionmedia <tj@vision-media.ca>
2009-06-30 14:19:23 -0700

    Moved specs

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -22,16 +22,6 @@ describe 'Express'
     end
   end
   
-  describe '.normalizePath()'
-    it 'should strip leading / trailing slashes'
-      Express.normalizePath('/foo/bar/').should.eql 'foo/bar'
-    end
-    
-    it 'should strip leading / trailing whitespace'
-      Express.normalizePath('  /foo/').should.eql 'foo'
-    end
-  end
-  
   describe '.contentsOf()'
     it 'should return the body of a function as a string'
       Express.contentsOf(function(){ return 'foo' }).should.include 'return', 'foo'
--- spec/spec.routing.js
+++ spec/spec.routing.js
@@ -10,6 +10,16 @@ describe 'Express'
       end
     end
     
+    describe '.normalizePath()'
+      it 'should strip leading / trailing slashes'
+        Express.normalizePath('/foo/bar/').should.eql 'foo/bar'
+      end
+
+      it 'should strip leading / trailing whitespace'
+        Express.normalizePath('  /foo/').should.eql 'foo'
+      end
+    end
+        
     describe '.routeMatches()'
       before_each
         request = { method : 'GET', uri : { path : '/user/1/edit'}}
