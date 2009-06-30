9bc091d339332a39d7a9236295ea4787b436ea66
visionmedia <tj@vision-media.ca>
2009-06-30 14:39:02 -0700

    Fixed regexp capture keys

--- spec/spec.routing.js
+++ spec/spec.routing.js
@@ -53,8 +53,10 @@ describe 'Express'
     
     describe '.pathToRegexp()'
       it 'should return a regexp with capture groups when using the :foo syntax'
-        Express.pathToRegexp('/user/:name').should.eql(/^user\/(.*?)$/)
-        Express.pathToRegexp('/user/:name/edit').should.eql(/^user\/(.*?)\/edit$/)
+        Express.pathToRegexp('/user/:name').should.eql(/^\/user\/(.*?)$/)
+        Express.regexpKeys.should.have_length 1
+        Express.pathToRegexp('/user/:name/edit').should.eql(/^\/user\/(.*?)\/edit$/)
+        Express.regexpKeys.should.have_length 1
       end
     end
   end
