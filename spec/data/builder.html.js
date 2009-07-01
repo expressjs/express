e3b7ef7e56eb80af1d578fad4c03812c58779bbd
visionmedia <tj@vision-media.ca>
2009-07-01 10:46:28 -0700

    Example data

---
+++ spec/data/builder.html.js
@@ -0,0 +1,9 @@
+
+<form id="user-login">
+  <fieldset>
+    <legend>{ userLoginTitle }</legend>
+    <input type="text" name="user[name]" value="{ user.name }" />
+    <input type="password" name="user[password]" />
+    <input type="submit" name="op" value="login" />
+  </fieldset>
+</form>
--- spec/spec.builder.js
+++ spec/spec.builder.js
@@ -2,16 +2,6 @@
 describe 'Express'
   describe '.build()'
     it 'should provide a markup DSL'
-      markup = Express.build(
-        form : { id : 'user-login'
-          fieldset : { 'class' : 'fieldset'
-            legend : { content : 'User Login' }
-            input : { type : 'text', name : 'user[name]' }
-            input : { type : 'password', name : 'user[password]' }
-            input : { type : 'submit', name : 'op', value : 'Login' }
-          }
-        }
-      })
     end
   end
 end
