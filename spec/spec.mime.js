bd5e77133787ac72c8e7545028be78095b8d0c60
visionmedia <tj@vision-media.ca>
2009-07-02 12:27:19 -0700

    Defaulting mimetype to "application/octet-stream"

--- spec/spec.mime.js
+++ spec/spec.mime.js
@@ -5,8 +5,8 @@ describe 'Express'
       Express.mime('jpeg').should.eql 'image/jpeg'
     end
     
-    it 'should return null when invalid'
-      Express.mime('foobar').should.be_null
+    it 'should return application/octet-stream when invalid'
+      Express.mime('foobar').should.eql 'application/octet-stream'
     end
     
     it 'should parse extension of basename or path passed'
