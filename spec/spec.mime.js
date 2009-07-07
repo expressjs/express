92a43c5575c40f506882c041638562cbcf9a0934
visionmedia <tj@vision-media.ca>
2009-07-07 16:28:06 -0700

    Express.Mime module

--- spec/spec.mime.js
+++ spec/spec.mime.js
@@ -1,17 +1,19 @@
 
+use(Express.Mime)
+
 describe 'Express'
   describe '.mime()'
     it 'should return media type of extensions passed'
-      Express.mime('jpeg').should.eql 'image/jpeg'
+      Express.utilities.mime('jpeg').should.eql 'image/jpeg'
     end
     
     it 'should return application/octet-stream when invalid'
-      Express.mime('foobar').should.eql 'application/octet-stream'
+      Express.utilities.mime('foobar').should.eql 'application/octet-stream'
     end
     
     it 'should parse extension of basename or path passed'
-      Express.mime('images/profile.png').should.eql 'image/png'
-      Express.mime('foo.bar.something-else.png').should.eql 'image/png'
+      Express.utilities.mime('images/profile.png').should.eql 'image/png'
+      Express.utilities.mime('foo.bar.something-else.png').should.eql 'image/png'
     end
   end
 end
