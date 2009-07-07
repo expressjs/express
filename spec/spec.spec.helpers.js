16fe60125c3fa0c8cc278e916a24f9742f12fab9
visionmedia <tj@vision-media.ca>
2009-07-07 12:21:56 -0700

    Fixed mock requests

--- spec/spec.spec.helpers.js
+++ spec/spec.spec.helpers.js
@@ -22,10 +22,38 @@ describe 'Express'
         end
       end
       
-      describe ''
-        get('users', function(){ 'User list' })
-        get('users').body.should.eql 'User list'
+      describe 'get()'
+        it 'should get route when available'
+          get('users', function(){ 'User list' })
+          get('users').body.should.eql 'User list'
+          get('other').status.should.eql 404
+        end
       end
+      
+      describe 'post()'
+        it 'should post route when available'
+          post('users', function(){ 'User list' })
+          post('users').body.should.eql 'User list'
+          post('other').status.should.eql 404          
+        end
+      end
+
+      describe 'put()'
+        it 'should put route when available'
+          put('users', function(){ 'User list' })
+          put('users').body.should.eql 'User list'
+          put('other').status.should.eql 404          
+        end
+      end
+      
+      describe 'del()'
+        it 'should delete route when available'
+          del('users', function(){ 'User list' })
+          del('users').body.should.eql 'User list'
+          del('other').status.should.eql 404          
+        end
+      end
+            
     end
   end
 end
