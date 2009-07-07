285252424ab4a2abe422bdcb28cbc86c921bd53c
visionmedia <tj@vision-media.ca>
2009-07-07 10:34:55 -0700

    Added node.js JSpec testing support

---
+++ spec/spec.node.js
@@ -0,0 +1,43 @@
+
+__loading__ = []
+__loadDelay__ = 500
+
+readFile = function(path, callback) {
+  __loading__.push(path)
+  var promise = node.fs.cat(path, "utf8")
+  promise.addErrback(function(){ throw "failed to read file `" + path + "'" })
+  promise.addCallback(function(contents){
+    setTimeout(function(){
+      if (__loading__[0] == path)
+        __loading__.shift(), callback(contents)
+      else
+        setTimeout(arguments.callee, 50)
+    }, 50)
+  })  
+}
+
+load = function(path) {
+  readFile(path, function(contents){
+    eval(contents)
+  })
+}
+
+load('/Users/tjholowaychuk/scripts/gems/JSpec/lib/jspec.js')
+load('lib/express.core.js')
+load('lib/express.mime.js')
+load('lib/express.view.js')
+
+setTimeout(function(){
+  JSpec
+  .exec('spec/spec.core.js')
+  .exec('spec/spec.routing.js')
+  .exec('spec/spec.mime.js')
+  .exec('spec/spec.view.js')
+  setTimeout(function(){ 
+    JSpec.run({ formatter : JSpec.formatters.Terminal, failuresOnly : true })
+    setTimeout(function() {
+      JSpec.report()
+    }, __loadDelay__ / 3)
+  }, __loadDelay__ / 3)
+}, __loadDelay__ / 3)
+
