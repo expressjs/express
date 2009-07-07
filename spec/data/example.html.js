1da8234bf1b535d4b7f1b1252163cab3a70c37c2
visionmedia <tj@vision-media.ca>
2009-07-07 12:44:03 -0700

    Removed unused fixture

--- spec/data/example.html.js
+++
@@ -1,9 +0,0 @@
-
-<form id="user-login">
-  <fieldset>
-    <legend>{ userLoginTitle }</legend>
-    <input type="text" name="user[name]" value="{ user.name || 'none' }" />
-    <input type="password" name="user[password]" />
-    <input type="submit" name="op" value="login" />
-  </fieldset>
-</form>
