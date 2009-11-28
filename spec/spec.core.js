f1f84295120c16ceae3c10855d340aa09ee41305
csausdev <gareth.jones@sensis.com.au>
2009-11-28 21:11:45 +1100

    all tests pass now

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -188,7 +188,8 @@ describe 'Express'
   
   describe 'BodyDecoder'
     it 'should parse urlencoded bodies'
-      request = mockRequest({ 
+      request = mockRequest({
+          method: 'POST',
           headers : [['Content-Type', 'application/x-www-form-urlencoded']],
           body : 'user[name]=foo%20bar&status=1'
         })
@@ -197,7 +198,8 @@ describe 'Express'
     end
 
     it 'should parse JSON bodies'
-      request = mockRequest({ 
+      request = mockRequest({
+          method: 'POST',
           headers : [['Content-Type', 'application/json']],
           body : '{ foo : "bar" }'
         })
@@ -215,12 +217,13 @@ describe 'Express'
   
   describe 'MethodOverride'
     it 'should override request method when _method param is present'
-      request = mockRequest({ 
+      request = mockRequest({
+          method: 'POST',
           headers : [['Content-Type', 'application/x-www-form-urlencoded']],
           body : '_method=delete'
         })
       del('foo', function(){ 'Deleted' })
-      post('foo', { request : { body : '_method=delete' }}).body.should.eql 'Deleted'
+      post('foo', { request : request}).body.should.eql 'Deleted'
     end
   end
   
