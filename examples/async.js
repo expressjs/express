5734bd5371d7e11a0cee88379427193fffcdd711
csausdev <gareth.jones@sensis.com.au>
2009-11-29 13:56:53 +0800

    added logger to async example

--- examples/async.js
+++ examples/async.js
@@ -1,6 +1,8 @@
 require.paths.unshift("./lib")
 require('express')
 
+use(Express.Logger)
+
 get('hello/:value/:time', function() {
 	setTimeout(function() {
 		Express.server.finished('<html><head><title>Asynchronous Example</title></head><body><h1>Hello '+param('value')+', I waited '+param('time')+'ms to talk to you.</h1></body></html>')
