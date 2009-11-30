f9b9c922c8b38140219180142b854b9ffab2f857
visionmedia <tj@vision-media.ca>
2009-11-29 20:05:55 -0800

    Misc refactoring to spec runner

--- spec/spec.node.js
+++ spec/spec.node.js
@@ -5,9 +5,8 @@ require("jspec")
 require("express")
 require("express.mocks")
 
-var posix = require("posix");
 readFile = function(path, callback) {
-  var promise = posix.cat(path, "utf8")
+  var promise = require('posix').cat(path, "utf8")
   promise.addErrback(function(){ throw "failed to read file `" + path + "'" })
   promise.addCallback(function(contents){
     callback(contents)
@@ -17,20 +16,19 @@ readFile = function(path, callback) {
 
 print = puts
 
-if (process.ARGV[2]) {
+if (process.ARGV[2])
   JSpec.exec('spec/spec.'+process.ARGV[2]+'.js')  
-} else {
-JSpec
-  .exec('spec/spec.core.js')
-  .exec('spec/spec.routing.js')
-  .exec('spec/spec.mocks.js')
-  .exec('spec/spec.modules.js')
-  .exec('spec/spec.mime.js')
-  .exec('spec/spec.cookie.js')
-  .exec('spec/spec.session.js')
-  .exec('spec/spec.view.js')  
-  .exec('spec/spec.async.js')
-}
-JSpec.run({ formatter : JSpec.formatters.Terminal, failuresOnly : false })
+else
+  JSpec
+    .exec('spec/spec.core.js')
+    .exec('spec/spec.routing.js')
+    .exec('spec/spec.mocks.js')
+    .exec('spec/spec.modules.js')
+    .exec('spec/spec.mime.js')
+    .exec('spec/spec.cookie.js')
+    .exec('spec/spec.session.js')
+    .exec('spec/spec.view.js')  
+    .exec('spec/spec.async.js')
+JSpec.run({ formatter: JSpec.formatters.Terminal, failuresOnly: false })
 JSpec.report()
 
