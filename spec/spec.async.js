f6238739e52257cdae39671b0f398ff6b42da7bc
visionmedia <tj@vision-media.ca>
2009-11-29 23:18:58 -0800

    Fixed whitespace from another devs commits

--- spec/spec.async.js
+++ spec/spec.async.js
@@ -5,29 +5,28 @@ describe 'Express'
       Express.routes = []
     end
 
-	describe 'callback'
-			
-		it 'should wait for an asynchronous handler to finish'
-			require('jspec.timers')
-			get('pants', function() {
-				setTimeout(function() {
-					Express.server.finished('asynchronous thing done')
-				}, 50)
-			})
-			var response = get('pants')
-			response.body.should.be_null
-
-			tick(50)
-			response.body.should.eql 'asynchronous thing done'
-			response.status.should.eql 200
-			response.headers['content-type'].should.eql 'text/html'
-		end
-			
-		it 'should still accept a synchronous handler that returns a string'
-			get('pants', function() { 'no pants' })
-			get('pants').body.should.eql 'no pants'
-		end
-	end
+    describe 'callback'
+      it 'should wait for an asynchronous handler to finish'
+        require('jspec.timers')
+        get('pants', function() {
+          setTimeout(function() {
+            Express.server.finished('asynchronous thing done')
+          }, 50)
+        })
+        var response = get('pants')
+        response.body.should.be_null
+    
+        tick(50)
+        response.body.should.eql 'asynchronous thing done'
+        response.status.should.eql 200
+        response.headers['content-type'].should.eql 'text/html'
+      end
+        
+      it 'should still accept a synchronous handler that returns a string'
+        get('pants', function() { 'no pants' })
+        get('pants').body.should.eql 'no pants'
+      end
+    end
   end
 end
 
