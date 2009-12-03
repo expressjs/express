3901c33ce9e1e06f8ebe48dc009934060d10ee0d
visionmedia <tj@vision-media.ca>
2009-12-03 14:27:38 -0800

    Added specs for configure()

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -27,4 +27,65 @@ describe 'Express'
       set('sessions').should.be_false
     end
   end
+  
+  describe 'configure()'
+    describe 'given a function'
+      it 'should be called for any environment'
+        var called = false
+        configure(function(){
+          called = true
+        })
+        configure('development')
+        called.should.be_true
+      end
+    end
+    
+    describe 'given several calls functions'
+      it 'should call them in sequence'
+        var order = []
+        configure(function(){
+          called.push('a')
+        })
+        configure(function(){
+          called.push('b')
+        })
+        configure('development')
+        order.should.eql ['a', 'b']
+      end
+      
+      it 'should call the specific environment only'
+        var order = []
+        configure('test', function(){
+          order.push('a')
+        })
+        configure('development', function(){
+          order.push('b')
+        })
+        configure('test', function(){
+          order.push('c')
+        })
+        configure('test')
+        order.should.eql ['a', 'c']
+      end
+      
+      it 'should call global configurations as well'
+        var order = []
+        configure(function(){
+          order.push('a')
+        })
+        configure('development', function(){
+          order.push('b')
+        })
+        configure('test', function(){
+          order.push('c')
+        })
+        configure(function(){
+          order.push('d')
+        })
+        configure('test')
+        order.should.eql ['a', 'c', 'd']
+      end
+    end
+
+  end
 end
