e2f6cf9d19db74eb104440b26902cc02c63eebb1
visionmedia <tj@vision-media.ca>
2009-12-02 20:26:20 -0800

    GLOBAL[method] -> get

--- spec/spec.core.routing.js
+++ spec/spec.core.routing.js
@@ -1,36 +1,35 @@
 
 describe 'Express'
+  before_each
+    reset()
+  end
+  
   describe 'route'
-    before
-      reset()
-      method = 'get'
-    end
-    
     describe 'with callback function'
       it 'should respond with a body string'
-        GLOBAL[method]('/user', function(){
+        get('/user', function(){
           return 'test'
         })
-        GLOBAL[method]('/user').body.should.eql 'test'
+        get('/user').body.should.eql 'test'
       end
     end
     
     describe 'with options and callback function'
       it 'should respond with a body string'
-        GLOBAL[method]('/user', {}, function(){
+        get('/user', {}, function(){
           return 'test with options'
         })
-        GLOBAL[method]('/user').body.should.eql 'test with options'
+        get('/user').body.should.eql 'test with options'
       end
     end
     
     describe 'with an exception thrown'
       it 'should provide display route method and path in the stacktrace'
-        GLOBAL[method]('/user', {}, function(){
+        get('/user', {}, function(){
           throw new Error('access denied')
         })
-        -{ GLOBAL[method]('/user') }.should.throw_error
-        try { GLOBAL[method]('/user') }
+        -{ get('/user') }.should.throw_error
+        try { get('/user') }
         catch (e){ 
           e.stack.should.include('get')
           e.stack.should.include('"/user"')
