2025ce5a6b68cc84be282f5deed7b6279344d3d9
visionmedia <tj@vision-media.ca>
2009-12-02 20:19:52 -0800

    Added set(), enable(), and disable()

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -5,4 +5,26 @@ describe 'Express'
       Express.version.should.match(/^\d+\.\d+\.\d+$/)
     end
   end
+  
+  describe 'set()'
+    it 'should set an option'
+      set('raise exceptions').should.be_null
+      set('raise exceptions', true)
+      set('raise exceptions').should.be_true
+    end
+  end
+  
+  describe 'enable()'
+    it 'should enable an option'
+      enable('sessions')
+      set('sessions').should.be_true
+    end
+  end
+  
+  describe 'disable()'
+    it 'should disable an option'
+      disable('sessions')
+      set('sessions').should.be_false
+    end
+  end
 end
