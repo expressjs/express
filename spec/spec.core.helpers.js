329f3855d11c67fc839ebe88569b48e381d0783a
visionmedia <tj@vision-media.ca>
2009-12-03 13:28:58 -0800

    Added parseParams() and specs

--- spec/spec.core.helpers.js
+++ spec/spec.core.helpers.js
@@ -4,6 +4,27 @@ describe 'Express'
     reset()
   end
   
+  describe 'parseParams()'
+    it 'should parse simple query string key / value pairs'
+      parseParams('foo=bar').should.eql { foo: 'bar' }
+      parseParams('foo=bar&baz=1').should.eql { foo: 'bar', baz: '1' }
+    end
+    
+    it 'should parse named nested params'
+      var user = { user: { name: 'tj', email: 'tj@vision-media.ca' }}
+      parseParams('user[name]=tj&user[email]=tj@vision-media.ca').should.eql user
+    end
+    
+    it 'should parse several levels of nesting'
+      var user = { user: { name: 'tj', email: { primary: 'tj@vision-media.ca' }}}
+      parseParams('user[name]=tj&user[email][primary]=tj@vision-media.ca').should.eql user
+    end
+    
+    it 'should parse anonymous pairs'
+      parseParams('users[]=tj&users[]=joe').should.eql { users: ['tj', 'joe'] }
+    end
+  end
+  
   describe 'toArray()'
     describe 'when given an array'
       it 'should return the array'
