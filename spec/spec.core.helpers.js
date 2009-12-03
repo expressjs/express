d0d15f8a467569d9f3e1c7c19b2f43c300dabf45
visionmedia <tj@vision-media.ca>
2009-12-03 11:51:42 -0800

    Started implementing halt()

--- spec/spec.core.helpers.js
+++ spec/spec.core.helpers.js
@@ -48,6 +48,7 @@ describe 'Express'
     describe 'when given no arguments'
       it 'should respond with 404 Not Found'
         get('/user', function(){ halt() })
+        p(get('/user').body)
         get('/user').body.should.include('Not Found')
         get('/user').status.should.eql 404
       end
