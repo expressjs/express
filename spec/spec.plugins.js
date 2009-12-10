31ac31d549dfb78e1335254f4ecab6d518424885
visionmedia <tj@vision-media.ca>
2009-12-10 14:12:01 -0800

    use() now accepts options [#45]

--- spec/spec.plugins.js
+++ spec/spec.plugins.js
@@ -15,6 +15,13 @@ describe 'Express'
       use(CSSColors)
     end
     
+    describe 'with options provided'
+      it 'should merge properties'
+        use(CSSColors, { foo: 'bar' })
+        $(Express.plugins).last().should.have_property 'foo', 'bar'
+      end
+    end
+    
     describe 'Event'
       describe '#init()'
         it 'should accept arbitrary data'
