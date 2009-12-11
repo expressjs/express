8026266ea45fe520dbf4ced31e1c5194c6a8e7fe
visionmedia <tj@vision-media.ca>
2009-12-11 08:49:24 -0800

    Spec for set("views") default

--- spec/spec.view.js
+++ spec/spec.view.js
@@ -1,5 +1,16 @@
 
 describe 'Express'
+  before_each
+    reset()
+  end
+  
+  describe 'set("views")'
+    it 'should default to <root>/views'
+      set('root', 'spec')
+      set('views').should.eql 'spec/views'
+    end
+  end
+
   describe 'render()'
     
   end
