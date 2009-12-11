0160213bddfe0c14cf2c086add3bf76404b6fa24
visionmedia <tj@vision-media.ca>
2009-12-11 12:00:45 -0800

    Started ElementCollection

--- spec/spec.collection.libxml.js
+++ spec/spec.collection.libxml.js
@@ -1,13 +1,14 @@
 
 process.mixin(require('express/collection'))
-require('support/libxmljs')
+process.mixin(require('express/element-collection'))
 
 describe 'Express'
-  describe 'Collection'
+  describe 'ElementCollection'
   
-    describe '$(array)'
-      it 'should return a Collection'
-        $(['foo', 'bar']).should.be_an_instance_of Collection
+    describe '$("markup string")'
+      it 'should return a ElementCollection'
+        $('<p>foo</p>').should.be_an_instance_of Collection
+        $('<p>foo</p>').should.be_an_instance_of ElementCollection
       end
     end
     
