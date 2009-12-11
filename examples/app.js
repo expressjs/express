521fb0bf41d13ce64aeee2088a2d2e70edc3ef79
visionmedia <tj@vision-media.ca>
2009-12-11 08:07:46 -0800

    Pre-loading view engine modules to prevent overhead of module loading

--- examples/app.js
+++ examples/app.js
@@ -3,7 +3,7 @@ require.paths.unshift("./lib")
 require('express')
 require('express/plugins')
 
-use(Profiler)
+//use(Profiler)
 use(MethodOverride)
 use(ContentLength)
 set('views', dirname(__filename) + '/views')
