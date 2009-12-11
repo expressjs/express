05e9cc67929c7b14ceb65a26925f3732df14e437
visionmedia <tj@vision-media.ca>
2009-12-11 09:28:24 -0800

    Cleaning up example app

--- examples/app.js
+++ examples/app.js
@@ -1,17 +1,20 @@
 
-require.paths.unshift("./lib")
+require.paths.unshift('lib')
 require('express')
 require('express/plugins')
 
-use(Profiler)
-use(MethodOverride)
-use(ContentLength)
-
 configure(function(){
+  use(Profiler)
+  use(MethodOverride)
+  use(ContentLength)
   set('root', dirname(__filename))
   enable('cache views')
 })
 
+get('/hello', function(){
+  contentType('html')
+  return '<h1>World<h1>'
+})
 
 get('/user/:id?', function() {
   render('user.haml.html', {
