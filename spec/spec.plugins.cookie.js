6a1d6b7c42424a9fa1e7d54d3ace7da4a8bab06d
visionmedia <tj@vision-media.ca>
2009-12-14 18:07:40 -0800

    Added compileCookie()

--- spec/spec.plugins.cookie.js
+++ spec/spec.plugins.cookie.js
@@ -14,6 +14,14 @@ describe 'Express'
         }
         compileCookie(data).should.eql 'path=/; domain=.vision-media.ca'
       end
+      
+      it 'should currectly format any Date objects'
+        var data = {
+          expires: new Date
+        }
+        print(new Date('October 13, 1975 11:13:00'))
+        compileCookie(data).should.eql 'expires='
+      end
     end
   
     describe 'parseCookie()'
