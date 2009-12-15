45753ab1ab191804da0dd35ef167dbf36625f992
visionmedia <tj@vision-media.ca>
2009-12-14 17:22:22 -0800

    Docs for Cookie.parse()

--- spec/spec.plugins.cookie.js
+++ spec/spec.plugins.cookie.js
@@ -37,17 +37,6 @@ describe 'Express'
         }
         parse(attrs).should.eql expected
       end
-      
-      it 'should allow quoted values'
-        var attrs = 'expires=Fri, 31-Dec-2010 23:59:59 GMT; data="some ;.= random data"; path=/; domain=.example.net'
-        var expected = {
-          expires: 'Fri, 31-Dec-2010 23:59:59 GMT',
-          data: 'some ;.= random data',
-          path: '/',
-          domain: '.example.net'
-        }
-        parse(attrs).should.eql expected
-      end
     end
   
     describe 'on'
