45cd102794328e077f64ad266454433fd7cb9b47
visionmedia <tj@vision-media.ca>
2009-12-15 15:28:42 -0800

    Moving CommonLogger in sample app lower to catch Content-Length

--- examples/app.js
+++ examples/app.js
@@ -4,9 +4,9 @@ require('express')
 require('express/plugins')
 
 configure(function(){
-  use(CommonLogger)
   use(MethodOverride)
   use(ContentLength)
+  use(CommonLogger)
   set('root', dirname(__filename))
   enable('helpful 404')
   enable('show exceptions')
