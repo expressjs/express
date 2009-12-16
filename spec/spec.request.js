5a85db18a13b7305909928f4f1818569a874a16e
visionmedia <tj@vision-media.ca>
2009-12-16 13:42:23 -0800

    Added specs for requests with content-type of "application/x-www-form-urlencoded"

--- spec/spec.request.js
+++ spec/spec.request.js
@@ -3,6 +3,30 @@ describe 'Express'
   before_each
     reset()
   end
+  
+  describe '#init()'
+    describe 'when content-type is "application/x-www-form-urlencoded"'
+      it 'should parse body as urlencoded params'
+        post('/login', function(){
+          return this.param('name')
+        })
+        var response = post('/login', { body: 'name=tj', headers: { 'content-type': 'application/x-www-form-urlencoded' }})
+        response.status.should.eql 200
+        response.body.should.eql 'tj'
+      end
+      
+      describe 'with charset'
+        it 'should parse body as urlencoded params'
+          post('/login', function(){
+            return this.param('name')
+          })
+          var response = post('/login', { body: 'name=tj', headers: { 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' }})
+          response.status.should.eql 200
+          response.body.should.eql 'tj'
+        end
+      end
+    end
+  end
 
   describe 'status()'
     it 'should set the response status'
