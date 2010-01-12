6b6ec3f19fcb35993a8101507bf2e1a59dba7724
visionmedia <tj@vision-media.ca>
2010-01-11 16:35:22 -0800

    Added link support to sample chat app

--- examples/app.js
+++ examples/app.js
@@ -26,7 +26,10 @@ get('/chat', function(){
 })
 
 post('/chat', function(){
-  messages.push(escape(this.param('message')).replace(/:\)/g, '<img src="http://icons3.iconfinder.netdna-cdn.com/data/icons/ledicons/emoticon_smile.png">'))
+  messages
+    .push(escape(this.param('message'))
+    .replace(/(http:\/\/[^\s]+)/g, '<a href="$1" target="express-chat">$1</a>')
+    .replace(/:\)/g, '<img src="http://icons3.iconfinder.netdna-cdn.com/data/icons/ledicons/emoticon_smile.png">'))
   this.halt(200)
 })
 
