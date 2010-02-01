6b9a079bbf0f4c4e0b497594e421921ff1e36fd6
visionmedia <tj@vision-media.ca>
2010-02-01 15:49:51 -0800

    Added spec for mock sessions when testing

--- spec/spec.plugins.session.js
+++ spec/spec.plugins.session.js
@@ -7,8 +7,16 @@ describe 'Express'
   end
   
   describe 'Session'
-    it 'should description'
-      
+    it 'should persist within specs'
+      post('/login', function(){
+        this.session.name = 'tj'
+        return ''
+      })
+      get('/login', function(){
+        return this.session.name
+      })
+      get('/login').status.should.eql 200
+      get('/login').body.should.eql 'tj'
     end
   end
 end
