8ed99f6bb81bafedc4ae7871a565ec15646b6195
visionmedia <tj@vision-media.ca>
2010-02-01 17:29:23 -0800

    Added session reaping

--- spec/spec.plugins.session.js
+++ spec/spec.plugins.session.js
@@ -21,7 +21,11 @@ describe 'Express'
       end
     end
   
-    describe 'with MemoryStore'
+    describe 'MemoryStore'
+      before_each
+        memory = new (require('express/plugins/session').MemoryStore)
+      end
+      
       it 'should persist'
         post('/login', function(){
           return this.session.name = 'tj'
@@ -34,6 +38,56 @@ describe 'Express'
         get('/login', headers).status.should.eql 200
         get('/login', headers).body.should.eql 'tj'
       end
+      
+      describe '#fetch()'
+        describe 'when the session does not exist'
+          it 'should return a new Session'
+            memory.fetch('1').should.have_property 'lastAccess'
+          end
+        end
+        
+        describe 'when the session does exist'
+          it 'should return the previous session'
+            memory.commit({ id: '1', same: true })
+            memory.fetch('1').should.have_property 'same', true
+          end
+        end
+      end
+      
+      describe '#clear()'
+        it 'should remove all sessions'
+          memory.commit({ id: '1' })
+          memory.commit({ id: '2' })
+          memory.clear()
+          memory.should.not.have_property '1'
+          memory.should.not.have_property '2'
+        end
+      end
+      
+      describe '#destroy()'
+        it 'should destroy a single session'
+          memory.commit({ id: '1' })
+          memory.commit({ id: '2' })
+          memory.destroy('1')
+          memory.store.should.not.have_property '1'
+          memory.store.should.have_property '2'
+        end
+      end
+      
+      describe '#reap()'
+        it 'should destroy sessions older than the given age in milliseconds'
+          memory.commit({ id: '1', lastAccess: Number(new Date) - 300 })
+          memory.commit({ id: '2', lastAccess: Number(new Date) - 250 })
+          memory.commit({ id: '3', lastAccess: Number(new Date) - 100 })
+          memory.commit({ id: '4', lastAccess: Number(new Date) })
+          memory.reap(200)
+          memory.store.should.not.have_property '1'
+          memory.store.should.not.have_property '2'
+          memory.store.should.have_property '3'
+          memory.store.should.have_property '4'
+        end
+      end
     end
+    
   end
 end
