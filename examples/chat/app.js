20cf8f81a2948ec928e8368e5acf96dfb4deea61
visionmedia <tj@vision-media.ca>
2010-02-03 14:18:40 -0800

    Session options in chat sample app

--- examples/chat/app.js
+++ examples/chat/app.js
@@ -4,11 +4,13 @@ require('express')
 require('express/plugins')
 
 configure(function(){
+  var fiveMinutes = 300000,
+      oneMinute = 60000
   use(MethodOverride)
   use(ContentLength)
   use(CommonLogger)
   use(Cookie)
-  use(Session)
+  use(Session, { lifetime: fiveMinutes, reapInterval: oneMinute })
   set('root', dirname(__filename))
   enable('cache static files')
   //enable('cache view contents')
