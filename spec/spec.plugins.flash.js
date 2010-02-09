9727fac2917df38a542a67b6a7f8cd70385f1af3
visionmedia <tj@vision-media.ca>
2010-02-09 08:41:28 -0800

    Added flash support. Closes #64

--- spec/spec.plugins.flash.js
+++ spec/spec.plugins.flash.js
@@ -9,6 +9,24 @@ describe 'Express'
   end
   
   describe 'Flash'
-    
+    describe 'flash()'
+      it 'should push a flash message'
+        var headers = { headers: { cookie: 'sid=123' }}
+        post('/', function(){ return this.flash('info', 'email sent') })
+        get('/', function(){ return this.flash('info', 'email received') })
+        get('/info', function(){ return this.flash('info').join(', ') || 'empty' })
+        get('/messages', function(){ return this.flash('info').join(', ') || 'empty' })
+        
+        post('/', headers).body.should.eql 'email sent'
+        get('/', headers).body.should.eql 'email received'
+        get('/info', headers).body.should.eql 'email sent, email received'
+        // TODO: seperate once segfault is fixed...
+      end
+      
+      it 'should return the message pushed'
+        get('/', function(){ return this.flash('info', 'email sent') })
+        get('/').body.should.eql 'email sent'
+      end
+    end
   end
 end
