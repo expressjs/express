822de581b30adcfd627b2442319816ddd105a9ce
visionmedia <tj@vision-media.ca>
2010-02-09 16:34:19 -0800

    flash() returns null when no flashes are available. Closes #198

--- spec/spec.plugins.flash.js
+++ spec/spec.plugins.flash.js
@@ -14,12 +14,13 @@ describe 'Express'
         var headers = { headers: { cookie: 'sid=123' }}
         post('/', function(){ return this.flash('info', 'email sent') })
         get('/', function(){ return this.flash('info', 'email received') })
-        get('/info', function(){ return this.flash('info').join(', ') || 'empty' })
-        get('/messages', function(){ return this.flash('info').join(', ') || 'empty' })
+        get('/info', function(){ return this.flash('info').join(', ') })
+        get('/messages', function(){ return this.flash('info') || 'empty' })
         
         post('/', headers).body.should.eql 'email sent'
         get('/', headers).body.should.eql 'email received'
         get('/info', headers).body.should.eql 'email sent, email received'
+        get('/messages').body.should.eql 'empty'
         // TODO: seperate once segfault is fixed...
       end
       
