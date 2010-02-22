f03e460ca567e0f611643e255f365f2f3f90aaea
visionmedia <tj@vision-media.ca>
2010-02-22 08:05:13 -0800

    Started converting fs module

--- spec/node.js
+++ spec/node.js
@@ -4,18 +4,9 @@ require("jspec")
 require("express")
 require("express/spec")
 
-quit = process.exit
 print = puts
-
-readFile = function(path) {
-  var result
-  require('posix')
-    .cat(path, "utf8")
-    .addCallback(function(contents){ result = contents })
-    .addErrback(function(){ throw new Error("failed to read file `" + path + "'") })
-    .wait()
-  return result
-}
+quit = process.exit
+readFile = require('fs').readFileSync
 
 function run(specs) {
   specs.forEach(function(spec){
