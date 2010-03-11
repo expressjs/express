565f68d2d7592dfbb8d40112b6aceb321b07e109
visionmedia <tj@vision-media.ca>
2010-03-11 11:00:47 -0800

    Added kiwi.seed() calls to make them available via require()
    This is needed since the view plugin uses
    the regular require() and not kiwi.require()

--- examples/chat/app.js
+++ examples/chat/app.js
@@ -3,9 +3,15 @@ require.paths.unshift('lib')
 require('express')
 require('express/plugins')
 
+var messages = [],
+    utils = require('express/utils'),
+    kiwi = require('kiwi')
+
 configure(function(){
   var fiveMinutes = 300000,
       oneMinute = 60000
+  kiwi.seed('haml')
+  kiwi.seed('sass')
   use(MethodOverride)
   use(ContentLength)
   use(CommonLogger)
@@ -15,9 +21,6 @@ configure(function(){
   set('root', __dirname)
 })
 
-var messages = [],
-    utils = require('express/utils')
-    
 get('/', function(){
   this.redirect('/chat')
 })
