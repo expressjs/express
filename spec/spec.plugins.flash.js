50e0593de6427e09011a9467b16a5951c589cad9
visionmedia <tj@vision-media.ca>
2010-03-16 19:34:11 -0700

    Request#flash() without args now returns all flashes

--- spec/spec.plugins.flash.js
+++ spec/spec.plugins.flash.js
@@ -41,6 +41,35 @@ describe 'Express'
             get('/', headers).body.should.eql 'nope!'
           end
         end
+        
+        describe 'given no arguments'
+          it 'should provide access to all types'
+            var flash, headers = { headers: { cookie: 'sid=123' }}
+            post('/', function(){ return this.flash('info', 'email sent') })
+            post('/error', function(){ return this.flash('error', 'email failed to send') })
+            get('/', function(){ return flash = this.flash(), '' })
+            post('/', headers)
+            post('/', headers)
+            post('/error', headers)
+            get('/')
+            flash.should.eql { info: ['email sent', 'email sent'], error: ['email failed to send'] }
+          end
+          
+          it 'should persist only until flushed'
+            var flash, headers = { headers: { cookie: 'sid=123' }}
+            post('/', function(){ return this.flash('info', 'email sent') })
+            post('/error', function(){ return this.flash('error', 'email failed to send') })
+            get('/', function(){ return flash = this.flash(), '' })
+            post('/', headers)
+            post('/', headers)
+            post('/error', headers)
+            get('/', headers)
+            flash.should.eql { info: ['email sent', 'email sent'], error: ['email failed to send'] }
+            get('/', headers)
+            flash.should.eql {}
+          end
+        end
+        
       end
     end
   end
