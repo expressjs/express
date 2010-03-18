31e608e0ae6ad3bc7e2bbd4beb3b7109f34e98d9
visionmedia <tj@vision-media.ca>
2010-03-18 10:58:50 -0700

    Added Request#sendfile() specs

--- spec/spec.static.js
+++ spec/spec.static.js
@@ -5,7 +5,7 @@ describe 'Express'
       StaticFile = require('express/static').File
     end
     
-    describe '#init'
+    describe '#constructor'
       it 'should accept and assign #path'
         (new StaticFile('/foo/bar')).path.should.eql '/foo/bar'
       end
@@ -19,5 +19,50 @@ describe 'Express'
         }
       end
     end
+    
+    describe '#sendfile()'
+      describe 'when the file exists'
+        it 'should transfer the file'
+          get('/public/*', function(file){
+            this.sendfile('spec/fixtures/' + file)
+          })
+          get('/public/user.json').body.should.include '"name":'
+        end
+        
+        it 'should automatically set the content type based on extension'
+          get('/public/*', function(file){
+            this.sendfile('spec/fixtures/' + file)
+          })
+          get('/public/user.json').headers['content-type'].should.eql 'application/json'
+        end
+      end
+      
+      describe 'when "cache static files" is enabled'
+        it 'should transfer the file'
+          enable('cache static files')
+          get('/public/*', function(file){
+            this.sendfile('spec/fixtures/' + file)
+          })
+          get('/cached', function(){
+            return this.cache.get('spec/fixtures/user.json') ? 'yes' : 'no'
+          })
+          get('/cached').body.should.eql 'no'
+          get('/public/user.json').body.should.include '"name":'
+          get('/cached').body.should.eql 'yes'
+          get('/cached').body.should.eql 'yes'
+        end
+        
+        it 'should automatically set the content type based on extension'
+          enable('cache static files')
+          get('/public/*', function(file){
+            this.sendfile('spec/fixtures/' + file)
+          })
+          get('/public/user.json').headers['content-type'].should.eql 'application/json'
+          get('/public/user.json').headers['content-type'].should.eql 'application/json'
+          get('/public/user.json').headers['content-type'].should.eql 'application/json'
+        end
+      end
+    end
+    
   end
 end
