206e800963477fd657022652b91693a3c692562c
visionmedia <tj@vision-media.ca>
2010-03-22 15:34:46 -0700

    Added assertion to ensure that partials dir is relative to set("views")

--- spec/spec.plugins.view.js
+++ spec/spec.plugins.view.js
@@ -13,9 +13,11 @@ describe 'Express'
     end
     
     describe 'set("partials")'
-      it 'should default to <root>/views/partials'
+      it 'should default to <views>/partials'
         set('root', 'spec')
         set('partials').should.eql 'spec/views/partials'
+        set('views', 'magicland')
+        set('partials').should.eql 'magicland/partials'
       end
     end
     
