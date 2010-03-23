9f23c7b31ab6b26f0b69bff9cbaf584731e08820
visionmedia <tj@vision-media.ca>
2010-03-23 10:31:46 -0700

    Added specs for param()

--- spec/spec.routing.js
+++ spec/spec.routing.js
@@ -4,6 +4,46 @@ describe 'Express'
     reset()
   end
   
+  describe 'param()'
+    describe 'given no args'
+      it 'should throw an error'
+        -{ param() }.should.throw_error TypeError
+      end
+    end
+    
+    describe 'given a non-string as the first argument'
+      it 'should throw an error'
+        -{ param(12) }.should.throw_error TypeError
+      end
+    end
+    
+    describe 'given no callback function'
+      it 'should throw an error'
+        -{ param('foo') }.should.throw_error TypeError
+      end
+    end
+    
+    describe 'given a non-function as the second argument'
+      it 'should throw an error'
+        -{ param('foo', 2) }.should.throw_error TypeError
+      end
+    end
+    
+    describe 'with key / callback function'
+      it 'should pre-process a value before it is passed to a route'
+        param('user_id', function(val){
+          val = parseInt(val)
+          return isNaN(val) ? false : val
+        })
+        get('/user/:user_id', function(id){
+          require('sys').p(id)
+          return String(typeof id === 'number')
+        })
+        get('/user/12').body.should.eql 'true'
+      end
+    end
+  end
+  
   describe 'route'
     describe 'with callback function'
       it 'should respond with a body string'
