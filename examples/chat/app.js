906ef02c5ec27686737e488bf84bc33499900ee8
visionmedia <tj@vision-media.ca>
2010-03-24 14:45:46 -0700

    request response event is now fired in reverse.
    This does a few things:
      a. Allows plugins such as Logger to properly "wrap" all
         other plugins when it is the first passed to use().
    
      b. Lessons the chance of a plugin failure effecting another plugin

--- examples/chat/app.js
+++ examples/chat/app.js
@@ -7,13 +7,13 @@ var messages = [],
     utils = require('express/utils')
     
 configure(function(){
+  use(Logger)
   use(MethodOverride)
   use(ContentLength)
   use(Cookie)
   use(Cache, { lifetime: (5).minutes, reapInterval: (1).minute })
   use(Session, { lifetime: (15).minutes, reapInterval: (1).minute })
   use(Static)
-  use(Logger)
   set('root', __dirname)
 })
 
