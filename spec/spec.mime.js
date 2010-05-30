8b1fcd4dd7ced05d047a98c383a79b842093d502
visionmedia <tj@vision-media.ca>
2010-05-30 13:14:08 -0700

    mime.type() no longer accepts ".type"

--- spec/spec.mime.js
+++ spec/spec.mime.js
@@ -10,13 +10,6 @@ describe 'Express'
   
   describe 'mime'
     describe 'type()'
-      describe 'when given an extension with leading dot'
-        it 'should return the associated mime type'
-          mime.type('.png').should.eql 'image/png'
-          mime.type('.foo.png').should.eql 'image/png'
-        end
-      end
-      
       describe 'when given an extension without leading dot'
         it 'should return the associated mime type'
           mime.type('png').should.eql 'image/png'
