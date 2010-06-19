64b24a96c849691c9d5336f94a3795a33dddab8f
Tj Holowaychuk <tj@vision-media.ca>
2010-06-18 17:30:20 -0700

    Fixing chat example

--- examples/chat/app.js
+++ examples/chat/app.js
@@ -1,80 +1,56 @@
 
-require.paths.unshift('lib')
-require('express')
-require('express/plugins')
+/**
+ * Module dependencies.
+ */
 
-var messages = [],
-    utils = require('express/utils'),
-    http = require('express/http')
-    
-configure(function(){
-  use(Logger)
-  use(MethodOverride)
-  use(ContentLength)
-  use(Cookie)
-  use(Cache, { lifetime: (5).minutes, reapInterval: (1).minute })
-  use(Session, { lifetime: (15).minutes, reapInterval: (1).minute })
-  use(Static)
-  set('root', __dirname)
-})
+var app = module.exports = require('express').createApplication([
+    { filter: 'log' },
+    { filter: 'method-override' },
+    { filter: 'cookie' },
+    { filter: 'session' },
+    { filter: 'body-decoder' },
+    { provider: 'sass', root: __dirname + '/public' },
+    { provider: 'static', root: __dirname + '/public' },
+]);
 
-get('/', function(){ 
-  this.pass('/chat')
-})
+/**
+ * Chat messages.
+ */
 
-get('/chat', function(){
-  var self = this
-  Session.store.length(function(err, len){
-    self.render('chat.html.haml', {
-      locals: {
-        title: 'Chat',
-        messages: messages,
-        name: self.session.name,
-        usersOnline: len
-      }
-    })
-  })
-})
+var messages = [];
 
-post('/chat', function(){
-  this.session.name = this.param('name')
-  messages
-    .push(utils.escape(this.param('name')) + ': ' + utils.escape(this.param('message'))
-    .replace(/(http:\/\/[^\s]+)/g, '<a href="$1" target="express-chat">$1</a>')
-    .replace(/:\)/g, '<img src="http://icons3.iconfinder.netdna-cdn.com/data/icons/ledicons/emoticon_smile.png">'))
-  this.respond(200)
-})
+// Routes
 
-get('/chat/messages', function(){
-  var self = this,
-      previousLength = messages.length,
-      timer = setInterval(function(){
-    if (messages.length > previousLength)
-      self.contentType('json'),
-      previousLength = messages.length,
-      self.respond(200, JSON.encode(messages)),
-      clearInterval(timer)
-  }, 100)
-})
+app.get('/', function(req, res){
+    req.sessionStore.length(function(err, len){
+        res.render('chat.html.haml', {
+            locals: {
+                title: 'Chat',
+                messages: messages,
+                name: self.session.name,
+                usersOnline: len
+            }
+        })
+    });
+});
 
-get('/*.css', function(file){
-  this.render(file + '.css.sass', { layout: false })
-})
+app.post('/chat', function(req, res){
+    var name = req.session.name = req.body.name;
+    messages.push(name + ': ' + req.body.message);
+    res.writeHead(204, {});
+    res.end();
+});
 
-get('/error/view', function(){
-  this.render('does.not.exist')
-})
-
-get('/error', function(){
-  throw new Error('oh noes!')
-})
-
-get('/simple', function(){
-  return 'Hello :)'
-})
-
-get('/favicon.ico', function(){
-  this.notFound()
-})
-
-run()
+app.get('/chat/messages', function(req, res){
+    var previousLength = messages.length;
+    var id = setInterval(function(){
+        if (messages.length > previousLength) {
+            previousLength = messages.length;
+            res.writeHead(200, {
+               'Content-Type': 'application/json' 
+            });
+            res.end(JSON.encode(messages));
+            clearInterval(id);
+        }
+    }, 100);
+});
