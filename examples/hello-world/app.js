a62a5d0d7b2e023c8609938752dc44741ae1dcd6
Tj Holowaychuk <tj@vision-media.ca>
2010-06-18 17:16:17 -0700

    Removed old cruft
    wow express is light now! haha.. go connect go

--- examples/hello-world/app.js
+++ examples/hello-world/app.js
@@ -5,7 +5,7 @@
 
 var app = module.exports = require('express').createApplication();
 
-app.get('/', function(req, res){
+app.get('/', function(req, res, params){
     res.writeHead(200, {});
     res.end('Hello World');
 });
