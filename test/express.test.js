5f9e4ace2fd3502879acbd5dcc24291a9aff275d
Tj Holowaychuk <tj@vision-media.ca>
2010-07-05 15:02:25 -0700

    Added basic app test

--- test/express.test.js
+++ test/express.test.js
@@ -1,8 +1,21 @@
 
-var express = require('express');
+var express = require('express'),
+    connect = require('connect');
 
 module.exports = {
     'test .version': function(assert){
         assert.ok(/^\d+\.\d+\.\d+$/.test(express.version), 'Test express.version format');
+    },
+    
+    'test basic app': function(){
+        var app = express.createApplication();
+        var server = connect.createServer(app);
+        app.get('/', function(req, res){
+            res.writeHead(200, {});
+            res.end('wahoo');
+        });
+        assert.response(server,
+            { url: '/' },
+            { body: 'wahoo' });
     }
 };
