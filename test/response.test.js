874975da5451ddd29fe309b1a13cfb32891b73cb
Tj Holowaychuk <tj@vision-media.ca>
2010-07-05 21:37:56 -0700

    Added response helpers

---
+++ test/response.test.js
@@ -0,0 +1,27 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+module.exports = {
+    '#send()': function(assert){
+        var app = express.createServer();
+
+        app.get('/html', function(req, res){
+            res.send('<p>test</p>', { 'Content-Language': 'en' });
+        });
+        
+        app.get('/json', function(req, res){
+            res.send({ foo: 'bar' }, {}, 201);
+        });
+
+        assert.response(app,
+            { url: '/html' },
+            { body: '<p>test</p>', headers: { 'Content-Language': 'en', 'Content-Type': 'text/html; charset=utf8' }});
+        assert.response(app,
+            { url: '/json' },
+            { body: '{"foo":"bar"}', status: 201, headers: { 'Content-Type': 'application/json; charset=utf8' }});
+    }
+};
