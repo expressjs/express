ac17a6afcf9ef8b48398f5f23a050d7880bbcad8
Tj Holowaychuk <tj@vision-media.ca>
2010-07-07 13:32:16 -0700

    Added res.contentType() tests

--- test/response.test.js
+++ test/response.test.js
@@ -23,5 +23,19 @@ module.exports = {
         assert.response(app,
             { url: '/json' },
             { body: '{"foo":"bar"}', status: 201, headers: { 'Content-Type': 'application/json; charset=utf8' }});
+    },
+    
+    '#contentType': function(assert){
+        var app = express.createServer();
+        
+        app.get('/html', function(req, res){
+            res.contentType('index.html');
+            res.writeHead(200, res.headers);
+            res.end('<p>yay</p>');
+        });
+        
+        assert.response(app,
+            { url: '/html' },
+            { body: '<p>yay</p>', headers: { 'Content-Type': 'text/html; charset=utf-8' }});
     }
 };
