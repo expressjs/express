35af1bab30bc69a2c4c8aea8df99bcdd76cd9c68
Tj Holowaychuk <tj@vision-media.ca>
2010-07-07 13:22:59 -0700

    Added req.xhr as alias of req.isXMLHttpRequest

--- test/request.test.js
+++ test/request.test.js
@@ -10,6 +10,7 @@ module.exports = {
         var app = express.createServer();
         
         app.get('/isxhr', function(req, res, params){
+            assert.equal(req.xhr, req.isXMLHttpRequest);
             res.send(req.isXMLHttpRequest
                 ? 'yeaaa boy'
                 : 'nope');
