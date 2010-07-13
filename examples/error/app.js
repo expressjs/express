caed07dfc125ebaa609c7b3ffc59b77d64a2e86d
Tj Holowaychuk <tj@vision-media.ca>
2010-07-13 08:48:44 -0700

    Added exception example

---
+++ examples/error/app.js
@@ -0,0 +1,26 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('./../../lib/express'),
+    connect = require('connect');
+
+var app = express.createServer();
+
+app.get('/', function(req, res){
+    // Caught and passed down to the errorHandler middleware
+    throw new Error('something broke!');
+});
+
+app.get('/next', function(req, res, params, next){
+    // We can also pass exceptions to next()
+    next(new Error('oh no!'))
+});
+
+// The errorHandler middleware in this case will dump exceptions to stderr
+// as well as show the stack trace in responses, currently handles text/plain,
+// text/html, and application/json responses to aid in development
+app.use('/', connect.errorHandler({ dumpExceptions: true, showStack: true }));
+
+app.listen(3000);
