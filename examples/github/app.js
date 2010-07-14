3ee4492b3cbe5e60ac374e0b6c49e8c3c142decd
Tj Holowaychuk <tj@vision-media.ca>
2010-07-14 11:11:14 -0700

    Started github example

---
+++ examples/github/app.js
@@ -0,0 +1,55 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('./../../lib/express'),
+    http = require('http');
+
+var app = express.createServer();
+
+app.set('views', __dirname + '/views');
+
+/**
+ * Request github json api `path`.
+ *
+ * @param {String} path
+ * @param {Function} fn
+ * @api public
+ */
+
+function request(path, fn){
+    var client = http.createClient(80, 'github.com'),
+        req = client.request('GET', '/api/v2/json' + path, { Host: 'github.com' });
+    req.addListener('response', function(res){
+        res.body = '';
+        res.addListener('data', function(chunk){ res.body += chunk; });
+        res.addListener('end', function(){
+            try {
+                fn(null, JSON.parse(res.body));
+            } catch (err) {
+                fn(err);
+            }
+        });
+    });
+    req.end();
+}
+
+app.get('/', function(req, res, params, next){
+    var name = 'visionmedia';
+    request('/repos/show/' + name, function(err, user){
+        require('sys').puts(require('sys').inspect(user.repositories));
+        if (err) {
+            next(err)
+        } else {
+            res.render('index.jade', {
+                locals: {
+                    repos: user.repositories,
+                    name: name
+                }
+            });
+        }
+    });
+});
+
+app.listen(3000);
