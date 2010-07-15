44a4a18320a60ecafe0735427b5a7d83da69cc29
Tj Holowaychuk <tj@vision-media.ca>
2010-07-15 15:52:47 -0700

    Removed version format test

--- test/express.test.js
+++ test/express.test.js
@@ -3,10 +3,6 @@ var express = require('express'),
     connect = require('connect');
 
 module.exports = {
-    'test .version': function(assert){
-        assert.ok(/^\d+\.\d+\.\d+$/.test(express.version), 'Test express.version format');
-    },
-    
     'test inheritance': function(assert){
         var server = express.createServer();
         assert.ok(server instanceof connect.Server, 'Test serverlication inheritance');
