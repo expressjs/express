b68473ae1185e1056e49bbfc49a9aa9a7406e6c6
Tj Holowaychuk <tj@vision-media.ca>
2010-07-22 09:28:59 -0700

    Multi-file example from blog. Closes #364

--- examples/blog/contact.js
+++ examples/blog/contact.js
@@ -4,7 +4,6 @@
 // which required it)
 var app = module.parent.exports;
 
-app.get('/', function(req, res){
-    res.send('<p>Visit <a href="/blog">/blog</a>'
-        + ' or <a href="/contact">/contact</a></p>');
+app.get('/contact', function(req, res){
+    res.send('<p>Wahoo contact page</p>');
 });
