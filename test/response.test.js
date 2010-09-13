a13df5b68db5d4d1969f14ab50343c9a29baed14
Tj Holowaychuk <tj@vision-media.ca>
2010-09-13 09:44:42 -0700

    Added test for res.clearCookie()

--- test/response.test.js
+++ test/response.test.js
@@ -255,5 +255,23 @@ module.exports = {
                     'rememberme=yes; expires=Thu, 01 Jan 1970 00:00:00 GMT; httpOnly',
                     res.headers['set-cookie'][0]);
             });
+    },
+    
+    'test #clearCookie()': function(assert){
+        var app = express.createServer();
+        
+        app.get('/', function(req, res){
+            res.clearCookie('rememberme');
+            res.redirect('/');
+        });
+        
+        assert.response(app,
+            { url: '/' },
+            function(res){
+                //assert.length(res.headers['set-cookie'], 1);
+                assert.equal(
+                    'rememberme=; expires=Thu, 01 Jan 1970 00:00:00 GMT',
+                    res.headers['set-cookie'][0]);
+            });
     }
 };
