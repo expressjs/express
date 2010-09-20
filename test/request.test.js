7c3dc7aa71b127f5c18dcac909dff9d6447dafd5
James Cooper <james@bitmechanic.com>
2010-09-20 07:22:36 -0700

    Added local() function to request + tests

--- test/request.test.js
+++ test/request.test.js
@@ -153,5 +153,26 @@ module.exports = {
         assert.response(app,
             { url: '/' },
             { body: 'ok' });
+    },
+
+    'test #local()': function(assert) {
+        var app = express.createServer();
+
+	app.get('/', function(req, res) {
+	    assert.eql(undefined, req.local('userId'));
+
+            req.local('userId', 'abc');
+            assert.eql('abc', req.local('userId'));
+
+            req.local('userId', ['a','b']);
+            assert.eql(['a','b'], req.local('userId'));
+
+            res.send('ok');
+	});
+
+        assert.response(app,
+            { url: '/' },
+            { body: 'ok' });
     }
+
 };
