2fc9a81e9eec98fb7f4933a716139010716badd8
Tj Holowaychuk <tj@vision-media.ca>
2010-10-15 09:55:04 -0700

    Added partial local inheritance [suggested by masylum]. Closes #102

--- test/view.test.js
+++ test/view.test.js
@@ -482,6 +482,7 @@ module.exports = {
             '<html>',
             '<body>',
             '<h1>My Cool Pets</h1>',
+            '<p>We have 3 cool pets\n</p>',
             '<ul>',
             '<li>Tobi is the coolest of My Cool Pets</li>',
             '<li>Jane</li>',
