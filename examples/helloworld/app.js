3d159015bedd891d51398deba47d5eb8c46b8895
Tj Holowaychuk <tj@vision-media.ca>
2010-11-25 13:56:12 -0800

    misc

--- examples/helloworld/app.js
+++ examples/helloworld/app.js
@@ -8,7 +8,7 @@ var express = require('./../../lib/express');
 var app = express.createServer();
 
 app.get('/', function(req, res){
-    res.send('Hello World');
+  res.send('Hello World');
 });
 
 app.listen(3000);
