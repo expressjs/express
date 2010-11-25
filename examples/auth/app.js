8d7e35b7239c6edf312bb06b959f01ecdf6a5db6
Tj Holowaychuk <tj@vision-media.ca>
2010-11-25 13:39:07 -0800

    refactored auth example

--- examples/auth/app.js
+++ examples/auth/app.js
@@ -6,8 +6,8 @@ require.paths.unshift(__dirname + '/../../support');
  * Module dependencies.
  */
 
-var express = require('../../lib/express'),
-    crypto = require('crypto');
+var express = require('../../lib/express')
+  , crypto = require('crypto');
 
 var app = express.createServer();
 
@@ -22,44 +22,44 @@ app.use(express.session());
 // however this is more light-weight for an example
 
 app.dynamicHelpers({
-    message: function(req){
-        var err = req.session.error,
-            msg = req.session.success;
-        delete req.session.error;
-        delete req.session.success;
-        if (err) return '<p class="msg error">' + err + '</p>';
-        if (msg) return '<p class="msg success">' + msg + '</p>';
-    }
+  message: function(req){
+    var err = req.session.error
+      , msg = req.session.success;
+    delete req.session.error;
+    delete req.session.success;
+    if (err) return '<p class="msg error">' + err + '</p>';
+    if (msg) return '<p class="msg success">' + msg + '</p>';
+  }
 });
 
 // Generate a salt for the user to prevent rainbow table attacks
 
 var users = {
-    tj: {
-        name: 'tj',
-        salt: 'randomly-generated-salt',
-        pass: md5('foobar' + 'randomly-generated-salt')
-    }
+  tj: {
+    name: 'tj'
+    , salt: 'randomly-generated-salt'
+    , pass: md5('foobar' + 'randomly-generated-salt')
+  }
 };
 
 // Used to generate a hash of the plain-text password + salt
 
 function md5(str) {
-    return crypto.createHash('md5').update(str).digest('hex');
+  return crypto.createHash('md5').update(str).digest('hex');
 }
 
 // Authenticate using our plain-object database of doom!
 
 function authenticate(name, pass, fn) {
-    var user = users[name];
-    // query the db for the given username
-    if (!user) return fn(new Error('cannot find user'));
-    // apply the same algorithm to the POSTed password, applying
-    // the md5 against the pass / salt, if there is a match we
-    // found the user
-    if (user.pass == md5(pass + user.salt)) return fn(null, user);
-    // Otherwise password is invalid
-    fn(new Error('invalid password'));
+  var user = users[name];
+  // query the db for the given username
+  if (!user) return fn(new Error('cannot find user'));
+  // apply the same algorithm to the POSTed password, applying
+  // the md5 against the pass / salt, if there is a match we
+  // found the user
+  if (user.pass == md5(pass + user.salt)) return fn(null, user);
+  // Otherwise password is invalid
+  fn(new Error('invalid password'));
 }
 
 function restrict(req, res, next) {
@@ -72,54 +72,54 @@ function restrict(req, res, next) {
 }
 
 function accessLogger(req, res, next) {
-    console.log('/restricted accessed by %s', req.session.user.name);
-    next();
+  console.log('/restricted accessed by %s', req.session.user.name);
+  next();
 }
 
 app.get('/', function(req, res){
-    res.redirect('/login');
+  res.redirect('/login');
 });
 
 app.get('/restricted', restrict, accessLogger, function(req, res){
-    res.send('Wahoo! restricted area');
+  res.send('Wahoo! restricted area');
 });
 
 app.get('/logout', function(req, res){
-    // destroy the user's session to log them out
-    // will be re-created next request
-    req.session.destroy(function(){
-        res.redirect('home');
-    });
+  // destroy the user's session to log them out
+  // will be re-created next request
+  req.session.destroy(function(){
+    res.redirect('home');
+  });
 });
 
 app.get('/login', function(req, res){
-    if (req.session.user) {
-        req.session.success = 'Authenticated as ' + req.session.user.name
-            + ' click to <a href="/logout">logout</a>. '
-            + ' You may now access <a href="/restricted">/restricted</a>.';
-    }
-    res.render('login');
+  if (req.session.user) {
+    req.session.success = 'Authenticated as ' + req.session.user.name
+      + ' click to <a href="/logout">logout</a>. '
+      + ' You may now access <a href="/restricted">/restricted</a>.';
+  }
+  res.render('login');
 });
 
 app.post('/login', function(req, res){
-    authenticate(req.body.username, req.body.password, function(err, user){
-        if (user) {
-            // Regenerate session when signing in
-            // to prevent fixation 
-            req.session.regenerate(function(){
-                // Store the user's primary key 
-                // in the session store to be retrieved,
-                // or in this case the entire user object
-                req.session.user = user;
-                res.redirect('back');
-            });
-        } else {
-            req.session.error = 'Authentication failed, please check your '
-                + ' username and password.'
-                + ' (use "tj" and "foobar")';
-            res.redirect('back');
-        }
-    });
+  authenticate(req.body.username, req.body.password, function(err, user){
+    if (user) {
+      // Regenerate session when signing in
+      // to prevent fixation 
+      req.session.regenerate(function(){
+        // Store the user's primary key 
+        // in the session store to be retrieved,
+        // or in this case the entire user object
+        req.session.user = user;
+        res.redirect('back');
+      });
+    } else {
+      req.session.error = 'Authentication failed, please check your '
+        + ' username and password.'
+        + ' (use "tj" and "foobar")';
+      res.redirect('back');
+    }
+  });
 });
 
 app.listen(3000);
