65bf562ff4f609607238271da884bc1b8acdf691
Tj Holowaychuk <tj@vision-media.ca>
2010-11-25 13:41:15 -0800

    refactored downloads example

--- examples/downloads/app.js
+++ examples/downloads/app.js
@@ -8,24 +8,24 @@ var express = require('./../../lib/express');
 var app = express.createServer();
 
 app.get('/', function(req, res){
-    res.send('<ul>'
-        + '<li>Download <a href="/files/amazing.txt">amazing.txt</a>.</li>'
-        + '<li>Download <a href="/files/missing.txt">missing.txt</a>.</li>'
-        + '</ul>');
+  res.send('<ul>'
+    + '<li>Download <a href="/files/amazing.txt">amazing.txt</a>.</li>'
+    + '<li>Download <a href="/files/missing.txt">missing.txt</a>.</li>'
+    + '</ul>');
 });
 
 app.get('/files/*', function(req, res){
-    var file = req.params[0];
-    res.download(__dirname + '/files/' + file);
+  var file = req.params[0];
+  res.download(__dirname + '/files/' + file);
 });
 
 app.error(function(err, req, res, next){
-    if (process.ENOENT == err.errno) {
-        res.send('Cant find that file, sorry!');
-    } else {
-        // Not a 404
-        next(err);
-    }
+  if (process.ENOENT == err.errno) {
+    res.send('Cant find that file, sorry!');
+  } else {
+    // Not a 404
+    next(err);
+  }
 });
 
 app.listen(3000);
