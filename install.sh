ded6f9af47a7ee53454b2c75a873b684387c813b
Dan Rench <drench+github@gmail.com>
2010-12-31 15:35:00 -0600

    Explicit "-f -" for compatibility with BSD tar

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ install() {
         && cd /tmp/$2 \
         && echo "... installing $2" \
         && curl -# -L "http://github.com/$1/$2/tarball/master" \
-            | tar xz --strip 1 \
+            | tar xzf - --strip 1 \
         && mkdir -p ~/.node_libraries \
         && cp -fr lib/$2 ~/.node_libraries/$2
 }
