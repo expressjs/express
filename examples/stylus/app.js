a61615a196a7193c3e1bbd9cd72dc8758997d3ed
Tj Holowaychuk <tj@vision-media.ca>
2011-02-08 10:42:39 -0800

    Setting up Stylus example

---
+++ examples/stylus/app.js
@@ -0,0 +1,32 @@
+
+// Expose modules in ./support for demo purposes
+require.paths.unshift(__dirname + '/../../support');
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('../../lib/express')
+  , stylus = require('stylus');
+
+var app = express.createServer();
+
+// $ npm install stylus
+
+function compile(str, path, fn) {
+  stylus(str)
+    .set('filename', path)
+    .set('compress', true)
+    .render(fn);
+};
+
+app.use(stylus.middleware({
+    src: __dirname + '/views'
+  , dest: __dirname + '/public'
+  , compile: compile
+}));
+
+app.use(staticProvider(__dirname + '/public'));
+
+app.listen(3000);
+console.log('server listening on port 3000');
