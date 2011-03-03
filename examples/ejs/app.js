2f7b78c03a021b3362959103333202d2e608f845
Tj Holowaychuk <tj@vision-media.ca>
2011-03-02 18:27:12 -0800

    ejs example using app.register()

--- examples/ejs/app.js
+++ examples/ejs/app.js
@@ -10,9 +10,14 @@ var express = require('../../lib/express');
 
 var app = express.createServer();
 
+// Register ejs as .html
+
+app.register('.html', require('ejs'));
+
 // Optional since express defaults to CWD/views
 
 app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
 
 // Dummy users
 var users = [
@@ -22,7 +27,7 @@ var users = [
 ];
 
 app.get('/', function(req, res){
-  res.render('users.ejs', { users: users });
+  res.render('users', { users: users });
 });
 
 app.listen(3000);
