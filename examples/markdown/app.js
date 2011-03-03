61aec6e96104c3b590f1f8541582d13419ad1821
Tj Holowaychuk <tj@vision-media.ca>
2011-03-02 18:37:36 -0800

    Added markdown template engine example

---
+++ examples/markdown/app.js
@@ -0,0 +1,41 @@
+
+// Expose modules in ./support for demo purposes
+require.paths.unshift(__dirname + '/../../support');
+
+// $ npm install markdown
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('../../lib/express')
+  , md = require('markdown').markdown;
+
+var app = express.createServer();
+
+// register .md so that markdown may comply
+// with the express view system by implementing
+// a .compile() method
+
+app.register('.md', {
+  compile: function(str, options){
+    var html = md.toHTML(str);
+    return function(locals){
+      return html.replace(/\{([^}]+)\}/g, function(_, name){
+        return locals[name];
+      });
+    };
+  }
+});
+
+// Optional since express defaults to CWD/views
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'md');
+
+app.get('/', function(req, res){
+  res.render('index', { layout: false, title: 'Markdown Example' });
+});
+
+app.listen(3000);
+console.log('Express app started on port 3000');
