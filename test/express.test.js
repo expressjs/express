07c9cae923b31cb52330bd7b8b5b1bda8e73c68c
Tj Holowaychuk <tj@vision-media.ca>
2011-03-14 14:36:22 -0700

    Fixed; expose HTTPSServer constructor

--- test/express.test.js
+++ test/express.test.js
@@ -13,6 +13,11 @@ module.exports = {
       var server = express.createServer();
       server.should.be.an.instanceof(connect.HTTPServer);
   },
+
+  'test constructor exports': function(){
+    express.should.have.property('HTTPServer');
+    express.should.have.property('HTTPSServer');
+  },
   
   'test connect middleware autoloaders': function(){
     express.errorHandler.should.equal(connect.errorHandler);
