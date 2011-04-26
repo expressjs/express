a4cfde350f2dbb012ae0029691791be0897bfe67
Tj Holowaychuk <tj@vision-media.ca>
2011-04-26 13:14:21 -0700

    moved more tests

--- test/router.test.js
+++ test/router.test.js
@@ -10,6 +10,70 @@ var express = require('express')
   , Route = express.Route;
 
 module.exports = {
+  'test route middleware': function(beforeExit){
+    var app = express.createServer()
+      , calls = 0;
+
+    function allow(role) {
+      return function(req, res, next) {
+        // this is totally not real, dont use this :)
+        // for tests only
+        if (req.headers['x-role'] == role) {
+          next();
+        } else {
+          res.send(401);
+        }
+      }
+    }
+    
+    function restrictAge(age) {
+      return function(req, res, next){
+        if (req.headers['x-age'] >= age) {
+          next();
+        } else {
+          res.send(403);
+        }
+      }
+    }
+
+    app.param('user', function(req, res, next, user){
+      ++calls;
+      next();
+    });
+
+    app.get('/xxx', allow('member'), restrictAge(18), function(req, res){
+      res.send(200);
+    });
+
+    app.get('/booze', [allow('member')], restrictAge(18), function(req, res){
+      res.send(200);
+    });
+
+    app.get('/tobi', [allow('member')], [[restrictAge(18)]], function(req, res){
+      res.send(200);
+    });
+    
+    app.get('/user/:user', [allow('member')], [[restrictAge(18)]], function(req, res){
+      res.send(200);
+    });
+
+    ['xxx', 'booze', 'tobi', 'user/tj'].forEach(function(thing){
+      assert.response(app,
+        { url: '/' + thing },
+        { body: 'Unauthorized', status: 401 });
+      assert.response(app,
+        { url: '/' + thing, headers: { 'X-Role': 'member' }},
+        { body: 'Forbidden', status: 403 });
+      assert.response(app,
+        { url: '/' + thing, headers: { 'X-Role': 'member', 'X-Age': 18 }},
+        { body: 'OK', status: 200 });
+    });
+
+    beforeExit(function(){
+      calls.should.equal(3);
+    });
+  },
+
   'test named capture groups': function(){
     var app = express.createServer();
 
