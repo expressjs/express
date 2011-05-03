b8068460492e8885d84fd34f5371785ed5d56071
Tj Holowaychuk <tj@vision-media.ca>
2011-05-03 16:44:17 -0700

    misc

--- examples/say/app.js
+++ examples/say/app.js
@@ -8,7 +8,8 @@ require.paths.unshift(__dirname + '/../../support');
 
 var express = require('../../')
   , path = require('path')
-  , exec = require('child_process').exec;
+  , exec = require('child_process').exec
+  , fs = require('fs');
 
 /**
  * Error handler.
@@ -18,21 +19,27 @@ function errorHandler(err, req, res, next) {
   var parts = err.stack.split('\n')[1].split(/[()]/)[1].split(':')
     , filename = parts.shift()
     , basename = path.basename(filename)
-    , line = parts.shift()
-    , col = parts.shift();
+    , lineno = parts.shift()
+    , col = parts.shift()
+    , lines = fs.readFileSync(filename, 'utf8').split('\n')
+    , line = lines[lineno - 1].replace(/\./, ' ');
 
   exec('say '
     + err.message
-    + ' on line ' + line
-    + ' of ' + basename);
+    + ' on line ' + lineno
+    + ' of ' + basename + '.'
+    + ' The contents of this line is '
+    + ' "' + line + '".');
 
   res.send(500);
 }
 
 var app = express.createServer();
 
-app.get('/', function(req, res){
-  throw new Error('something exploded');
+app.get('/', function(request, response){
+  if (request.is(foo)) {
+    
+  }
 });
 
 app.use(errorHandler);
