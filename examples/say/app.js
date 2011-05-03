6bef3ef891f45f24e99aaa26f532849da5421287
Tj Holowaychuk <tj@vision-media.ca>
2011-05-03 16:48:17 -0700

    misc

--- examples/say/app.js
+++ examples/say/app.js
@@ -15,33 +15,33 @@ var express = require('../../')
  * Error handler.
  */
 
-function errorHandler(err, req, res, next) {
-  var parts = err.stack.split('\n')[1].split(/[()]/)[1].split(':')
-    , filename = parts.shift()
-    , basename = path.basename(filename)
-    , lineno = parts.shift()
-    , col = parts.shift()
-    , lines = fs.readFileSync(filename, 'utf8').split('\n')
-    , line = lines[lineno - 1].replace(/\./, ' ');
+function errorHandler(voice) {
+  return function(err, req, res, next) {
+    var parts = err.stack.split('\n')[1].split(/[()]/)[1].split(':')
+      , filename = parts.shift()
+      , basename = path.basename(filename)
+      , lineno = parts.shift()
+      , col = parts.shift()
+      , lines = fs.readFileSync(filename, 'utf8').split('\n')
+      , line = lines[lineno - 1].replace(/\./, ' ');
 
-  exec('say '
-    + err.message
-    + ' on line ' + lineno
-    + ' of ' + basename + '.'
-    + ' The contents of this line is '
-    + ' "' + line + '".');
+    exec('say -v "' + voice + '" '
+      + err.message
+      + ' on line ' + lineno
+      + ' of ' + basename + '.'
+      + ' The contents of this line is '
+      + ' "' + line + '".');
 
-  res.send(500);
+    res.send(500);
+  }
 }
 
 var app = express.createServer();
 
 app.get('/', function(request, response){
-  if (request.is(foo)) {
-    
-  }
+  if (request.is(foo)) response.end('bar');
 });
 
-app.use(errorHandler);
+app.use(errorHandler('Vicki'));
 
 app.listen(3000);
