b67bacea186270cce033fbe72755788716416954
Tj Holowaychuk <tj@vision-media.ca>
2011-06-02 13:58:46 -0700

    more refactoring of cookie example

--- examples/cookies/app.js
+++ examples/cookies/app.js
@@ -37,9 +37,7 @@ app.get('/forget', function(req, res){
 
 app.post('/', function(req, res){
   var minute = 60000;
-  if (req.body.remember) {
-    res.cookie('remember', 1, { maxAge: minute, path: '/' });
-  }
+  if (req.body.remember) res.cookie('remember', 1, { maxAge: minute });
   res.redirect('back');
 });
 
