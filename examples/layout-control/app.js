3ec2739925cc43ea35e6d485168687bd1603e8f7
Tj Holowaychuk <tj@vision-media.ca>
2011-07-11 13:19:00 -0700

    Removed "view options" setting. Closes #748

--- examples/layout-control/app.js
+++ examples/layout-control/app.js
@@ -10,7 +10,7 @@ var app = express.createServer();
 app.set('views', __dirname + '/views');
 
 // set default layout, usually "layout"
-app.set('view options', { layout: 'layouts/default' });
+app.local('layout', 'layouts/default');
 
 // Set our default template engine to "ejs"
 // which prevents the need for extensions
