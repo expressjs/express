23dbe6a5f0a163e75469b90a82e48a828e202369
Tj Holowaychuk <tj@vision-media.ca>
2011-08-08 17:15:13 -0700

    better layout control example

--- examples/layout-control/app.js
+++ examples/layout-control/app.js
@@ -3,29 +3,39 @@
  * Module dependencies.
  */
 
-var express = require('../../lib/express');
+var express = require('../../lib/express')
+  , url = require('url');
 
 var app = express.createServer();
 
 app.set('views', __dirname + '/views');
 
+// map .html to ejs module
+app.register('html', require('ejs'));
+app.set('view engine', 'html');
+
 // set default layout, usually "layout"
 app.locals.layout = 'layouts/default';
 
-// Set our default template engine to "ejs"
-// which prevents the need for extensions
-// (although you can still mix and match)
-app.set('view engine', 'ejs');
+// expose the current path as a view local
+app.use(function(req, res, next){
+  res.locals.path = url.parse(req.url).pathname;
+  next();
+});
 
 app.get('/', function(req, res){
-  res.render('pages/default');
+  res.render('page');
 });
 
 app.get('/alternate', function(req, res){
+  res.render('page', { layout: 'layouts/alternate' });
+});
+
+app.get('/defined-in-view', function(req, res){
   // note that we do not explicitly
   // state the layout here, the view does,
   // although we could do it here as well.
-  res.render('pages/alternate');
+  res.render('pages');
 });
 
 app.listen(3000);
