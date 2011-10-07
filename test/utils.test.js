fed7fa4cd2f3fa21a09b344487bc951592932bd3
Tj Holowaychuk <tj@vision-media.ca>
2011-10-07 13:56:14 -0700

    removed old parseRange util

--- test/utils.test.js
+++ test/utils.test.js
@@ -14,16 +14,5 @@ module.exports = {
     utils.miniMarkdown('__foo__').should.equal('<strong>foo</strong>');
     utils.miniMarkdown('_foo__').should.equal('<em>foo</em>_');  
     utils.miniMarkdown('[User Login](/login)').should.equal('<a href="/login">User Login</a>');
-  },
-  
-  'test .parseRange()': function(){
-    utils.parseRange(1000, 'bytes=0-499').should.eql([{ start: 0, end: 499 }]);
-    utils.parseRange(1000, 'bytes=40-80').should.eql([{ start: 40, end: 80 }]);
-    utils.parseRange(1000, 'bytes=-500').should.eql([{ start: 500, end: 999 }]);
-    utils.parseRange(1000, 'bytes=-400').should.eql([{ start: 600, end: 999 }]);
-    utils.parseRange(1000, 'bytes=500-').should.eql([{ start: 500, end: 999 }]);
-    utils.parseRange(1000, 'bytes=400-').should.eql([{ start: 400, end: 999 }]);
-    utils.parseRange(1000, 'bytes=0-0').should.eql([{ start: 0, end: 0 }]);
-    utils.parseRange(1000, 'bytes=-1').should.eql([{ start: 999, end: 999 }]);
   }
 };
