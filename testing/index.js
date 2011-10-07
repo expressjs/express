65c0d14a4eb8a3724d7b781d790d315c5ac1fd7d
Tj Holowaychuk <tj@vision-media.ca>
2011-10-07 10:00:10 -0700

    expose connect middleware as express.* again

--- testing/index.js
+++ testing/index.js
@@ -3,7 +3,13 @@
  * Module dependencies.
  */
 
-var express = require('../');
+var express = require('../')
+  , http = require('http');
 
 var app = express();
 
+app.use(express.logger('dev'));
+app.use(express.static(__dirname));
+
+http.createServer(app).listen(3000);
+console.log('listening on port 3000');
