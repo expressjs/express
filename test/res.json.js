a013ffe547834fccc992f3fbc444a4d544c1ba2a
Tj Holowaychuk <tj@vision-media.ca>
2011-11-08 17:57:25 -0800

    Added res.json(status, object) test

--- test/res.json.js
+++ test/res.json.js
@@ -40,4 +40,23 @@ describe('res', function(){
       })
     })
   })
+  
+  describe('.json(status, object)', function(){
+    it('should respond with json and set the .statusCode', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.json(201, { id: 1 });
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        res.statusCode.should.equal(201);
+        res.headers.should.have.property('content-type', 'application/json; charset=utf-8');
+        res.body.should.equal('{"id":1}');
+        done();
+      })
+    })
+  })
 })
