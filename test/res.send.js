7b50a78662e196c3574b076a6f81bdce3026b258
Tj Holowaychuk <tj@vision-media.ca>
2011-11-08 17:38:18 -0800

    Added res.send(null || undefined) tests

--- test/res.send.js
+++ test/res.send.js
@@ -3,6 +3,40 @@ var express = require('../')
   , request = require('./support/http');
 
 describe('res', function(){
+  describe('.send(null)', function(){
+    it('should set body to ""', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.send(null);
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        res.body.should.equal('');
+        done();
+      })
+    })
+  })
+  
+  describe('.send(undefined)', function(){
+    it('should set body to ""', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.send(undefined);
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        res.body.should.equal('');
+        done();
+      })
+    })
+  })
+
   describe('.send(code)', function(){
     it('should set .statusCode', function(done){
       var app = express();
