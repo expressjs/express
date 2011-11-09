84f01d1e30d8193025c954b0310c44bf87bb7a30
Tj Holowaychuk <tj@vision-media.ca>
2011-11-09 15:00:40 -0800

    Added res.render() tests

---
+++ test/res.render.js
@@ -0,0 +1,144 @@
+
+var express = require('../')
+  , request = require('./support/http');
+
+describe('res', function(){
+  describe('.render(name)', function(){
+    it('should expose app.locals', function(done){
+      var app = express();
+  
+      app.set('views', __dirname + '/fixtures');
+      app.locals.user = { name: 'tobi' };
+
+      app.use(function(req, res){
+        res.render('user.jade');
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        res.body.should.equal('<p>tobi</p>');
+        done();
+      });
+    })
+  
+    it('should support index.<engine>', function(done){
+      var app = express();
+  
+      app.set('views', __dirname + '/fixtures');
+      app.set('view engine', 'jade');
+
+      app.use(function(req, res){
+        res.render('blog/post');
+      });
+      
+      request(app)
+      .get('/')
+      .end(function(res){
+        res.body.should.equal('<h1>blog post</h1>');
+        done();
+      });
+    })
+  
+    describe('when an error occurs', function(){
+      it('should next(err)', function(done){
+        var app = express();
+  
+        app.set('views', __dirname + '/fixtures');
+
+        app.use(function(req, res){
+          res.render('user.jade');
+        });
+        
+        app.use(function(err, req, res, next){
+          res.end(err.message);
+        });
+
+        request(app)
+        .get('/')
+        .end(function(res){
+          res.body.should.match(/user is not defined/);
+          done();
+        });
+      })
+    })
+  
+    describe('when "view engine" is given', function(){
+      it('should render the template', function(done){
+        var app = express();
+  
+        app.set('view engine', 'jade');
+        app.set('views', __dirname + '/fixtures');
+
+        app.use(function(req, res){
+          res.render('email');
+        });
+
+        request(app)
+        .get('/')
+        .end(function(res){
+          res.body.should.equal('<p>This is an email</p>');
+          done();
+        });
+      })
+    })
+  })
+
+  describe('.render(name, option)', function(){
+    it('should render the template', function(done){
+      var app = express();
+  
+      app.set('views', __dirname + '/fixtures');
+  
+      var user = { name: 'tobi' };
+
+      app.use(function(req, res){
+        res.render('user.jade', { user: user });
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        res.body.should.equal('<p>tobi</p>');
+        done();
+      });
+    })
+  
+    it('should expose app.locals', function(done){
+      var app = express();
+    
+      app.set('views', __dirname + '/fixtures');
+      app.locals.user = { name: 'tobi' };
+
+      app.use(function(req, res){
+        res.render('user.jade', {});
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        res.body.should.equal('<p>tobi</p>');
+        done();
+      });
+    })
+    
+    it('should give precedence to res.render() locals', function(done){
+      var app = express();
+    
+      app.set('views', __dirname + '/fixtures');
+      app.locals.user = { name: 'tobi' };
+      var jane = { name: 'jane' };
+    
+      app.use(function(req, res){
+        res.render('user.jade', { user: jane });
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        res.body.should.equal('<p>jane</p>');
+        done();
+      });
+    })
+  })
+})
