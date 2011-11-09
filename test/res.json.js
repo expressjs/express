835558229f58d032eb399d6c3241015d10c3c531
Tj Holowaychuk <tj@vision-media.ca>
2011-11-08 17:55:41 -0800

    refactored tests

--- test/res.json.js
+++ test/res.json.js
@@ -2,32 +2,42 @@
 var express = require('../')
   , request = require('./support/http');
 
-function test(input, output) {
-  return function(done){
-    var app = express();
-
-    app.use(function(req, res){
-      res.json(input);
-    });
-
-    request(app)
-    .get('/')
-    .end(function(res){
-      res.headers.should.have.property('content-type', 'application/json; charset=utf-8');
-      res.body.should.equal(output);
-      done();
-    })
-  }
-}
-
 describe('res', function(){
   describe('.json(object)', function(){
     describe('when given primitives', function(){
-      it('should respond with json', test(null, 'null'))
+      it('should respond with json', function(done){
+        var app = express();
+
+        app.use(function(req, res){
+          res.json(null);
+        });
+
+        request(app)
+        .get('/')
+        .end(function(res){
+          res.headers.should.have.property('content-type', 'application/json; charset=utf-8');
+          res.body.should.equal('null');
+          done();
+        })
+      })
     })
     
     describe('when given an object', function(){
-      it('should respond with json', test({ name: 'tobi' }, '{"name":"tobi"}'))
+      it('should respond with json', function(done){
+        var app = express();
+
+        app.use(function(req, res){
+          res.json({ name: 'tobi' });
+        });
+
+        request(app)
+        .get('/')
+        .end(function(res){
+          res.headers.should.have.property('content-type', 'application/json; charset=utf-8');
+          res.body.should.equal('{"name":"tobi"}');
+          done();
+        })
+      })
     })
   })
 })
