7a24021dc97c47d4a6bca2d4be382a8d4be48013
Tj Holowaychuk <tj@vision-media.ca>
2011-11-08 18:40:01 -0800

    more res.redirect() tests

--- test/res.redirect.js
+++ test/res.redirect.js
@@ -3,7 +3,7 @@ var express = require('../')
   , request = require('./support/http');
 
 describe('res', function(){
-  describe('.redirect(absolute-url)', function(){
+  describe('.redirect(url)', function(){
     it('should default to a 302 redirect', function(done){
       var app = express();
 
@@ -16,6 +16,42 @@ describe('res', function(){
       .end(function(res){
         res.statusCode.should.equal(302);
         res.headers.should.have.property('location', 'http://google.com');
+        done();
+      })
+    })
+  })
+  
+  describe('.redirect(status, url)', function(){
+    it('should set the response status', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.redirect(303, 'http://google.com');
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        res.statusCode.should.equal(303);
+        res.headers.should.have.property('location', 'http://google.com');
+        done();
+      })
+    })
+  })
+  
+  describe('when the request method is HEAD', function(){
+    it('should ignore the body', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.redirect('http://google.com');
+      });
+
+      request(app)
+      .head('/')
+      .end(function(res){
+        res.headers.should.have.property('location', 'http://google.com');
+        res.body.should.equal('');
         done();
       })
     })
