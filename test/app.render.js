7e352fe28d566ed4394bbcde84b60a18ddd15673
Tj Holowaychuk <tj@vision-media.ca>
2011-11-08 20:07:49 -0800

    Added app.render() app.locals test

--- test/app.render.js
+++ test/app.render.js
@@ -4,6 +4,18 @@ var express = require('../')
 
 describe('app', function(){
   describe('.render(name, fn)', function(){
+    it('should expose app.locals', function(){
+      var app = express();
+
+      app.set('views', __dirname + '/fixtures');
+      app.locals.user = { name: 'tobi' };
+
+      app.render('user.jade', function(err, str){
+        assert(null == err);
+        str.should.equal('<p>tobi</p>');
+      })
+    })
+
     describe('when an error occurs', function(){
       it('should invoke the callback', function(done){
         var app = express();
