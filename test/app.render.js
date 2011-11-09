d3b48e2fd5026331ce4fcea9521dcb23a8170313
Tj Holowaychuk <tj@vision-media.ca>
2011-11-08 20:06:15 -0800

    Added more app.render() tests

--- test/app.render.js
+++ test/app.render.js
@@ -4,6 +4,22 @@ var express = require('../')
 
 describe('app', function(){
   describe('.render(name, fn)', function(){
+    describe('when an error occurs', function(){
+      it('should invoke the callback', function(done){
+        var app = express();
+
+        app.set('views', __dirname + '/fixtures');
+
+        app.render('user.jade', function(err, str){
+          // nextTick to prevent cyclic
+          process.nextTick(function(){
+            err.message.should.match(/user is not defined/);
+            done();
+          });
+        })
+      })
+    })
+
     describe('when an extension is given', function(){
       it('should render the template', function(done){
         var app = express();
@@ -30,6 +46,22 @@ describe('app', function(){
           str.should.equal('<p>This is an email</p>');
           done();
         })
+      })
+    })
+  })
+  
+  describe('.render(name, options, fn)', function(){
+    it('should render the template', function(done){
+      var app = express();
+
+      app.set('views', __dirname + '/fixtures');
+
+      var user = { name: 'tobi' };
+
+      app.render('user.jade', { user: user }, function(err, str){
+        assert(null == err);
+        str.should.equal('<p>tobi</p>');
+        done();
       })
     })
   })
