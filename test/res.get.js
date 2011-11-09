a2c51984a83078b11474c5be182e054a9edb4269
Tj Holowaychuk <tj@vision-media.ca>
2011-11-08 17:25:05 -0800

    Added res.get(field) test

---
+++ test/res.get.js
@@ -0,0 +1,23 @@
+
+var express = require('../')
+  , request = require('./support/http');
+
+describe('res', function(){
+  describe('.get(field)', function(){
+    it('should get the response header field', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.setHeader('Content-Type', 'text/x-foo');
+        res.send(res.get('Content-Type'));
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        res.body.should.equal('text/x-foo');
+        done();
+      })
+    })
+  })
+})
