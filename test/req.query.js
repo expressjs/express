ddeee9b6dbcd282350163875ad559ad7a1f32229
Tj Holowaychuk <tj@vision-media.ca>
2011-11-08 19:27:09 -0800

    Added req.query tests

---
+++ test/req.query.js
@@ -0,0 +1,37 @@
+
+var express = require('../')
+  , request = require('./support/http');
+
+describe('req', function(){
+  describe('.query', function(){
+    it('should default to {}', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        req.query.should.eql({});
+        res.end();
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        done();
+      });
+    })
+    
+    it('should contain the parsed query-string', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        req.query.should.eql({ user: { name: 'tj' }});
+        res.end();
+      });
+
+      request(app)
+      .get('/?user[name]=tj')
+      .end(function(res){
+        done();
+      });
+    })
+  })
+})
