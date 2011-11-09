66f8ca52d6fc5ea567ebe3d85515595f097f42dc
Tj Holowaychuk <tj@vision-media.ca>
2011-11-08 20:14:53 -0800

    Added app.engine() tests

---
+++ test/app.engine.js
@@ -0,0 +1,38 @@
+
+var express = require('../')
+  , assert = require('assert');
+
+function render(str, options, fn) {
+  str = str.replace('{{user.name}}', options.user.name);
+  fn(null, str);
+}
+
+describe('app', function(){
+  describe('.engine(ext, fn)', function(){
+    it('should map a template engine', function(){
+      var app = express();
+
+      app.set('views', __dirname + '/fixtures');
+      app.engine('.html', render);
+      app.locals.user = { name: 'tobi' };
+
+      app.render('user.html', function(err, str){
+        assert(null == err);
+        str.should.equal('<p>tobi</p>');
+      })
+    })
+    
+    it('should work without leading "."', function(){
+      var app = express();
+
+      app.set('views', __dirname + '/fixtures');
+      app.engine('html', render);
+      app.locals.user = { name: 'tobi' };
+
+      app.render('user.html', function(err, str){
+        assert(null == err);
+        str.should.equal('<p>tobi</p>');
+      })
+    })
+  })
+})
