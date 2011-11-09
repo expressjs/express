5b296b525736f7374ea10bef3aaadaa8085d491e
Tj Holowaychuk <tj@vision-media.ca>
2011-11-08 17:02:15 -0800

    middleware tests
    that dont quite work yet because node is being a PITA

---
+++ test/support/http.js
@@ -0,0 +1,89 @@
+
+/**
+ * Module dependencies.
+ */
+
+var EventEmitter = require('events').EventEmitter
+  , http = require('http');
+
+module.exports = request;
+
+function request(app) {
+  return new Request(app);
+}
+
+function Request(app) {
+  var self = this;
+  this.data = [];
+  this.header = {};
+  this.app = app;
+  if (!this.server) {
+    this.server = http.Server(app);
+    this.server.listen(0, function(){
+      self.addr = self.server.address();
+      self.listening = true;
+    });
+  }
+}
+
+/**
+ * Inherit from `EventEmitter.prototype`.
+ */
+
+Request.prototype.__proto__ = EventEmitter.prototype;
+
+Request.prototype.get = function(path){
+  return this.request('GET', path);
+};
+
+Request.prototype.set = function(field, val){
+  this.header[field] = val;
+  return this;
+};
+
+Request.prototype.write = function(data){
+  this.data.push(data);
+  return this;
+};
+
+Request.prototype.request = function(method, path){
+  this.method = method;
+  this.path = path;
+  return this;
+};
+
+Request.prototype.end = function(fn){
+  var self = this;
+
+  if (this.listening) {
+    var req = http.request({
+        method: this.method
+      , port: this.addr.port
+      , host: this.addr.address
+      , path: this.path
+      , headers: this.header
+    });
+
+    this.data.forEach(function(chunk){
+      req.write(chunk);
+    });
+    
+    req.on('response', function(res){
+      var buf = '';
+      res.setEncoding('utf8');
+      res.on('data', function(chunk){ buf += chunk });
+      res.on('end', function(){
+        res.body = buf;
+        fn(res);
+      });
+    });
+
+    req.end();
+  } else {
+    this.server.on('listening', function(){
+      self.end(fn);
+    });
+  }
+
+  return this;
+};
