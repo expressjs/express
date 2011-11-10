b9dee730a8d14f2ae40b30a566d44f37ab500898
Tj Holowaychuk <tj@vision-media.ca>
2011-11-10 08:22:42 -0800

    Fixed app.render() absolute path support

--- test/app.render.js
+++ test/app.render.js
@@ -3,6 +3,18 @@ var express = require('../');
 
 describe('app', function(){
   describe('.render(name, fn)', function(){
+    it('should support absolute paths', function(done){
+      var app = express();
+
+      app.locals.user = { name: 'tobi' };
+
+      app.render(__dirname + '/fixtures/user.jade', function(err, str){
+        if (err) return done(err);
+        str.should.equal('<p>tobi</p>');
+        done();
+      })
+    })
+
     it('should expose app.locals', function(done){
       var app = express();
 
