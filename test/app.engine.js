e9d7b6fbad057258ed2e59bd1deead672b1e71ab
Tj Holowaychuk <tj@vision-media.ca>
2011-11-10 13:42:08 -0800

    Changed template engine signature to engine.__express(path, options, fn). Closes #878

--- test/app.engine.js
+++ test/app.engine.js
@@ -1,9 +1,13 @@
 
-var express = require('../');
+var express = require('../')
+  , fs = require('fs');
 
-function render(str, options, fn) {
-  str = str.replace('{{user.name}}', options.user.name);
-  fn(null, str);
+function render(path, options, fn) {
+  fs.readFile(path, 'utf8', function(err, str){
+    if (err) return fn(err);
+    str = str.replace('{{user.name}}', options.user.name);
+    fn(null, str);
+  });
 }
 
 describe('app', function(){
