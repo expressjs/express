c9f184c8d560d6f8fd83572dd8d5b13ea6c54ad1
Tj Holowaychuk <tj@vision-media.ca>
2011-11-09 16:11:46 -0800

    Added res.sendfile() ENOENT test

--- test/res.sendfile.js
+++ test/res.sendfile.js
@@ -24,6 +24,28 @@ describe('res', function(){
         done();
       });
     })
+    
+    it('should invoke the callback on 404', function(done){
+      var app = express()
+        , calls = 0;
+
+      app.use(function(req, res){
+        res.sendfile('test/fixtures/nope.html', function(err){
+          assert(!res.headerSent);
+          ++calls;
+          res.send(err.code);
+        });
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        calls.should.equal(1);
+        res.body.should.equal('ENOENT');
+        res.statusCode.should.equal(200);
+        done();
+      });
+    })
   })
 
   describe('.sendfile(path)', function(){
