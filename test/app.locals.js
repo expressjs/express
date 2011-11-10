429e48d8733e86071ca6e10ee5ca986ffd8d2683
Tj Holowaychuk <tj@vision-media.ca>
2011-11-10 08:36:18 -0800

    Added app.locals.settings test

--- test/app.locals.js
+++ test/app.locals.js
@@ -14,4 +14,15 @@ describe('app', function(){
       app.locals.age.should.equal(2);
     })
   })
+  
+  describe('.locals.settings', function(){
+    it('should expose app settings', function(){
+      var app = express();
+      app.set('title', 'House of Manny');
+      var obj = app.locals.settings;
+      obj.should.have.property('root', '/');
+      obj.should.have.property('env', 'test');
+      obj.should.have.property('title', 'House of Manny');
+    })
+  })
 })
