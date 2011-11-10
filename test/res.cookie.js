7ac857acef7bc62eab31ec38f4cea877c58746ca
Tj Holowaychuk <tj@vision-media.ca>
2011-11-10 12:04:21 -0800

    Added JSON cookie support & tests. Closes #879

--- test/res.cookie.js
+++ test/res.cookie.js
@@ -3,7 +3,25 @@ var express = require('../')
   , request = require('./support/http');
 
 describe('res', function(){
-  describe('.cookie(name, value)', function(){
+  describe('.cookie(name, object)', function(){
+    it('should generate a JSON cookie', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.cookie('user', { name: 'tobi' }).end();
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        var val = ['user=j%3A%7B%22name%22%3A%22tobi%22%7D; path=/'];
+        res.headers['set-cookie'].should.eql(val);
+        done();
+      })
+    })
+  })
+
+  describe('.cookie(name, string)', function(){
     it('should set a cookie', function(done){
       var app = express();
 
@@ -57,7 +75,7 @@ describe('res', function(){
     })
   })
   
-  describe('.cookie(name, value, options)', function(){
+  describe('.cookie(name, string, options)', function(){
     it('should set params', function(done){
       var app = express();
 
