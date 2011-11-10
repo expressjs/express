a47b5990487602f7ddd1bee0fac377f1fa219b8e
Tj Holowaychuk <tj@vision-media.ca>
2011-11-10 12:18:16 -0800

    Added req.signedCookies tests

---
+++ test/req.signedCookies.js
@@ -0,0 +1,45 @@
+
+var express = require('../')
+  , request = require('./support/http');
+
+describe('req', function(){
+  describe('.signedCookies', function(){
+    it('should unsign cookies', function(done){
+      var app = express();
+
+      app.use(express.cookieParser('foo bar baz'));
+
+      app.use(function(req, res){
+        req.cookies.should.not.have.property('name');
+        res.end(req.signedCookies.name);
+      });
+
+      request(app)
+      .get('/')
+      .set('Cookie', 'name=tobi.2HDdGQqJ6jQU1S9dagggYDPaxGE')
+      .end(function(res){
+        res.body.should.equal('tobi');
+        done();
+      })
+    })
+    
+    it('should parse JSON cookies', function(done){
+      var app = express();
+
+      app.use(express.cookieParser('foo bar baz'));
+
+      app.use(function(req, res){
+        req.cookies.should.not.have.property('user');
+        res.end(req.signedCookies.user.name);
+      });
+
+      request(app)
+      .get('/')
+      .set('Cookie', 'user=j%3A%7B%22name%22%3A%22tobi%22%7D.aEbp4PGZo63zMX%2FcIMSn2M9pvms')
+      .end(function(res){
+        res.body.should.equal('tobi');
+        done();
+      })
+    })
+  })
+})
