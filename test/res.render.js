8f45dd41907bbb405cad04a9792b6ac023deaffc
Tj Holowaychuk <tj@vision-media.ca>
2011-11-10 08:24:10 -0800

    Added res.render() absolute path tests

--- test/res.render.js
+++ test/res.render.js
@@ -4,6 +4,41 @@ var express = require('../')
 
 describe('res', function(){
   describe('.render(name)', function(){
+    it('should support absolute paths', function(done){
+      var app = express();
+  
+      app.locals.user = { name: 'tobi' };
+
+      app.use(function(req, res){
+        res.render(__dirname + '/fixtures/user.jade');
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        res.body.should.equal('<p>tobi</p>');
+        done();
+      });
+    })
+    
+    it('should support absolute paths with "view engine"', function(done){
+      var app = express();
+  
+      app.locals.user = { name: 'tobi' };
+      app.set('view engine', 'jade');
+
+      app.use(function(req, res){
+        res.render(__dirname + '/fixtures/user');
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        res.body.should.equal('<p>tobi</p>');
+        done();
+      });
+    })
+
     it('should expose app.locals', function(done){
       var app = express();
   
