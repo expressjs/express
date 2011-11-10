d9098c5fcd9f5c0e8b87847f6b63fb568acc11a9
Tj Holowaychuk <tj@vision-media.ca>
2011-11-10 08:23:12 -0800

    more tests

--- test/app.render.js
+++ test/app.render.js
@@ -14,6 +14,19 @@ describe('app', function(){
         done();
       })
     })
+    
+    it('should support absolute paths with "view engine"', function(done){
+      var app = express();
+
+      app.set('view engine', 'jade');
+      app.locals.user = { name: 'tobi' };
+
+      app.render(__dirname + '/fixtures/user', function(err, str){
+        if (err) return done(err);
+        str.should.equal('<p>tobi</p>');
+        done();
+      })
+    })
 
     it('should expose app.locals', function(done){
       var app = express();
