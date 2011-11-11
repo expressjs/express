ffded44f324d76f39f6fff5cf5633fbe7ee4314a
Tj Holowaychuk <tj@vision-media.ca>
2011-11-10 16:11:50 -0800

    Added req.param() tests. Closes #885

---
+++ test/req.param.js
@@ -0,0 +1,56 @@
+
+var express = require('../')
+  , request = require('./support/http');
+
+describe('req', function(){
+  describe('.param(name)', function(){
+    it('should check req.query', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.end(req.param('name'));
+      });
+
+      request(app)
+      .get('/?name=tj')
+      .end(function(res){
+        res.body.should.equal('tj');
+        done();
+      })
+    })
+    
+    it('should check req.body', function(done){
+      var app = express();
+
+      app.use(express.bodyParser());
+
+      app.use(function(req, res){
+        res.end(req.param('name'));
+      });
+
+      request(app)
+      .post('/')
+      .set('Content-Type', 'application/json')
+      .write('{"name":"tj"}')
+      .end(function(res){
+        res.body.should.equal('tj');
+        done();
+      })
+    })
+    
+    it('should check req.params', function(done){
+      var app = express();
+
+      app.get('/user/:name', function(req, res){
+        res.end(req.param('name'));
+      });
+
+      request(app)
+      .get('/user/tj')
+      .end(function(res){
+        res.body.should.equal('tj');
+        done();
+      })
+    })
+  })
+})
