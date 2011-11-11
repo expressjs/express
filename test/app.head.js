74a0177ec92a86cb0ed1cc7a73d0a47b82a3c346
Tj Holowaychuk <tj@vision-media.ca>
2011-11-11 13:49:35 -0800

    Added app.head() override test

--- test/app.head.js
+++ test/app.head.js
@@ -1,8 +1,9 @@
 
 var express = require('../')
-  , request = require('./support/http');
+  , request = require('./support/http')
+  , assert = require('assert');
 
-describe('app.head()', function(){
+describe('HEAD', function(){
   it('should default to GET', function(done){
     var app = express();
 
@@ -14,5 +15,28 @@ describe('app.head()', function(){
     request(app)
     .head('/tobi')
     .expect(200, done);
+  })
+})
+
+describe('app.head()', function(){
+  it('should override', function(done){
+    var app = express()
+      , called;
+
+    app.head('/tobi', function(req, res){
+      res.end();
+    });
+
+    app.get('/tobi', function(req, res){
+      assert(0, 'should not call GET');
+      res.send('tobi');
+    });
+
+    request(app)
+    .head('/tobi')
+    .expect(200, function(){
+      assert(called);
+      done();
+    });
   })
 })
