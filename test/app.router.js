953eba03769870d94bb5c74fc8af1b3239741865
Tj Holowaychuk <tj@vision-media.ca>
2011-11-11 13:18:50 -0800

    Added meta-generated tests for all HTTP methods supported

--- test/app.router.js
+++ test/app.router.js
@@ -3,6 +3,23 @@ var express = require('../')
   , request = require('./support/http');
 
 describe('app.router', function(){
+  describe('methods supported', function(){
+    express.methods.forEach(function(method){
+      it('should include ' + method, function(done){
+        var app = express();
+        var calls = [];
+
+        app[method]('/foo', function(req, res){
+          res.end(method);
+        });
+
+        request(app)
+        [method]('/foo')
+        .expect(method, done);
+      })
+    });
+  })
+
   it('should be .use()able', function(done){
     var app = express();
 
