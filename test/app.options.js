81652c82445e981276317168a22da677713a5614
Tj Holowaychuk <tj@vision-media.ca>
2011-11-11 13:46:44 -0800

    Added app.del() test

--- test/app.options.js
+++ test/app.options.js
@@ -6,6 +6,7 @@ describe('OPTIONS', function(){
   it('should default to the routes defined', function(done){
     var app = express();
 
+    app.del('/', function(){});
     app.get('/users', function(req, res){});
     app.put('/users', function(req, res){});
 
