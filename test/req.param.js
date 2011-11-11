16587fcce108b840dafb785adf9821cbdf6437c5
Tj Holowaychuk <tj@vision-media.ca>
2011-11-10 16:13:31 -0800

    Added req.param() default value test

--- test/req.param.js
+++ test/req.param.js
@@ -3,6 +3,23 @@ var express = require('../')
   , request = require('./support/http');
 
 describe('req', function(){
+  describe('.param(name, default)', function(){
+    it('should use the default value unless defined', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.end(req.param('name', 'tj'));
+      });
+
+      request(app)
+      .get('/')
+      .end(function(res){
+        res.body.should.equal('tj');
+        done();
+      })
+    })
+  })
+
   describe('.param(name)', function(){
     it('should check req.query', function(done){
       var app = express();
