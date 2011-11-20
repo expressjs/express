4fa9a2e54bd82693b6109051820ee1e460a3a1b1
TJ Holowaychuk <tj@vision-media.ca>
2011-11-20 12:13:24 -0800

    fixed error-pages example

--- examples/error-pages/app.js
+++ examples/error-pages/app.js
@@ -1,27 +1,23 @@
 
-// Expose modules in ./support for demo purposes
-require.paths.unshift(__dirname + '/../../support');
-
 /**
  * Module dependencies.
  */
 
-var express = require('../../lib/express');
+var express = require('../../')
+  , app = express();
 
-var app = express.createServer();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-// Serve default connect favicon
 app.use(express.favicon());
-
-// Logger is placed below favicon, so favicon.ico
-// requests will not be logged
-app.use(express.logger('":method :url" :status'));
+app.use(express.logger('dev'));
 
 // "app.router" positions our routes 
-// specifically above the middleware
-// assigned below
+// above the middleware defined below,
+// this means that Express will attempt
+// to match & call routes _before_ continuing
+// on, at which point we assume it's a 404 because
+// no route has handled the request.
 
 app.use(app.router);
 
@@ -29,11 +25,26 @@ app.use(app.router);
 // middleware use()d, we assume 404, as nothing else
 // responded.
 
+// $ curl http://localhost:3000/notfound
+// $ curl http://localhost:3000/notfound -H "Accept: application/json"
+// $ curl http://localhost:3000/notfound -H "Accept: text/plain"
+
 app.use(function(req, res, next){
-  // the status option, or res.statusCode = 404
-  // are equivalent, however with the option we
-  // get the "status" local available as well
-  res.render('404', { status: 404, url: req.url });
+  // respond with html page
+  if (req.accepts('html')) {
+    res.status(404);
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
 });
 
 // error-handling middleware, take the same form
@@ -48,15 +59,12 @@ app.use(function(req, res, next){
 // would remain being executed, however here
 // we simply respond with an error page.
 
-
 app.use(function(err, req, res, next){
   // we may use properties of the error object
   // here and next(err) appropriately, or if
   // we possibly recovered from the error, simply next().
-  res.render('500', {
-      status: err.status || 500
-    , error: err
-  });
+  res.status(err.status || 500);
+  res.render('500', { error: err });
 });
 
 // Routes
