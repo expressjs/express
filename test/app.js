17a831e32fb0906e08623e60a0eeb232d9603cfd
Tj Holowaychuk <tj@vision-media.ca>
2011-12-06 15:36:44 -0800

    app.path()

--- test/app.js
+++ test/app.js
@@ -1,10 +1,56 @@
 
-var express = require('../');
+var express = require('../')
+  , assert = require('assert');
 
 describe('app', function(){
   it('should inherit from event emitter', function(done){
     var app = express();
     app.on('foo', done);
     app.emit('foo');
+  })
+})
+
+describe('app.parent', function(){
+  it('should return the parent when mounted', function(){
+    var app = express()
+      , blog = express()
+      , blogAdmin = express();
+
+    app.use('/blog', blog);
+    blog.use('/admin', blogAdmin);
+
+    assert(!app.parent, 'app.parent');
+    blog.parent.should.equal(app);
+    blogAdmin.parent.should.equal(blog);
+  })
+})
+
+describe('app.route', function(){
+  it('should return the mounted path', function(){
+    var app = express()
+      , blog = express()
+      , blogAdmin = express();
+
+    app.use('/blog', blog);
+    blog.use('/admin', blogAdmin);
+
+    app.route.should.equal('/');
+    blog.route.should.equal('/blog');
+    blogAdmin.route.should.equal('/admin');
+  })
+})
+
+describe('app.path()', function(){
+  it('should return the canonical', function(){
+    var app = express()
+      , blog = express()
+      , blogAdmin = express();
+
+    app.use('/blog', blog);
+    blog.use('/admin', blogAdmin);
+
+    app.path().should.equal('');
+    blog.path().should.equal('/blog');
+    blogAdmin.path().should.equal('/blog/admin');
   })
 })
