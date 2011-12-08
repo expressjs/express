5757f875f2e92ba2007f457793b6b69fe182337d
Tj Holowaychuk <tj@vision-media.ca>
2011-12-08 12:45:46 -0800

    Added X-Forwarded-Proto support to `res.redirect()`. Closes #927

--- test/res.redirect.js
+++ test/res.redirect.js
@@ -4,6 +4,24 @@ var express = require('../')
 
 describe('res', function(){
   describe('.redirect(url)', function(){
+    it('should respect X-Forwarded-Proto', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.redirect('/login');
+      });
+
+      request(app)
+      .get('/')
+      .set('Host', 'example.com')
+      .set('X-Forwarded-Proto', 'https')
+      .end(function(res){
+        res.statusCode.should.equal(302);
+        res.headers.should.have.property('location', 'https://example.com/login');
+        done();
+      })
+    })
+
     it('should default to a 302 redirect', function(done){
       var app = express();
 
