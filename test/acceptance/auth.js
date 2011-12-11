4c520464d0998de622cc7f8423efa44c1a0aae8a
Robert Sköld <robert@publicclass.se>
2011-12-11 17:51:28 +0100

    Refactored acceptence/auth.

--- test/acceptance/auth.js
+++ test/acceptance/auth.js
@@ -1,40 +1,37 @@
 var app = require('../../examples/auth/app')
   , request = require('../support/http');
 
+function redirects(to,fn){
+  return function(res){
+    res.statusCode.should.equal(302)
+    res.headers.should.have.property('location').match(to);
+    fn()
+  }
+}
+
 describe('auth', function(){
   var cookie;
   describe('GET /',function(){
     it('should redirect to /login', function(done){
       request(app)
         .get('/')
-        .end(function(res){
-          res.statusCode.should.equal(302);
-          res.headers.should.have.property('location').match(/\/login$/);
-          done();
-        })
+        .end(redirects(/\/login$/,done))
     })
   })
   describe('GET /restricted (w/o cookie)',function(){
     it('should redirect to /login', function(done){
       request(app)
         .get('/restricted')
-        .end(function(res){
-          res.statusCode.should.equal(302);
-          res.headers.should.have.property('location').match(/\/login$/);
-          done();
-        })
+        .end(redirects(/\/login$/,done))
     })
   })
   describe('POST /login', function(){
     it('should fail without proper credentials', function(done){
       request(app)
         .post('/login')
+        .set('content-type','application/x-www-form-urlencoded')
         .write('&username=not-tj&password=foobar')
-        .end(function(res){
-          res.statusCode.should.equal(302);
-          res.headers.should.have.property('location').match(/\/login$/);
-          done();
-        })
+        .end(redirects(/\/login$/,done))
     })
     it('should authenticate', function(done){
       request(app)
