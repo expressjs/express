a12a9e3fd98a55e036d3a6a39bdb6787164b6b79
TJ Holowaychuk <tj@vision-media.ca>
2011-12-28 12:03:50 -0700

    tests for Router#match()

--- test/Router.js
+++ test/Router.js
@@ -1,7 +1,8 @@
 
 var express = require('../')
   , Router = express.Router
-  , request = require('./support/http');
+  , request = require('./support/http')
+  , assert = require('assert');
 
 describe('Router', function(){
   var router, app;
@@ -9,6 +10,30 @@ describe('Router', function(){
   beforeEach(function(){
     router = new Router;
     app = express();
+  })
+
+  describe('.match(req, i)', function(){
+    it('should match based on index', function(){
+      router.route('get', '/foo', function(){});
+      router.route('get', '/foob?', function(){});
+      router.route('get', '/bar', function(){});
+      var req = { method: 'GET', url: '/foo?bar=baz' };
+
+      var route = router.match(req, 0);
+      route.constructor.name.should.equal('Route');
+      route.method.should.equal('get');
+      route.path.should.equal('/foo');
+
+      var route = router.match(req, 1);
+      route.path.should.equal('/foob?');
+
+      var route = router.match(req, 2);
+      assert(!route);
+
+      req.url = '/bar';
+      var route = router.match(req);
+      route.path.should.equal('/bar');
+    })
   })
 
   describe('.middleware', function(){
