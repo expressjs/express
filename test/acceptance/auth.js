ee290d82d1e3d80557597ca9e157c02e20988324
TJ Holowaychuk <tj@vision-media.ca>
2012-02-18 14:04:11 -0800

    fixing acceptance tests

--- test/acceptance/auth.js
+++ test/acceptance/auth.js
@@ -9,8 +9,13 @@ function redirects(to,fn){
   }
 }
 
+function getCookie(res) {
+  return res.headers['set-cookie'][0].split(';')[0];
+}
+
 describe('auth', function(){
   var cookie;
+
   describe('GET /',function(){
     it('should redirect to /login', function(done){
       request(app)
@@ -18,6 +23,7 @@ describe('auth', function(){
         .end(redirects(/\/login$/,done))
     })
   })
+
   describe('GET /restricted (w/o cookie)',function(){
     it('should redirect to /login', function(done){
       request(app)
@@ -25,6 +31,7 @@ describe('auth', function(){
         .end(redirects(/\/login$/,done))
     })
   })
+
   describe('POST /login', function(){
     it('should fail without proper credentials', function(done){
       request(app)
@@ -33,28 +40,35 @@ describe('auth', function(){
         .write('&username=not-tj&password=foobar')
         .end(redirects(/\/login$/,done))
     })
+
     it('should authenticate', function(done){
       request(app)
         .post('/login')
-        .set('content-type','application/x-www-form-urlencoded')
-        .write('&username=tj&password=foobar')
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .write('username=tj&password=foobar')
         .end(function(res){
           res.statusCode.should.equal(302);
-          res.headers.should.have.property('location').match(/\/restricted$/);
-          cookie = res.headers['set-cookie'][0].split(';')[0];
-          cookie.should.match(/^connect.sid=/)
-          done();
+          cookie = getCookie(res);
+          request(app)
+            .get('/login')
+            .set('Cookie', cookie)
+            .end(function(res){
+              res.body.should.include('Authenticated as tj');
+              done();
+            })
         })
     })
   })
+
   describe('GET /restricted (w. cookie)',function(){
-    it('should respond with 200',function(done){
+    it('should respond with 200', function(done){
       request(app)
         .get('/restricted')
-        .set('cookie',cookie)
-        .expect(200,done)
+        .set('Cookie', cookie)
+        .expect(200, done);
     })
   })
+
   describe('GET /logout',function(){
     it('should respond with 302 and clear cookie',function(done){
       request(app)
