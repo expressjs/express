eb3105ef25483b227b25c7cc36b6a87ad9e0dc69
TJ Holowaychuk <tj@vision-media.ca>
2012-02-23 10:58:06 -0800

    Added extname support to `res.format()`. Closes #1024

--- test/res.format.js
+++ test/res.format.js
@@ -26,55 +26,79 @@ app.use(function(req, res, next){
 });
 
 app.use(function(err, req, res, next){
-  res.send(406, 'Supports: ' + err.types.join(', '));
+  res.send(err.status, 'Supports: ' + err.types.join(', '));
+})
+
+var app2 = express();
+
+app2.use(function(req, res, next){
+  res.format({
+    text: function(){ res.send('hey') },
+    html: function(){ res.send('<p>hey</p>') },
+    json: function(){ res.send({ message: 'hey' }) }
+  });
+});
+
+app2.use(function(err, req, res, next){
+  res.send(err.status, 'Supports: ' + err.types.join(', '));
 })
 
 describe('req', function(){
   describe('.format(obj)', function(){
-    it('should utilize qvalues in negotiation', function(done){
-      request(app)
-      .get('/')
-      .set('Accept', 'text/html; q=.5, application/json, */*; q=.1')
-      .expect('{"message":"hey"}', done);
+    describe('with canonicalized mime types', function(){
+      test(app);
     })
 
-    it('should allow wildcard type/subtypes', function(done){
-      request(app)
-      .get('/')
-      .set('Accept', 'text/html; q=.5, application/*, */*; q=.1')
-      .expect('{"message":"hey"}', done);
-    })
-
-    it('should default the Content-Type', function(done){
-      request(app)
-      .get('/')
-      .set('Accept', 'text/html; q=.5, text/plain')
-      .end(function(res){
-        res.headers['content-type'].should.equal('text/plain');
-        res.body.should.equal('hey');
-        done();
-      });
-    })
-
-    describe('when Accept is not present', function(){
-      it('should invoke the first callback', function(done){
-        request(app)
-        .get('/')
-        .expect('hey', done);
-      })
-    })
-
-    describe('when no match is made', function(){
-      it('should should respond with 406 not acceptable', function(done){
-        request(app)
-        .get('/')
-        .set('Accept', 'foo/bar')
-        .end(function(res){
-          res.should.have.status(406);
-          res.body.should.equal('Supports: text/plain, text/html, application/json');
-          done();
-        });
-      })
+    describe('with extnames', function(){
+      test(app2);
     })
   })
 })
+
+function test(app) {
+  it('should utilize qvalues in negotiation', function(done){
+    request(app)
+    .get('/')
+    .set('Accept', 'text/html; q=.5, application/json, */*; q=.1')
+    .expect('{"message":"hey"}', done);
+  })
+
+  it('should allow wildcard type/subtypes', function(done){
+    request(app)
+    .get('/')
+    .set('Accept', 'text/html; q=.5, application/*, */*; q=.1')
+    .expect('{"message":"hey"}', done);
+  })
+
+  it('should default the Content-Type', function(done){
+    request(app)
+    .get('/')
+    .set('Accept', 'text/html; q=.5, text/plain')
+    .end(function(res){
+      res.headers['content-type'].should.equal('text/plain');
+      res.body.should.equal('hey');
+      done();
+    });
+  })
+
+  describe('when Accept is not present', function(){
+    it('should invoke the first callback', function(done){
+      request(app)
+      .get('/')
+      .expect('hey', done);
+    })
+  })
+
+  describe('when no match is made', function(){
+    it('should should respond with 406 not acceptable', function(done){
+      request(app)
+      .get('/')
+      .set('Accept', 'foo/bar')
+      .end(function(res){
+        res.should.have.status(406);
+        res.body.should.equal('Supports: text/plain, text/html, application/json');
+        done();
+      });
+    })
+  })
+}
