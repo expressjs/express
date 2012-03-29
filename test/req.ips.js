dd468fbe9aae9d69ddf629764a424caa828f6289
TJ Holowaychuk <tj@vision-media.ca>
2012-03-29 16:26:59 -0700

    Added `req.ips`

---
+++ test/req.ips.js
@@ -0,0 +1,40 @@
+
+var express = require('../')
+  , request = require('./support/http');
+
+describe('req', function(){
+  describe('.ips', function(){
+    describe('when X-Forwarded-For is present', function(){
+      it('should return an array of the specified addresses', function(done){
+        var app = express();
+
+        app.enable('trust proxy');
+
+        app.use(function(req, res, next){
+          res.send(req.ips);
+        });
+
+        request(app)
+        .get('/')
+        .set('X-Forwarded-For', 'client, proxy')
+        .expect('["client","proxy"]', done);
+      })
+    })
+
+    describe('when X-Forwarded-For is not present', function(){
+      it('should return []', function(done){
+        var app = express();
+
+        app.enable('trust proxy');
+
+        app.use(function(req, res, next){
+          res.send(req.ips);
+        });
+
+        request(app)
+        .get('/')
+        .expect('[]', done);
+      })
+    })
+  })
+})
