c33f1bac089165128ad24c499aa13ffcc14f08de
TJ Holowaychuk <tj@vision-media.ca>
2012-04-26 03:24:42 -0700

    mounted hw bench

--- support/app.js
+++ support/app.js
@@ -5,7 +5,14 @@
 
 var express = require('../');
 
-var app = express.createServer();
+var app = express()
+  , blog = express();
+
+app.use('/blog', blog);
+
+blog.get('/', function(req, res){
+  res.send('Hello World\n');
+});
 
 app.get('/', function(req, res){
   res.send('Hello World\n');
