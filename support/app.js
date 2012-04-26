a169cc7119e6aaa448ea095a61884b82fde7f43c
TJ Holowaychuk <tj@vision-media.ca>
2012-04-26 12:36:26 -0700

    render bench

--- support/app.js
+++ support/app.js
@@ -9,6 +9,13 @@ var app = express()
   , blog = express();
 
 app.use('/blog', blog);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.locals.self = true;
+
+app.get('/render', function(req, res){
+  res.render('hello');
+});
 
 blog.get('/', function(req, res){
   res.send('Hello World\n');
