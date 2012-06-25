18d6c78ef4027b1a03b47c34a93267c094422e3c
TJ Holowaychuk <tj@vision-media.ca>
2012-06-25 12:42:51 -0700

    fixed a test race condition

--- test/res.sendfile.js
+++ test/res.sendfile.js
@@ -6,22 +6,19 @@ var express = require('../')
 describe('res', function(){
   describe('.sendfile(path, fn)', function(){
     it('should invoke the callback when complete', function(done){
-      var app = express()
-        , calls = 0;
+      var app = express();
 
       app.use(function(req, res){
         res.sendfile('test/fixtures/user.html', function(err){
           assert(!err);
-          ++calls;
+          done();
         });
       });
 
       request(app)
       .get('/')
       .end(function(res){
-        calls.should.equal(1);
-        res.statusCode.should.equal(200);
-        done();
+        res.should.have.status(200);
       });
     })
 
