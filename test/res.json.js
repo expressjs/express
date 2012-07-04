43ad1857b0c132e2e6278d5760b028a89e3242b2
riadh <riadh@riadh-K70ID>
2012-07-04 22:39:41 +0200

    issue #1190 : renaming jsonp callback

--- test/res.json.js
+++ test/res.json.js
@@ -22,6 +22,23 @@ describe('res', function(){
         })
       })
 
+      it('should allow renaming callback', function(done){
+        var app = express();
+        
+        app.set('jsonp callback name', 'clb');
+        app.use(function(req, res){
+          res.json({ count: 1 });
+        });
+
+        request(app)
+        .get('/?clb=something')
+        .end(function(err, res){
+          res.headers.should.have.property('content-type', 'text/javascript; charset=utf-8');
+          res.text.should.equal('something({"count":1});');
+          done();
+        })
+      })      
+      
       it('should allow []', function(done){
         var app = express();
 
