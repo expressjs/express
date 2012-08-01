0f20a5e06af75c07ab92b3a2266e4021acd053d1
TJ Holowaychuk <tj@vision-media.ca>
2012-08-01 13:17:46 -0700

    add CORS example

---
+++ examples/cors/index.js
@@ -0,0 +1,45 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('../..')
+  , app = express()
+  , api = express();
+
+// app middleware
+
+app.use(express.static(__dirname + '/public'));
+
+// api middleware
+
+api.use(express.logger('dev'));
+api.use(express.bodyParser());
+
+/**
+ * OPTIONS for all routes.
+ */
+
+api.all('*', function(req, res, next){
+  // use "*" here to accept any origin
+  res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.set('Access-Control-Allow-Methods', 'GET, POST');
+  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  // res.set('Access-Control-Allow-Max-Age', 3600);
+  next();
+});
+
+/**
+ * POST a user.
+ */
+
+api.post('/user', function(req, res){
+  console.log(req.body);
+  res.send(201);
+});
+
+app.listen(3000);
+api.listen(3001);
+
+console.log('app listening on 3000');
+console.log('api listening on 3001');
