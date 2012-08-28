f7c1c638bbe79aa9dd3e8e329af164dbe06859c1
TJ Holowaychuk <tj@vision-media.ca>
2012-08-28 10:28:58 -0700

    add some comments to error-pages example

--- examples/error-pages/index.js
+++ examples/error-pages/index.js
@@ -76,16 +76,21 @@ app.get('/', function(req, res){
 });
 
 app.get('/404', function(req, res, next){
+  // trigger a 404 since no other middleware
+  // will match /404 after this one, and we're not
+  // responding here
   next();
 });
 
 app.get('/403', function(req, res, next){
+  // trigger a 403 error
   var err = new Error('not allowed!');
   err.status = 403;
   next(err);
 });
 
 app.get('/500', function(req, res, next){
+  // trigger a generic (500) error
   next(new Error('keyboard cat!'));
 });
 
