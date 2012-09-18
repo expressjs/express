315e79e9f0bb5ce7aec8334ebb916d7e2842f8af
TJ Holowaychuk <tj@vision-media.ca>
2012-09-18 09:31:12 -0700

    add redis search example

---
+++ examples/search/client.js
@@ -0,0 +1,14 @@
+
+var search = document.querySelector('[type=search]');
+var code = document.querySelector('pre');
+
+search.addEventListener('keyup', function(){
+  var xhr = new XMLHttpRequest;
+  xhr.open('GET', '/search/' + search.value, true);
+  xhr.onreadystatechange = function(){
+    if (4 == xhr.readyState) {
+      code.textContent = xhr.responseText;
+    }
+  };
+  xhr.send();
+}, false);
