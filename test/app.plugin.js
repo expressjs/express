625c10d6bea0a866c8e4e2a0cf2525aa1f6a1152
Jan Buschtöns <buschtoens@gmail.com>
2012-12-03 14:41:10 +0100

    Add app.plugin(fn, param...) test

---
+++ test/app.plugin.js
@@ -0,0 +1,20 @@
+
+var express = require('../')
+  , request = require('./support/http');
+
+describe('app', function(){
+  describe('.plugin(plugin, param...)', function(){
+    it('should execute the plugin fn with this and param...', function(){
+      var app = express()
+        , plugin = function plugin(param0, param1, param2) {
+	        	this.should.equal(app);
+	        	param0.should.eql(params[0]);
+	        	param0.should.eql(params[1]);
+	        	param0.should.eql(params[2]);
+	        }
+	      , params = ["param0", "param1", "param2"];
+
+      app.plugin(plugin, params[0], params[1], params[2]);
+    })
+  })
+})
