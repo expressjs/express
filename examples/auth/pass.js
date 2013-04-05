ecc2691ec0b4bef61907cd3982dcc71585cca4c1
Josh Lubawy <jlubawy@gmail.com>
2013-04-04 23:26:27 -0700

    Modified hash to return base64 encoded strings.

--- examples/auth/pass.js
+++ examples/auth/pass.js
@@ -31,7 +31,9 @@ var iterations = 12000;
 
 exports.hash = function (pwd, salt, fn) {
   if (3 == arguments.length) {
-    crypto.pbkdf2(pwd, salt, iterations, len, fn);
+    crypto.pbkdf2(pwd, salt, iterations, len, function(err, hash){
+      fn(err, (new Buffer(hash, 'binary')).toString('base64'));
+    });
   } else {
     fn = salt;
     crypto.randomBytes(len, function(err, salt){
@@ -39,7 +41,7 @@ exports.hash = function (pwd, salt, fn) {
       salt = salt.toString('base64');
       crypto.pbkdf2(pwd, salt, iterations, len, function(err, hash){
         if (err) return fn(err);
-        fn(null, salt, hash);
+        fn(null, salt, (new Buffer(hash, 'binary')).toString('base64'));
       });
     });
   }
