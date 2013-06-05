7a4f4520a8a7a10dcafdf41d803b05d7ad7a9214
TJ Holowaychuk <tj@vision-media.ca>
2013-06-05 12:05:45 -0700

    add support for multiple X-Forwarded-Proto values. Closes #1646

--- test/req.secure.js
+++ test/req.secure.js
@@ -48,6 +48,36 @@ describe('req', function(){
         .set('X-Forwarded-Proto', 'https')
         .expect('yes', done)
       })
+
+      it('should return false when initial proxy is http', function(done){
+        var app = express();
+
+        app.enable('trust proxy');
+
+        app.get('/', function(req, res){
+          res.send(req.secure ? 'yes' : 'no');
+        });
+
+        request(app)
+        .get('/')
+        .set('X-Forwarded-Proto', 'http, https')
+        .expect('no', done)
+      })
+
+      it('should return true when initial proxy is https', function(done){
+        var app = express();
+
+        app.enable('trust proxy');
+
+        app.get('/', function(req, res){
+          res.send(req.secure ? 'yes' : 'no');
+        });
+
+        request(app)
+        .get('/')
+        .set('X-Forwarded-Proto', 'https, http')
+        .expect('yes', done)
+      })
     })
   })
 })
