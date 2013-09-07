b97dd8b711857f3b98fa4a9c18cea8641fbe6941
Eduardo Sorribas <eduardo@sorribas.org>
2013-09-06 21:44:03 -0400

    Allow multiple call concatenation for res.links. Fixes #1683

--- test/res.links.js
+++ test/res.links.js
@@ -15,5 +15,18 @@ describe('res', function(){
           '<http://api.example.com/users?page=2>; rel="next", '
         + '<http://api.example.com/users?page=5>; rel="last"');
     })
+
+    it('should set Link header field for multiple calls', function() {
+      // just one call here, because of the call on the first test
+      res.links({
+        prev: 'http://api.example.com/users?page=1',
+      });
+
+      res.get('link')
+      .should.equal(
+          '<http://api.example.com/users?page=2>; rel="next", '
+        + '<http://api.example.com/users?page=5>; rel="last", '
+        + '<http://api.example.com/users?page=1>; rel="prev"');
+    })
   })
 })
