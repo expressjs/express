ab8be2d741543c15452aa088a6ec143a9ccc0026
TJ Holowaychuk <tj@vision-media.ca>
2013-09-16 23:33:42 +0000

    remove second signed cookie test
    for now

--- test/req.signedCookies.js
+++ test/req.signedCookies.js
@@ -34,30 +34,6 @@ describe('req', function(){
         });
       });
     })
-
-    it('should return a signed cookie', function(done){
-      var app = express();
-
-      var replacer = app.get('json replacer');
-      var spaces = app.get('json spaces');
-
-      app.use(express.cookieParser('secret'));
-
-      app.use(function(req, res){
-        res.send(req.signedCookies);
-      });
-
-      app.response.req = { secret: 'secret' };
-      app.response.cookie('foo', 'bar', { signed: true });
-      var cookie = app.response.get('set-cookie');
-
-      var val = JSON.stringify({ foo: 'bar' }, replacer, spaces);
-      
-      request(app)
-      .get('/')
-      .set('Cookie', cookie)
-      .expect(val, done);
-    })
   })
 })
 
