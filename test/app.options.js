1d97599f8b2f4ea6be0b7082ac1d9d93c8d00665
Doug Patti <douglas.patti@gmail.com>
2014-01-02 13:05:23 -0500

    prevent incorrect automatic OPTIONS responses
    The router has automatic handling of OPTIONS based on the registered
    routes, but if you make an OPTIONS request for an endpoint that does
    not exist, then it will still return a 200 with nothing allowed.
    Instead, we can let the request move on down the middleware chain. This
    has two benefits: first, if the route was not defined and no other
    middleware handles it, it will return with a 404. Secondly, if multiple
    routers are used and a later one has the route or a custom OPTIONS
    defined, the first router will not respond incorrectly.

--- test/app.options.js
+++ test/app.options.js
@@ -15,6 +15,30 @@ describe('OPTIONS', function(){
     .expect('GET,PUT')
     .expect('Allow', 'GET,PUT', done);
   })
+
+  it('should not respond if the path is not defined', function(done){
+    var app = express();
+
+    app.get('/users', function(req, res){});
+
+    request(app)
+    .options('/other')
+    .expect(404, done);
+  })
+
+  it('should forward requests down the middleware chain', function(done){
+    var app = express();
+    var router = new express.Router();
+
+    router.get('/users', function(req, res){});
+    app.use(router.middleware);
+    app.get('/other', function(req, res){});
+
+    request(app)
+    .options('/other')
+    .expect('GET')
+    .expect('Allow', 'GET', done);
+  })
 })
 
 describe('app.options()', function(){
