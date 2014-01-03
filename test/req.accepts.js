cec0c06a70c874298761282984573475293c0071
Jonathan Ong <jonathanrichardong@gmail.com>
2014-01-03 02:50:09 -0800

    refactor req.is and req.accepts*

--- test/req.accepts.js
+++ test/req.accepts.js
@@ -43,19 +43,6 @@ describe('req', function(){
     })
   })
 
-  it('should accept a comma-delimited list of types', function(done){
-    var app = express();
-
-    app.use(function(req, res, next){
-      res.end(req.accepts('json, html'));
-    });
-
-    request(app)
-    .get('/')
-    .set('Accept', 'text/html')
-    .expect('html', done);
-  })
-
   it('should accept an argument list of type names', function(done){
     var app = express();
 
--- test/req.acceptsCharset.js
+++ test/req.acceptsCharset.js
@@ -9,7 +9,7 @@ describe('req', function(){
         var app = express();
 
         app.use(function(req, res, next){
-          res.end(req.acceptsCharset('utf-8') ? 'yes' : 'no');
+          res.end(req.acceptsCharsets('utf-8') ? 'yes' : 'no');
         });
 
         request(app)
@@ -23,7 +23,7 @@ describe('req', function(){
         var app = express();
 
         app.use(function(req, res, next){
-          res.end(req.acceptsCharset('utf-8') ? 'yes' : 'no');
+          res.end(req.acceptsCharsets('utf-8') ? 'yes' : 'no');
         });
 
         request(app)
@@ -36,7 +36,7 @@ describe('req', function(){
         var app = express();
 
         app.use(function(req, res, next){
-          res.end(req.acceptsCharset('utf-8') ? 'yes' : 'no');
+          res.end(req.acceptsCharsets('utf-8') ? 'yes' : 'no');
         });
 
         request(app)
