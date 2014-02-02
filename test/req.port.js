d9a3857fe569cfb56a1a3d55dc378f825de36135
Hage Yaapa <captain@hacksparrow.com>
2014-02-02 21:58:03 +0530

    missed some stuff

--- test/req.port.js
+++ test/req.port.js
@@ -10,7 +10,7 @@ describe('req', function(){
       var app = express();
 
       app.use(function(req, res){
-        res.end(req.host);
+        res.end(String(req.port));
       });
 
       request(app)
@@ -23,7 +23,7 @@ describe('req', function(){
       var app = express();
 
       app.use(function(req, res){
-        res.end(req.host);
+        res.end(String(req.port));
       });
 
       request(app)
@@ -37,7 +37,7 @@ describe('req', function(){
 
       app.use(function(req, res){
         req.headers.host = null;
-        res.end(String(req.host));
+        res.end(String(req.port));
       });
 
       request(app)
