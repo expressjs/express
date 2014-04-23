ce17efd95babe62369e73fc843214bc94f1ecd37
Fernando Silveira <fsilveira@gmail.com>
2014-04-22 23:37:49 -0300

    MVC example: EJS -> Jade.

--- examples/mvc/controllers/pet/index.js
+++ examples/mvc/controllers/pet/index.js
@@ -4,8 +4,6 @@
 
 var db = require('../../db');
 
-exports.engine = 'jade';
-
 exports.before = function(req, res, next){
   var pet = db.pets[req.params.pet_id];
   if (!pet) return next(new Error('Pet not found'));
