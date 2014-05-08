d136774d5789102ece45e6c8918aa37738190de0
Peter Conerly <pconerly@gmail.com>
2014-05-08 15:38:59 -0700

    3.x maintain order of json|jsonp(status,object) when both are numbers

--- test/res.jsonp.js
+++ test/res.jsonp.js
@@ -264,5 +264,22 @@ describe('res', function(){
         done();
       })
     })
+
+    it('should default to .jsonp(status, object) when when both properties are integerss', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.jsonp(201, 0);
+      });
+
+      request(app)
+      .get('/')
+      .end(function(err, res){
+        res.statusCode.should.equal(201);
+        res.headers.should.have.property('content-type', 'application/json');
+        res.text.should.equal('0');
+        done();
+      })
+    })
   })
 })
