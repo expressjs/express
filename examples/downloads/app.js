3db6dd752f51748c5338c8ac1741abd3dfe07472
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-06-06 10:23:47 -0400

    change confusing 404 handling in download example

--- examples/downloads/app.js
+++ examples/downloads/app.js
@@ -19,23 +19,13 @@ app.get('/files/:file(*)', function(req, res, next){
   var file = req.params.file;
   var path = __dirname + '/files/' + file;
 
-  res.download(path);
-});
-
-// error handling middleware. Because it's
-// below our routes, you will be able to
-// "intercept" errors, otherwise Connect
-// will respond with 500 "Internal Server Error".
-app.use(function(err, req, res, next){
-  // special-case 404s,
-  // remember you could
-  // render a 404 template here
-  if (404 == err.status) {
+  res.download(path, function(err){
+    if (!err) return; // file sent
+    if (err && err.status !== 404) return next(err); // non-404 error
+    // file for download not found
     res.statusCode = 404;
     res.send('Cant find that file, sorry!');
-  } else {
-    next(err);
-  }
+  });
 });
 
 /* istanbul ignore next */
