75d5e30d3d69d558f8456bb7b87412c58a897025
Scott Gardner <sgardn04@gmail.com>
2014-09-02 13:37:59 -0400

    adding methodOverride to examples/route-separation/index.js

--- examples/route-separation/index.js
+++ examples/route-separation/index.js
@@ -7,6 +7,7 @@ var app = express();
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var site = require('./site');
 var post = require('./post');
 var user = require('./user');
