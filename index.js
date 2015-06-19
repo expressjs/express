95ad276cad3369bf5f7253d0110a6e04425bd32a
Douglas Christopher Wilson <doug@somethingdoug.com>
2015-06-18 23:00:20 -0400

    docs: add license comments

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+/*!
+ * express
+ * Copyright(c) 2009-2013 TJ Holowaychuk
+ * Copyright(c) 2013 Roman Shtylman
+ * Copyright(c) 2014-2015 Douglas Christopher Wilson
+ * MIT Licensed
+ */
 
 module.exports = require('./lib/express');
