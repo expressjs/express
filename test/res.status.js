c32439ee5277089b0e0fb36642f7b0f8a3cfc53f
Joshua Caron <joshua@joshuacaron.ca>
2015-11-12 13:33:06 -0500

    Throw TypeError if argument to res.status is null or undefined; fixes #2795.

--- test/res.status.js
+++ test/res.status.js
@@ -16,5 +16,37 @@ describe('res', function(){
       .expect('Created')
       .expect(201, done);
     })
+
+    it('should throw a TypeError if code is undefined', function(done){
+      var app = express();
+      app.use(function(req, res){
+        res.status(undefined).end('Created');
+      });
+
+      request(app)
+      .get('/')
+      .expect(invalidCode)
+      .expect(500, done);
+    })
+
+    it('should throw a TypeError if code is null', function(done){
+      var app = express();
+      app.use(function(req, res){
+        res.status(null).end('Created');
+      });
+
+      request(app)
+      .get('/')
+      .expect(invalidCode)
+      .expect(500, done);
+    })
+
   })
 })
+
+function invalidCode(res) {
+  if (!(res.error.text.match(/TypeError/ && res.error.text.match(/status code/)))) {
+    throw new Error('Expected a TypeError for invalid status code.');
+  }
+}
+
