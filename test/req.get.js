0dfd1601e2c019043307752ce17f1a9fcefe4af1
Alvaro Paçó <alvaropaco@tokenlab.com.br>
2016-05-10 20:12:06 -0300

    Setting TypeError to string header validation

--- test/req.get.js
+++ test/req.get.js
@@ -3,6 +3,10 @@ var express = require('../')
   , request = require('supertest')
   , assert = require('assert');
 
+function typeThrowError(name) {
+    new TypeError('Parameter "name" needs to be a String but got a ' + (typeof name));
+}
+
 describe('req', function(){
   describe('.get(field)', function(){
     it('should return the header field value', function(done){
@@ -10,6 +14,12 @@ describe('req', function(){
 
       app.use(function(req, res){
         assert(req.get('Something-Else') === undefined);
+        res.end(req.get('Content-Type'));
+      });
+      
+      app.use(function(req, res){
+        var name = ['something'];
+        assert.throws(typeThrowError(name), TypeError, "Error thrown");
         res.end(req.get('Content-Type'));
       });
 
