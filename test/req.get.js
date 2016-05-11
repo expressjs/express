2cdd470781d2384ef0ddaaae369b5d00eaf8f223
Alvaro Paçó <alvaropaco@tokenlab.com.br>
2016-05-10 21:21:40 -0300

    Fixing the test case for throw error on set heard

--- test/req.get.js
+++ test/req.get.js
@@ -3,10 +3,6 @@ var express = require('../')
   , request = require('supertest')
   , assert = require('assert');
 
-function typeThrowError(name) {
-    new TypeError('Parameter "name" needs to be a String but got a ' + (typeof name));
-}
-
 describe('req', function(){
   describe('.get(field)', function(){
     it('should return the header field value', function(done){
@@ -16,17 +12,24 @@ describe('req', function(){
         assert(req.get('Something-Else') === undefined);
         res.end(req.get('Content-Type'));
       });
-      
-      app.use(function(req, res){
-        var name = ['something'];
-        assert.throws(typeThrowError(name), TypeError, "Error thrown");
-        res.end(req.get('Content-Type'));
-      });
 
       request(app)
       .post('/')
       .set('Content-Type', 'application/json')
       .expect('application/json', done);
+    })
+    
+    it('should handle render error throws', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.end(req.get(['Referer']));
+      });
+
+      request(app)
+      .post('/')
+      .set(['Referrer'], 'http://foobar.com')
+      .expect(500, done);
     })
 
     it('should special-case Referer', function(done){
