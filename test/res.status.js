61d4afed05efdeb3faf3c3a3f31818163e9c3d79
Nicholas Clawson <nickclaw@gmail.com>
2016-11-23 11:23:26 -0800

    Gracefully handle invalid status codes

--- test/res.status.js
+++ test/res.status.js
@@ -16,5 +16,41 @@ describe('res', function(){
       .expect('Created')
       .expect(201, done);
     })
+
+    it('should throw a TypeError if invalid', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.status(10000).end();
+      });
+
+      request(app)
+      .get('/')
+      .expect(500, /Invalid status code/, done);
+    })
+
+    it('should handle numeric strings', function(done) {
+      var app = express();
+
+      app.use(function(req, res){
+        res.status('400').end();
+      });
+
+      request(app)
+      .get('/')
+      .expect(400, done);
+    })
+
+    it('should handle floats as integers', function(done) {
+      var app = express();
+
+      app.use(function(req, res){
+        res.status(404.04).end();
+      });
+
+      request(app)
+      .get('/')
+      .expect(404, done);
+    })
   })
 })
