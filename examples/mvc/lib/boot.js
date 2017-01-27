6b506d801a731eb079fc0f442d25a7398b5f1a5b
Douglas Christopher Wilson <doug@somethingdoug.com>
2017-01-26 22:33:52 -0500

    examples: fix mvc example to ignore files in controllers dir
    fixes #3182

--- examples/mvc/lib/boot.js
+++ examples/mvc/lib/boot.js
@@ -8,6 +8,7 @@ var fs = require('fs');
 module.exports = function(parent, options){
   var verbose = options.verbose;
   fs.readdirSync(__dirname + '/../controllers').forEach(function(name){
+    if (!fs.statSync(__dirname + '/../controllers/' + name).isDirectory()) return;
     verbose && console.log('\n   %s:', name);
     var obj = require('./../controllers/' + name);
     var name = obj.name || name;
