ee40a881f5d8cb4ce71bc45262fde8e4b7640d05
Wes <wes@wesleytodd.com>
2017-02-20 17:36:39 -0600

    call callback once on listen error

--- test/app.listen.js
+++ test/app.listen.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var express = require('../')
+var assert = require('assert')
 
 describe('app.listen()', function(){
   it('should wrap with an HTTP server', function(done){
@@ -9,5 +10,18 @@ describe('app.listen()', function(){
     var server = app.listen(0, function () {
       server.close(done)
     });
+  })
+  it('should callback on HTTP server errors', function (done) {
+    var app1 = express()
+    var app2 = express()
+
+    var server1 = app1.listen(0, function (err) {
+      assert(!err)
+      app2.listen(server1.address().port, function (err) {
+        assert(err.code === 'EADDRINUSE')
+        server1.close()
+        done()
+      })
+    })
   })
 })
