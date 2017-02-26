57d3dfd9f828c4529fa90dde4b3d216898181e99
Jamie Barton <hi_jamie@me.com>
2017-02-26 13:26:39 +0000

    examples: merge the jade example into ejs
    closes #3223

--- examples/ejs/index.js
+++ examples/ejs/index.js
@@ -24,6 +24,10 @@ app.engine('.html', require('ejs').__express);
 
 app.set('views', path.join(__dirname, 'views'));
 
+// Path to our public directory
+
+app.use(express.static(path.join(__dirname + 'public')));
+
 // Without this you would need to
 // supply the extension to res.render()
 // ex: res.render('users.html').
