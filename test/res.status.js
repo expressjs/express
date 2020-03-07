739bc4713409e1647bbff94b2c2ae104b0c9a927
Jon Church <me@jonchurch.com>
2020-03-07 16:46:30 -0500

    check status code is integer, or string integer, in range

--- test/res.status.js
+++ test/res.status.js
@@ -1,6 +1,8 @@
 
 var express = require('../')
-  , request = require('supertest');
+  , res = require('../lib/response')
+  , request = require('supertest')
+  , assert = require('assert');
 
 describe('res', function(){
   describe('.status(code)', function(){
@@ -15,6 +17,43 @@ describe('res', function(){
       .get('/')
       .expect('Created')
       .expect(201, done);
+    })
+
+    describe('should throw', function() {
+      var InvalidStatusError = new TypeError('Invalid status code')
+
+      it('if status code is < 100 || > 999', function(done) {
+        assert.throws(function() {
+          res.status(99)
+        }, InvalidStatusError, "for value 99")
+        assert.throws(function() {
+          res.status(1000)
+        }, InvalidStatusError, "for value 1000")
+        done()
+      })
+      it('if status code is not an integer', function (done) {
+        var cases = [
+          200.1,
+          '200.1',
+          NaN,
+          Infinity,
+          -Infinity,
+          undefined,
+          null,
+          function () {},
+          true,
+          false,
+        {},
+        [],
+        ]
+        cases.forEach(function (item) {
+          assert.throws(function () {
+            res.status(item)
+          }, InvalidStatusError)
+        })
+        done()
+      })
+
     })
   })
 })
