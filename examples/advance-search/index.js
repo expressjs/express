95187e3a95e8690345276e39b520ce7aabbffe29
Nondukishor <nipuchakraborty86@hotmail.com>
2020-05-30 21:28:50 +0600

    view structure changed for search

--- examples/advance-search/index.js
+++ examples/advance-search/index.js
@@ -10,24 +10,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/',(req,res)=>{
-    res.render('index')
-})
 
-app.get('/search',(req,res)=>{
     var keyword = req.query.search;
-    console.log(keyword)
     var allCountries = countries;
-    console.log(allCountries)
-    if(!keyword) return res.json({results:"Keyword is empty"})
-
-
+    if(!keyword) return res.render('index',{countries: countries})
+    
     if(keyword)
     var results = allCountries.filter(country=>{
         return country.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()) || country.code.toLocaleLowerCase().includes(keyword.toLocaleLowerCase())  
     })
     if(!results) return res.json({results:"Not found"})
-    return res.json({results})
+    
+    return res.render('index',{
+        countries:results
+    })
 })
+
+
 
 
 
