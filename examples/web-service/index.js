21cf522dcd94ac2af6898ca81df02b373362bf24
Aalaap Ghag <aalaap@gmail.com>
2020-11-29 09:41:16 +0530

    examples: improve 404 message wording
    closes #4471

--- examples/web-service/index.js
+++ examples/web-service/index.js
@@ -105,7 +105,7 @@ app.use(function(err, req, res, next){
 // invoke next() and do not respond.
 app.use(function(req, res){
   res.status(404);
-  res.send({ error: "Lame, can't find that" });
+  res.send({ error: "Sorry, can't find that" })
 });
 
 /* istanbul ignore next */
