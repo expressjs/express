5b98b1006413445018185a9667124b0416e9658f
LAPTOP-8PI7N0KD\USER <maakifurrahman@gmail.com>
2021-05-23 19:44:21 +0530

    check for invalid or unknown http status codes.

--- test/res.status.js
+++ test/res.status.js
@@ -16,5 +16,14 @@ describe('res', function(){
       .expect('Created')
       .expect(201, done);
     })
+    it("should throw an error internal server error for invalid status code.", function (done) {
+      var app = express();
+
+      app.use(function (req, res) {
+        res.status(209).end();
+      });
+
+      request(app).get("/").expect(500, done);
+    });
   })
 })
