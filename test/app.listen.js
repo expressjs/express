dab6ee58229f2af3680023021703e7ab5cf9c76f
Juan José Arboleda <soyjuanarbol@gmail.com>
2021-10-02 19:33:41 -0500

    tests: remove extraneous code from app.listen test
    closes #4714

--- test/app.listen.js
+++ test/app.listen.js
@@ -5,10 +5,6 @@ describe('app.listen()', function(){
   it('should wrap with an HTTP server', function(done){
     var app = express();
 
-    app.del('/tobi', function(req, res){
-      res.end('deleted tobi!');
-    });
-
     var server = app.listen(9999, function(){
       server.close();
       done();
