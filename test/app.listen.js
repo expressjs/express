a86bcae2aadc20aef8015b2d58621d5d847f49cd
leonardo <leonardo.fc2@outlook.com>
2022-06-09 00:16:33 -0300

    fix: revert commit

--- test/app.listen.js
+++ test/app.listen.js
@@ -7,7 +7,7 @@ describe('app.listen()', function(){
   it('should wrap with an HTTP server', function(done){
     var app = express();
 
-    var server = app.listen('1200', function(){
+    var server = app.listen(9999, function(){
       server.close();
       done();
     });
