7f5138d71c33f0dad88d1961702762593bf4f6db
leonardo <leonardo.fc2@outlook.com>
2022-06-09 00:11:45 -0300

    test: test new validation

--- test/app.listen.js
+++ test/app.listen.js
@@ -1,14 +1,22 @@
 'use strict'
 
 var express = require('../')
+var assert = require('assert')
 
 describe('app.listen()', function(){
   it('should wrap with an HTTP server', function(done){
     var app = express();
 
-    var server = app.listen(9999, function(){
+    var server = app.listen('1200', function(){
       server.close();
       done();
     });
+  })
+  it('should only accept numbers for port param', function(done){
+    var app = express();
+    assert.throws(function() { app.listen('foo', function(){
+      server.close();
+    }); }, TypeError);
+    done();
   })
 })
