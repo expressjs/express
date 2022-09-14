97bacdd612507f2615141008c24a48b91e4578cb
Yaniv Peretz <yaniv.peretz@gm.com>
2022-09-14 23:45:09 +0300

    lint

--- test/app.close.js
+++ test/app.close.js
@@ -11,6 +11,17 @@ describe("app.close()", function () {
       res.end("hi!");
     });
 
+    app.listen();
+    app.close(null, done);
+  });
+
+  it("Should close a single app instance", function (done) {
+    var app = express();
+
+    app.get("/greet", function (req, res) {
+      res.end("hi!");
+    });
+
     app.listen(9999);
     app.close(null, done);
   });
@@ -24,7 +35,7 @@ describe("app.close()", function () {
 
     app.listen(9998);
     app.listen(9999);
-    let i = 0;
+    var i = 0;
     app.close(null, () => {
       if (++i === 2) done();
     });
