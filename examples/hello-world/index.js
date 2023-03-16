e85c70fc47793f59125a25a550fa8165acd5c515
Shivam Surya <95014732+shivamsurya5@users.noreply.github.com>
2023-03-16 09:22:52 +0530

    upadate

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('Hello World1');
 });
 
 /* istanbul ignore next */
