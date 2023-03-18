b1e8c153314ff20d0b0dbdc3a609b9f2fd4921b0
Abdul8057 <115917722+Abdul8057@users.noreply.github.com>
2023-03-18 19:40:41 +0530

    Update index.js
    reverted to original file

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-'use strict'
-const path = require('path');
-const node_modules = path.join(__dirname, 'node_modules');
-var express = require(path.join(node_modules, 'express'));
+/*!
+ * express
+ * Copyright(c) 2009-2013 TJ Holowaychuk
+ * Copyright(c) 2013 Roman Shtylman
+ * Copyright(c) 2014-2015 Douglas Christopher Wilson
+ * MIT Licensed
+ */
 
-var app = module.exports = express()
+'use strict';
 
-app.get('/', function(req, res){
-  res.send('Hello World');
-});
-
-/* istanbul ignore next */
-if (!module.parent) {
-  app.listen(3000);
-  console.log('Express started on port 3000');
-}
-
+module.exports = require('./lib/express');
