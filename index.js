31362f3114c544637d22c2dac233e960352dee2d
Abdul8057 <115917722+Abdul8057@users.noreply.github.com>
2023-03-18 15:52:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-/*!
- * express
- * Copyright(c) 2009-2013 TJ Holowaychuk
- * Copyright(c) 2013 Roman Shtylman
- * Copyright(c) 2014-2015 Douglas Christopher Wilson
- * MIT Licensed
- */
+'use strict'
+const path = require('path');
+const node_modules = path.join(__dirname, 'node_modules');
+var express = require(path.join(node_modules, 'express'));
 
-'use strict';
+var app = module.exports = express()
 
-module.exports = require('./lib/express');
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
+
