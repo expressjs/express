ceca5beb503694de2dfc4fe9998558a93d734fec
Robert Collins <58490986+robkebab@users.noreply.github.com>
2023-08-29 18:27:27 -0500

    fix no-cond-assign issues

--- test/app.param.js
+++ test/app.param.js
@@ -12,8 +12,8 @@ describe('app', function(){
       app.param(function(name, regexp){
         if (Object.prototype.toString.call(regexp) === '[object RegExp]') { // See #1557
           return function(req, res, next, val){
-            var captures;
-            if (captures = regexp.exec(String(val))) {
+            var captures = regexp.exec(String(val));
+            if (captures) {
               req.params[name] = captures[1];
               next();
             } else {
