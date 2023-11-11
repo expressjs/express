a13e252db4b1b4df4055a04997597e931335786b
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2023-11-11 18:20:05 +0100

    feat: remove examples folder

--- examples/content-negotiation/db.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var users = [];
-
-users.push({ name: 'Tobi' });
-users.push({ name: 'Loki' });
-users.push({ name: 'Jane' });
-
-module.exports = users;
--- examples/markdown/views/index.md
+++
@@ -1,4 +0,0 @@
-
-# {title}
-
-Just an example view rendered with _markdown_.
