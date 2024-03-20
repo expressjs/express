d0c0cadebb3f47747f58def376fb0f60d2f74e67
Jon Church <me@jonchurch.com>
2024-03-20 14:45:59 -0400

    delete the exising test after moving it to describe

--- test/res.location.js
+++ test/res.location.js
@@ -45,36 +45,6 @@ describe('res', function(){
       .expect(200, done)
     })
 
-    it('should not encode bad "url"', function (done) {
-      var app = express()
-
-      app.use(function (req, res) {
-        // This is here to show a basic check one might do which
-        // would pass but then the location header would still be bad
-        if (url.parse(req.query.q).host !== 'google.com') {
-          res.status(400).end('Bad url');
-        }
-        res.location(req.query.q).end();
-      });
-
-      request(app)
-        .get('/?q=http://google.com\\@apple.com')
-        .expect(200)
-        .expect('Location', 'http://google.com\\@apple.com')
-        .end(function (err) {
-          if (err) {
-            throw err;
-          }
-
-          // This ensures that our protocol check is case insensitive
-          request(app)
-            .get('/?q=HTTP://google.com\\@apple.com')
-            .expect(200)
-            .expect('Location', 'HTTP://google.com\\@apple.com')
-            .end(done)
-        });
-    });
-
     it('should not touch already-encoded sequences in "url"', function (done) {
       var app = express()
 
