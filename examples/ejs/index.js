203a69d9972e2c087569a7e4cafed7caa90389e1
adarshRsinha <93790691+adarshRsinha@users.noreply.github.com>
2024-05-23 02:05:09 +0530

    Issue #5615 using the express instead of http method

--- examples/ejs/index.js
+++ examples/ejs/index.js
@@ -52,6 +52,7 @@ app.get('/', function(req, res){
 
 /* istanbul ignore next */
 if (!module.parent) {
-  app.listen(3000);
-  console.log('Express started on port 3000');
+  app.listen(3000, () => {
+    console.log('Express started on port 3000');
+  });
 }
