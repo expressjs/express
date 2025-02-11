b5628f4906a918f7816a39bbdbe6f34395bd04eb
User <aleksakovacevicc@gmail.com>
2025-02-11 14:03:01 +0100

    more accurate approach for test if callback was executed synchronously

--- test/view.js
+++ test/view.js
@@ -1,99 +1,104 @@
-'use strict'
+'use strict';
 
-var path = require('node:path')
-var assert = require('node:assert')
+const path = require('node:path');
+const assert = require('node:assert');
 const View = require('../lib/view.js');
 
+describe('View.prototype.render', function() {
+  it('should ensure callback is always async', function(done) {
+    const mockEngine = function(filePath, options, callback) {
+      callback(null, 'rendered content');
+    };
 
-describe('View.prototype.render', function () {
-    it('should force callback to be async and pass correct arguments', function (done) {
-      var mockEngine = function (filePath, options, callback) {
-        callback(null, 'rendered content');
-      };
-  
-      var view = new View('test', {
-        root: path.join(__dirname, 'fixtures'),
-        engines: { '.tmpl': mockEngine },
-        defaultEngine: '.tmpl'
-      });
-  
-      var isAsync = false;
-  
-      var originalCallback = function (err, html) {
-        assert(isAsync, 'Callback should be async');
-        assert.strictEqual(err, null, 'Error should be null');
-        assert.strictEqual(html, 'rendered content', 'Rendered content should match');
-        done();
-      };
-  
-      view.render({}, function (err, html) {
-        isAsync = true;
-        originalCallback(err, html);
-      });
+    const view = new View('test', {
+      root: path.join(__dirname, 'fixtures'),
+      engines: { '.tmpl': mockEngine },
+      defaultEngine: '.tmpl'
     });
-  
-    it('should handle errors correctly', function (done) {
-      var mockEngine = function (filePath, options, callback) {
-        callback(new Error('render error'));
-      };
-  
-      var view = new View('test', {
-        root: path.join(__dirname, 'fixtures'),
-        engines: { '.tmpl': mockEngine },
-        defaultEngine: '.tmpl'
-      });
-  
-      view.render({}, function (err, html) {
-        assert(err instanceof Error, 'Error should be an instance of Error');
-        assert.strictEqual(err.message, 'render error', 'Error message should match');
-        assert.strictEqual(html, undefined, 'HTML should be undefined when there is an error');
-        done();
-      });
+
+    let isAsync = false;
+
+    view.render({}, function(err, html) {
+      isAsync = true;
+      assert.strictEqual(err, null);
+      assert.strictEqual(html, 'rendered content');
+      done();
     });
-  
-    it('should handle synchronous callbacks correctly', function (done) {
-      var mockEngine = function (filePath, options, callback) {
-        callback(null, 'sync rendered content');
-      };
-  
-      var view = new View('test', {
-        root: path.join(__dirname, 'fixtures'),
-        engines: { '.tmpl': mockEngine },
-        defaultEngine: '.tmpl'
-      });
-  
-      var isAsync = false;
-  
-      var originalCallback = function (err, html) {
-        assert(isAsync, 'Callback should be async');
-        assert.strictEqual(err, null, 'Error should be null');
-        assert.strictEqual(html, 'sync rendered content', 'Rendered content should match');
-        done();
-      };
-  
-      view.render({}, function (err, html) {
-        isAsync = true;
-        originalCallback(err, html);
-      });
-    });
-  
-    it('should pass correct arguments to the engine', function (done) {
-      var mockEngine = function (filePath, options, callback) {
-        assert.strictEqual(filePath, view.path, 'File path should match');
-        assert.deepStrictEqual(options, { key: 'value' }, 'Options should match');
-        callback(null, 'rendered content');
-      };
-  
-      var view = new View('test', {
-        root: path.join(__dirname, 'fixtures'),
-        engines: { '.tmpl': mockEngine },
-        defaultEngine: '.tmpl'
-      });
-  
-      view.render({ key: 'value' }, function (err, html) {
-        assert.strictEqual(err, null, 'Error should be null');
-        assert.strictEqual(html, 'rendered content', 'Rendered content should match');
-        done();
-      });
-    });
+
+    assert.strictEqual(isAsync, false, 'Callback was executed synchronously!');
   });
+
+  it('should handle errors correctly', function(done) {
+    const mockEngine = function(filePath, options, callback) {
+      callback(new Error('render error'));
+    };
+
+    const view = new View('test', {
+      root: path.join(__dirname, 'fixtures'),
+      engines: { '.tmpl': mockEngine },
+      defaultEngine: '.tmpl'
+    });
+
+    let isAsync = false;
+
+    view.render({}, function(err, html) {
+      isAsync = true;
+      assert(err instanceof Error);
+      assert.strictEqual(err.message, 'render error');
+      assert.strictEqual(html, undefined);
+      done();
+    });
+
+    assert.strictEqual(isAsync, false, 'Error callback was executed synchronously!');
+  });
+
+  it('should handle synchronous engine callbacks correctly', function(done) {
+    const mockEngine = function(filePath, options, callback) {
+      return callback(null, 'sync rendered content');
+    };
+
+    const view = new View('test', {
+      root: path.join(__dirname, 'fixtures'),
+      engines: { '.tmpl': mockEngine },
+      defaultEngine: '.tmpl'
+    });
+
+    let isAsync = false;
+
+    view.render({}, function(err, html) {
+      isAsync = true;
+      assert.strictEqual(err, null);
+      assert.strictEqual(html, 'sync rendered content');
+      done();
+    });
+
+    assert.strictEqual(isAsync, false, 'Callback from synchronous engine was not properly delayed!');
+  });
+
+  it('should pass correct arguments to the engine', function(done) {
+    const expectedOptions = { key: 'value' };
+    
+    const mockEngine = function(filePath, options, callback) {
+      assert.strictEqual(filePath, view.path);
+      assert.deepStrictEqual(options, expectedOptions);
+      callback(null, 'rendered content');
+    };
+
+    const view = new View('test', {
+      root: path.join(__dirname, 'fixtures'),
+      engines: { '.tmpl': mockEngine },
+      defaultEngine: '.tmpl'
+    });
+
+    let isAsync = false;
+
+    view.render(expectedOptions, function(err, html) {
+      isAsync = true;
+      assert.strictEqual(err, null);
+      assert.strictEqual(html, 'rendered content');
+      done();
+    });
+
+    assert.strictEqual(isAsync, false, 'Callback was executed synchronously!');
+  });
+});
