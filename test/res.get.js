ca7ed7857d50d1cc7d1eea9021552edef6593332
Kushan2k <kushangayantha001@gmail.com>
2025-02-18 22:52:37 +0530

    changes

--- test/res.get.js
+++ test/res.get.js
@@ -1,21 +1,42 @@
-'use strict'
+"use strict";
 
-var express = require('..');
-var request = require('supertest');
+var express = require("..");
+// const express = require("express");
+var request = require("supertest");
 
-describe('res', function(){
-  describe('.get(field)', function(){
-    it('should get the response header field', function (done) {
+describe("res", function () {
+  describe(".get(field)", function () {
+    it("should get the response header field", function (done) {
       var app = express();
 
       app.use(function (req, res) {
-        res.setHeader('Content-Type', 'text/x-foo');
-        res.send(res.get('Content-Type'));
+        res.setHeader("Content-Type", "text/x-foo");
+        res.send(res.get("Content-Type"));
       });
 
+      request(app).get("/").expect(200, "text/x-foo", done);
+    });
+  });
+});
+
+describe("res", function () {
+  describe(".get(field)", function () {
+    it("should get the response header field", async function (done) {
+      const app = express();
+
+      // Middleware that sets a header and retrieves it
+      app.use(function (req, res) {
+        res.setHeader("Content-Type", "application/json"); // Set Content-Type header
+        res.setHeader("X-Custom-Header", "my-custom-value"); // Set a custom header
+        res.send(res.get("X-Custom-Header")); // Get and send the value of X-Custom-Header
+      });
+
+      // Test the response
+
       request(app)
-      .get('/')
-      .expect(200, 'text/x-foo', done);
-    })
-  })
-})
+        .get("/") // Make a GET request to the root
+        .expect(200) // Expect a 200 OK status
+        .expect(JSON.parse({ msg: "my-custom-value" }), done); // Expect the header 'X-Custom-Header' to have value 'my-custom-value'
+    });
+  });
+});
--- test/res.links.js
+++ test/res.links.js
@@ -1,47 +1,51 @@
-'use strict'
+"use strict";
 
-var express = require('..');
-var request = require('supertest');
+var express = require("..");
+var request = require("supertest");
 
-describe('res', function(){
-  describe('.links(obj)', function(){
-    it('should set Link header field', function (done) {
+describe("res", function () {
+  describe(".links(obj)", function () {
+    it("should set Link header field", function (done) {
       var app = express();
 
       app.use(function (req, res) {
         res.links({
-          next: 'http://api.example.com/users?page=2',
-          last: 'http://api.example.com/users?page=5'
+          next: "http://api.example.com/users?page=2",
+          last: "http://api.example.com/users?page=5",
         });
         res.end();
       });
 
       request(app)
-      .get('/')
-      .expect('Link', '<http://api.example.com/users?page=2>; rel="next", <http://api.example.com/users?page=5>; rel="last"')
-      .expect(200, done);
-    })
+        .get("/")
+        .expect(
+          "Link",
+          '<http://api.example.com/users?page=2>; rel="next", <http://api.example.com/users?page=5>; rel="last"'
+        )
+        .expect(200, done);
+    });
 
-    it('should set Link header field for multiple calls', function (done) {
+    it("should set Link header field for multiple calls", function (done) {
       var app = express();
-
       app.use(function (req, res) {
         res.links({
-          next: 'http://api.example.com/users?page=2',
-          last: 'http://api.example.com/users?page=5'
+          next: "http://api.example.com/users?page=2",
+          last: "http://api.example.com/users?page=5",
         });
-
         res.links({
-          prev: 'http://api.example.com/users?page=1'
+          prev: "http://api.example.com/users?page=1",
         });
 
         res.end();
       });
 
       request(app)
-      .get('/')
-      .expect('Link', '<http://api.example.com/users?page=2>; rel="next", <http://api.example.com/users?page=5>; rel="last", <http://api.example.com/users?page=1>; rel="prev"')
-      .expect(200, done);
-    })
-  })
-})
+        .get("/")
+        .expect(
+          "Link",
+          '<http://api.example.com/users?page=2>; rel="next", <http://api.example.com/users?page=5>; rel="last", <http://api.example.com/users?page=1>; rel="prev"'
+        )
+        .expect(200, done);
+    });
+  });
+});
