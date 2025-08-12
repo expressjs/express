786fbf417a4096dd25562c3f307c1a84e98d7eae
Sebastian Beltran <bjohansebas@gmail.com>
2025-08-12 17:51:31 -0500

    test: enhance req.is() tests with additional cases
    Signed-off-by: Sebastian Beltran <bjohansebas@gmail.com>

--- test/req.is.js
+++ test/req.is.js
@@ -2,6 +2,7 @@
 
 var express = require('..')
 var request = require('supertest')
+var after = require('after')
 
 describe('req.is()', function () {
   describe('when given a mime type', function () {
@@ -27,10 +28,24 @@ describe('req.is()', function () {
       })
 
       request(app)
-      .post('/')
-      .type('application/json')
-      .send('{}')
-      .expect(200, 'false', done)
+        .post('/')
+        .type('application/json')
+        .send('{}')
+        .expect(200, 'false', done)
+    })
+
+    it('should return false when none in list matches', function (done) {
+      var app = express()
+
+      app.use(function (req, res) {
+        res.json(req.is(['image/jpeg', 'text/html']))
+      })
+
+      request(app)
+        .post('/')
+        .type('application/json')
+        .send('{}')
+        .expect(200, 'false', done)
     })
 
     it('should ignore charset', function (done) {
@@ -41,15 +56,15 @@ describe('req.is()', function () {
       })
 
       request(app)
-      .post('/')
-      .type('application/json; charset=UTF-8')
-      .send('{}')
-      .expect(200, '"application/json"', done)
+        .post('/')
+        .type('application/json; charset=UTF-8')
+        .send('{}')
+        .expect(200, '"application/json"', done)
     })
   })
 
   describe('when content-type is not present', function(){
-    it('should return false', function (done) {
+    it('should return false for single type', function (done) {
       var app = express()
 
       app.use(function (req, res) {
@@ -61,6 +76,19 @@ describe('req.is()', function () {
       .send('{}')
       .expect(200, 'false', done)
     })
+
+    it('should return false for multiple types', function (done) {
+      var app = express()
+
+      app.use(function (req, res) {
+        res.json(req.is(['application/json', 'image/jpeg']))
+      })
+
+      request(app)
+        .post('/')
+        .send('{}')
+        .expect(200, 'false', done)
+    })  
   })
 
   describe('when given an extension', function(){
@@ -76,6 +104,27 @@ describe('req.is()', function () {
       .type('application/json')
       .send('{}')
       .expect(200, '"json"', done)
+    })
+
+  it('should lookup the first matching extension from list', function (done) {
+      var app = express()
+      var cb = after(2, done)
+
+      app.use(function (req, res) {
+        res.json(req.is(['json', 'html']))
+      })
+
+      request(app)
+        .post('/')
+        .type('application/json')
+        .send('{}')
+        .expect(200, '"json"', cb)
+
+      request(app)
+        .post('/')
+        .type('text/html')
+        .send('{}')
+        .expect(200, '"html"', cb)
     })
   })
 
@@ -165,5 +214,32 @@ describe('req.is()', function () {
       .send('{}')
       .expect(200, '"application/json"', done)
     })
+  })
+
+  it('should match wildcards in list and return full type or false', function (done){
+    var app = express()      
+    var cb = after(3, done)
+    
+    app.use(function (req, res) {
+      res.json(req.is(['application/*', '*/jpeg']))
+    })
+
+    request(app)
+      .post('/')
+      .type('image/jpeg')
+      .send('{}')
+      .expect(200, '"image/jpeg"', cb)
+
+    request(app)
+      .post('/')
+      .type('text/html')
+      .send('{}')
+      .expect(200, 'false', cb)
+
+    request(app)
+      .post('/')
+      .type('application/json')
+      .send('{}')
+      .expect(200, '"application/json"', cb)
   })
 })
