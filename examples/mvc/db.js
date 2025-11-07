ec14398c037536034467670b30f0a5035c89009d
Ayush Saha <160033408+skyeyeye@users.noreply.github.com>
2025-11-07 18:46:22 +0530

    Fix syntax error in user definition

--- examples/mvc/db.js
+++ examples/mvc/db.js
@@ -11,6 +11,6 @@ pets.push({ name: 'Raul', id: 3 });
 
 var users = exports.users = [];
 
-users.push({ name: 'TJ', pets: [pets[0], pets[1], pets[2]], id: 0  });
+users.push({ name: 'TJ', pets: [pets[0], pets[1], pets[2]], id: 0 });
 users.push({ name: 'Guillermo', pets: [pets[3]], id: 1 });
 users.push({ name: 'Nathan', pets: [], id: 2 });
