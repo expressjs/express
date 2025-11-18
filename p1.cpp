ea57ac394fe4b727efb027e58291003b1bc248b8
anshiii14 <anshigangar@gmail.com>
2025-11-18 15:45:41 +0000

    pr1

---
+++ p1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // std::cout is used to output data to the console (standard output)
+    std::cout << "Hello, World!" << std::endl;
+    
+    // The program returns 0 to indicate successful execution
+    return 0;
+}
