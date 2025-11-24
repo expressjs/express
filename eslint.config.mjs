11efa392a59fdfe51b58788fbbf1602e55f5599f
dynst <148708712+dynst@users.noreply.github.com>
2025-11-24 00:00:00 +0000

    convert to eslint flat config
    npx @eslint/migrate-config

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import globals from "globals";
+
+export default [{
+  ignores: ["**/coverage", "**/node_modules"],
+}, {
+  languageOptions: {
+    globals: {
+      ...globals.node,
+    },
+  },
+
+  rules: {
+    "eol-last": "error",
+    eqeqeq: ["error", "allow-null"],
+
+    indent: ["error", 2, {
+      MemberExpression: "off",
+      SwitchCase: 1,
+    }],
+
+    "no-trailing-spaces": "error",
+
+    "no-unused-vars": ["error", {
+      vars: "all",
+      args: "none",
+      ignoreRestSiblings: true,
+    }],
+
+    "no-restricted-globals": ["error", {
+      name: "Buffer",
+      message: "Use `import { Buffer } from \"node:buffer\"` instead of the global Buffer.",
+    }],
+  },
+}];
