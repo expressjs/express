44d01e6f7c1fc2f3be35c608f8faef6a17ff8657
dynst <148708712+dynst@users.noreply.github.com>
2025-11-26 00:00:00 +0000

    re-add no-restricted-globals lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,10 @@ export default [
   {
     rules: {
       'global-require': 'warn',
+      'no-restricted-globals': ['error', {
+        name: 'Buffer',
+        message: 'Use `import { Buffer } from "node:buffer"` instead of the global Buffer.',
+      }],
       'n/handle-callback-err': 'off'
     }
   }
