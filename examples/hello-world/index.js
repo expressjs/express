c1f37ff1a9a8f13c6df0fd798b7f558d38d18f11
datlasuryavarma320-hue <datlasuryavarma320@gmail.com>
2026-02-04 18:30:37 +0530

    Update index.js

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -8,7 +8,6 @@ app.get('/', function(req, res){
   res.send('Hello World');
 });
 
-/* istanbul ignore next */
 if (!module.parent) {
   app.listen(3000);
   console.log('Express started on port 3000');
