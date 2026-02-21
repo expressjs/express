4f167f99e8ccaf9c31c1c31790e15d82a88d4835
Doulgas Ma <speedcoder430@gmail.com>
2026-02-21 03:50:55 +0000

    fix(res): preserve original value when mime.contentType() returns false

--- test/res.set.js
+++ test/res.set.js
@@ -87,6 +87,20 @@ describe('res', function(){
       .get('/')
       .expect(500, /TypeError: Content-Type cannot be set to an Array/, done)
     })
+
+    it('should preserve original value when mime.contentType() returns false', function (done) {
+      var app = express()
+
+      app.use(function (req, res) {
+        res.set('Content-Type', 'some-custom-type')
+        res.end()
+      });
+
+      request(app)
+      .get('/')
+      .expect('Content-Type', 'some-custom-type')
+      .expect(200, done)
+    })
   })
 
   describe('.set(object)', function(){
