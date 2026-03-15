f449444fe215c5b0d23b93b7416ffb88e726d229
emilANS <emilbuisness1234567@gmail.com>
2026-03-15 00:27:23 -0500

    Fix issue #5995: cookies encryption added

--- examples/cookies/index.js
+++ examples/cookies/index.js
@@ -1,53 +1,86 @@
-'use strict'
+"use strict";
 
 /**
  * Module dependencies.
  */
 
-var express = require('../../');
-var app = module.exports = express();
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var express = require("../../");
+var app = (module.exports = express());
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var crypto = require("node:crypto");
 
 // custom log format
-if (process.env.NODE_ENV !== 'test') app.use(logger(':method :url'))
+if (process.env.NODE_ENV !== "test") app.use(logger(":method :url"));
 
 // parses request cookies, populating
 // req.cookies and req.signedCookies
 // when the secret is passed, used
 // for signing the cookies.
-app.use(cookieParser('my secret here'));
+app.use(cookieParser("my secret here"));
+
+app.use(express.json());
 
 // parses x-www-form-urlencoded
-app.use(express.urlencoded())
+app.use(express.urlencoded());
 
-app.get('/', function(req, res){
+app.get("/", function (req, res) {
   if (req.cookies.remember) {
-    res.send('Remembered :). Click to <a href="/forget">forget</a>!.');
+    res.send(
+      'Remembered :). Click to <a href="/forget">forget</a>!. <br></br>',
+    );
   } else {
-    res.send('<form method="post"><p>Check to <label>'
-      + '<input type="checkbox" name="remember"/> remember me</label> '
-      + '<input type="submit" value="Submit"/>.</p></form>');
+    res.send(
+      '<form method="post"><p>Check to <label>' +
+        '<input type="checkbox" name="remember"/> remember me</label> ' +
+        '<input type="submit" value="Submit"/>.' +
+        "</p></form>",
+    );
   }
 });
 
-app.get('/forget', function(req, res){
-  res.clearCookie('remember');
-  res.redirect(req.get('Referrer') || '/');
+app.get("/forget", function (req, res) {
+  res.clearCookie("remember");
+  res.redirect(req.get("Referrer") || "/");
 });
 
-app.post('/', function(req, res){
+app.post("/", function (req, res) {
   var minute = 60000;
 
   if (req.body && req.body.remember) {
-    res.cookie('remember', 1, { maxAge: minute })
+    res.cookie("remember", 1, { maxAge: minute });
   }
 
-  res.redirect(req.get('Referrer') || '/');
+  res.redirect(req.get("Referrer") || "/");
+});
+
+app.post("/encryptCookies", function (req, res) {
+  const iv = crypto.randomBytes(16);
+
+  const encryptionAlgorithm = "aes-256-cbc";
+
+  const hashAlgorithm = "sha256";
+
+  res.cookie(
+    "encryptedCookie",
+    "i like to hide my cookies under the sofa",
+    {signed: false},
+    { encryptionAlgorithm, hashAlgorithm, iv },
+  );
+
+  res.send("cookie encrypted");
+});
+
+app.post("/decryptCookies", function (req, res) {
+  const encryptedCookie = req.cookies.encryptedCookie;
+
+  const decryptedCookie = res.decryptCookie(encryptedCookie);
+
+  res.send(decryptedCookie);
 });
 
 /* istanbul ignore next */
 if (!module.parent) {
   app.listen(3000);
-  console.log('Express started on port 3000');
+  console.log("Express started on port 3000");
 }
