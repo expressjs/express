4d83d789eabbb31e2bbf5e143fe47b98af65e67b
notone1246-blip <notone1246@gmail.com>
2026-05-17 02:29:21 +0530

    fix(dead_code): The file defines three response handlers (html, text, json) for a users

--- examples/content-negotiation/users.js
+++ examples/content-negotiation/users.js
@@ -1,19 +1,13 @@
-'use strict'
-
-var users = require('./db');
-
-exports.html = function(req, res){
-  res.send('<ul>' + users.map(function(user){
-    return '<li>' + user.name + '</li>';
-  }).join('') + '</ul>');
-};
-
-exports.text = function(req, res){
-  res.send(users.map(function(user){
-    return ' - ' + user.name + '\n';
-  }).join(''));
-};
-
-exports.json = function(req, res){
-  res.json(users);
-};
+/* If the handlers are needed, add them to the router, e.g.:
+const express = require('express');
+const router = express.Router();
+const usersHandlers = require('./examples/content-negotiation/users');
+router.get('/users', (req, res, next) => {
+  const accept = req.headers.accept || '';
+  if (accept.includes('text/html')) return usersHandlers.html(req, res);
+  if (accept.includes('application/json')) return usersHandlers.json(req, res);
+  return usersHandlers.text(req, res);
+});
+module.exports = router;
+*/
+/* Or, if the file is obsolete, simply remove it. */
