0536019450bdb2e2948a50acb1ec27689ec1d26c
testuzerz123 <testuzerz123@gmail.com>
2026-05-17 16:37:18 +0530

    fix(missing_test): The controller exports an index function that simply redirects the respo

--- examples/mvc/controllers/main/index.js
+++ examples/mvc/controllers/main/index.js
@@ -1,5 +1,36 @@
-'use strict'
+const path = require('path');
+const controllerPath = path.resolve(__dirname, '../../examples/mvc/controllers/main/index.js');
+const mainController = require(controllerPath);
 
-exports.index = function(req, res){
-  res.redirect('/users');
-};
+describe('examples/mvc/controllers/main/index.js', () => {
+  test('index redirects to /users with status 302 by default', () => {
+    const req = {};
+    const res = {
+      redirect: jest.fn()
+    };
+    mainController.index(req, res);
+    expect(res.redirect).toHaveBeenCalledTimes(1);
+    expect(res.redirect).toHaveBeenCalledWith('/users');
+  });
+
+  test('index still calls redirect even if extra properties are present on req/res', () => {
+    const req = { foo: 'bar' };
+    const res = {
+      redirect: jest.fn(),
+      status: jest.fn().mockReturnThis()
+    };
+    mainController.index(req, res);
+    expect(res.redirect).toHaveBeenCalledWith('/users');
+  });
+
+  test('throws when res.redirect is not a function', () => {
+    const req = {};
+    const res = { redirect: null };
+    expect(() => mainController.index(req, res)).toThrow();
+  });
+
+  test('throws when res is undefined', () => {
+    const req = {};
+    expect(() => mainController.index(req, undefined)).toThrow();
+  });
+});
