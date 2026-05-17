00e03af36ff616a0a1f61662fb74de1b0e974255
testuzerz123 <testuzerz123@gmail.com>
2026-05-17 11:24:25 +0530

    fix(doc_gap): The file defines two route handlers that return a list of users in diffe

--- examples/content-negotiation/users.js
+++ examples/content-negotiation/users.js
@@ -1,59 +1,23 @@
-const path = require('path');
-const users = require(path.join(__dirname, '../../examples/content-negotiation/db'));
-const handlers = require(path.join(__dirname, '../../examples/content-negotiation/users'));
+/**
+ * Sends an HTML unordered list of all users.
+ *
+ * @param {Object} req Express request object (unused).
+ * @param {Object} res Express response object used to send the generated HTML.
+ */
+exports.html = function(req, res) {
+  res.send('<ul>' + users.map(function(user) {
+    return '<li>' + user.name + '</li>';
+  }).join('') + '</ul>');
+};
 
-describe('users handlers', () => {
-  const mockRes = () => {
-    const res = {};
-    res.send = jest.fn().mockReturnValue(res);
-    res.json = jest.fn().mockReturnValue(res);
-    return res;
-  };
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  test('html handler returns an unordered list with each user name', () => {
-    const req = {};
-    const res = mockRes();
-    handlers.html(req, res);
-    const expectedHtml = '<ul>' + users.map(u => `<li>${u.name}</li>`).join('') + '</ul>';
-    expect(res.send).toHaveBeenCalledTimes(1);
-    expect(res.send).toHaveBeenCalledWith(expectedHtml);
-  });
-
-  test('text handler returns each user name prefixed with a dash and newline', () => {
-    const req = {};
-    const res = mockRes();
-    handlers.text(req, res);
-    const expectedText = users.map(u => ` - ${u.name}\n`).join('');
-    expect(res.send).toHaveBeenCalledTimes(1);
-    expect(res.send).toHaveBeenCalledWith(expectedText);
-  });
-
-  test('json handler returns the raw users array via res.json', () => {
-    const req = {};
-    const res = mockRes();
-    handlers.json(req, res);
-    expect(res.json).toHaveBeenCalledTimes(1);
-    expect(res.json).toHaveBeenCalledWith(users);
-  });
-
-  test('html handler works with empty users array', () => {
-    const originalUsers = [...users];
-    try {
-      // temporarily replace the module's users reference
-      jest.resetModules();
-      jest.doMock(path.join(__dirname, '../../examples/content-negotiation/db'), () => []);
-      const emptyHandlers = require(path.join(__dirname, '../../examples/content-negotiation/users'));
-      const res = mockRes();
-      emptyHandlers.html({}, res);
-      expect(res.send).toHaveBeenCalledWith('<ul></ul>');
-    } finally {
-      // restore original module cache
-      jest.resetModules();
-      jest.doMock(path.join(__dirname, '../../examples/content-negotiation/db'), () => originalUsers);
-    }
-  });
-});
+/**
+ * Sends a plain‑text list of all users, each prefixed with a dash.
+ *
+ * @param {Object} req Express request object (unused).
+ * @param {Object} res Express response object used to send the generated text.
+ */
+exports.text = function(req, res) {
+  res.send(users.map(function(user) {
+    return ' - ' + user.name + '\n';
+  }).join(''));
+};
