864097de888e746d25b04f62d6bacc9ada4de6f7
testuzerz123 <testuzerz123@gmail.com>
2026-05-17 14:51:25 +0530

    fix(dead_code): The file defines three handler functions (html, text, json) that render

--- examples/content-negotiation/users.js
+++ examples/content-negotiation/users.js
@@ -1,23 +1,3 @@
-/**
- * Sends an HTML unordered list of user names.
- *
- * @param {Object} req - Express request object (unused).
- * @param {Object} res - Express response object used to send the HTML.
- */
-exports.html = function(req, res) {
-  res.send('<ul>' + users.map(function(user) {
-    return '<li>' + user.name + '</li>';
-  }).join('') + '</ul>');
-};
-
-/**
- * Sends a plain‑text list of user names, each prefixed with a dash.
- *
- * @param {Object} req - Express request object (unused).
- * @param {Object} res - Express response object used to send the text.
- */
-exports.text = function(req, res) {
-  res.send(users.map(function(user) {
-    return ' - ' + user.name + '\n';
-  }).join(''));
-};
+// Remove the orphaned file or comment out its exports
+// module.exports = {};
+// Alternatively, delete examples/content-negotiation/users.js if not needed.
