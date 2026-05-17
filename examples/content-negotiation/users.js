aedc726f2ab902dda15f09bf50eafaf0a238b87e
sreedarsan0311 <sreedarsan0311@gmail.com>
2026-05-17 19:00:12 +0530

    fix(doc_gap): The file exports an `html` handler that responds with an HTML unordered

--- examples/content-negotiation/users.js
+++ examples/content-negotiation/users.js
@@ -1,61 +1,12 @@
-const path = require('path');
-const users = require(path.join(__dirname, '../../examples/content-negotiation/db'));
-
-// Mock response object
-function createRes() {
-  return {
-    send: jest.fn(),
-    json: jest.fn()
-  };
-}
-
-describe('content-negotiation users handlers', () => {
-  const handlers = require(path.join(__dirname, '../../examples/content-negotiation/users'));
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  test('html handler returns unordered list with user names', () => {
-    const req = {};
-    const res = createRes();
-    handlers.html(req, res);
-    const expected = '<ul>' + users.map(u => `<li>${u.name}</li>`).join('') + '</ul>';
-    expect(res.send).toHaveBeenCalledTimes(1);
-    expect(res.send).toHaveBeenCalledWith(expected);
-  });
-
-  test('text handler returns bullet list with newline separators', () => {
-    const req = {};
-    const res = createRes();
-    handlers.text(req, res);
-    const expected = users.map(u => ` - ${u.name}\n`).join('');
-    expect(res.send).toHaveBeenCalledTimes(1);
-    expect(res.send).toHaveBeenCalledWith(expected);
-  });
-
-  test('json handler returns the raw users array', () => {
-    const req = {};
-    const res = createRes();
-    handlers.json(req, res);
-    expect(res.json).toHaveBeenCalledTimes(1);
-    expect(res.json).toHaveBeenCalledWith(users);
-  });
-
-  test('html handler works with empty users array', () => {
-    const original = users.slice();
-    try {
-      // temporarily replace users with empty array
-      jest.resetModules();
-      jest.doMock(path.join(__dirname, '../../examples/content-negotiation/db'), () => []);
-      const emptyHandlers = require(path.join(__dirname, '../../examples/content-negotiation/users'));
-      const res = createRes();
-      emptyHandlers.html({}, res);
-      expect(res.send).toHaveBeenCalledWith('<ul></ul>');
-    } finally {
-      // restore original module cache
-      jest.resetModules();
-      jest.doMock(path.join(__dirname, '../../examples/content-negotiation/db'), () => original);
-    }
-  });
-});
+/**
+ * HTTP handler that returns an HTML unordered list of user names.
+ *
+ * @param {import('express').Request} req - The Express request object (unused).
+ * @param {import('express').Response} res - The Express response object used to send the HTML.
+ * @returns {void}
+ */
+exports.html = function(req, res){
+  res.send('<ul>' + users.map(function(user){
+    return '<li>' + user.name + '</li>';
+  }).join('') + '</ul>');
+};
