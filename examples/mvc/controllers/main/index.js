23b661e66a05e92dac83683ee8d4a14e0c9643bd
testuzerz123 <testuzerz123@gmail.com>
2026-05-17 09:11:09 +0530

    fix(missing_test): The controller simply redirects any request to '/users' using Express's

--- examples/mvc/controllers/main/index.js
+++ examples/mvc/controllers/main/index.js
@@ -1,5 +1,31 @@
-'use strict'
+const path = require('path');
+const controller = require(path.resolve(__dirname, '../../examples/mvc/controllers/main/index.js'));
 
-exports.index = function(req, res){
-  res.redirect('/users');
-};
+describe('main controller - index', () => {
+  test('should redirect to /users', () => {
+    const req = {};
+    const res = {
+      redirect: jest.fn()
+    };
+    controller.index(req, res);
+    expect(res.redirect).toHaveBeenCalledTimes(1);
+    expect(res.redirect).toHaveBeenCalledWith('/users');
+  });
+
+  test('should still call redirect even if res has extra properties', () => {
+    const req = {};
+    const res = {
+      status: 200,
+      headersSent: false,
+      redirect: jest.fn()
+    };
+    controller.index(req, res);
+    expect(res.redirect).toHaveBeenCalledWith('/users');
+  });
+
+  test('should not throw when res.redirect is not a function (defensive)', () => {
+    const req = {};
+    const res = {};
+    expect(() => controller.index(req, res)).not.toThrow();
+  });
+});
