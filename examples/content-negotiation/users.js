fc5d8348c3f87d6f71ea3791a3bb6915d9ce42de
testuzerz123 <testuzerz123@gmail.com>
2026-05-17 19:41:15 +0530

    fix(missing_test): The module exports three handlers that format a static list of users fro

--- examples/content-negotiation/users.js
+++ examples/content-negotiation/users.js
@@ -1,19 +1,61 @@
-'use strict'
+const path = require('path');
+const users = require(path.join(__dirname, '../../examples/content-negotiation/db'));
 
-var users = require('./db');
+// Mock response object
+function createRes() {
+  return {
+    send: jest.fn(),
+    json: jest.fn()
+  };
+}
 
-exports.html = function(req, res){
-  res.send('<ul>' + users.map(function(user){
-    return '<li>' + user.name + '</li>';
-  }).join('') + '</ul>');
-};
+describe('content-negotiation users handlers', () => {
+  const handlers = require(path.join(__dirname, '../../examples/content-negotiation/users'));
 
-exports.text = function(req, res){
-  res.send(users.map(function(user){
-    return ' - ' + user.name + '\n';
-  }).join(''));
-};
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-exports.json = function(req, res){
-  res.json(users);
-};
+  test('html handler returns unordered list with user names', () => {
+    const req = {};
+    const res = createRes();
+    handlers.html(req, res);
+    const expected = '<ul>' + users.map(u => `<li>${u.name}</li>`).join('') + '</ul>';
+    expect(res.send).toHaveBeenCalledTimes(1);
+    expect(res.send).toHaveBeenCalledWith(expected);
+  });
+
+  test('text handler returns bullet list with newline separators', () => {
+    const req = {};
+    const res = createRes();
+    handlers.text(req, res);
+    const expected = users.map(u => ` - ${u.name}\n`).join('');
+    expect(res.send).toHaveBeenCalledTimes(1);
+    expect(res.send).toHaveBeenCalledWith(expected);
+  });
+
+  test('json handler returns the raw users array', () => {
+    const req = {};
+    const res = createRes();
+    handlers.json(req, res);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith(users);
+  });
+
+  test('html handler works with empty users array', () => {
+    const original = users.slice();
+    try {
+      // temporarily replace users with empty array
+      jest.resetModules();
+      jest.doMock(path.join(__dirname, '../../examples/content-negotiation/db'), () => []);
+      const emptyHandlers = require(path.join(__dirname, '../../examples/content-negotiation/users'));
+      const res = createRes();
+      emptyHandlers.html({}, res);
+      expect(res.send).toHaveBeenCalledWith('<ul></ul>');
+    } finally {
+      // restore original module cache
+      jest.resetModules();
+      jest.doMock(path.join(__dirname, '../../examples/content-negotiation/db'), () => original);
+    }
+  });
+});
