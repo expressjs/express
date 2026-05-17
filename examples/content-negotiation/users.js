cb8329ccbc601aeb7477c1e6b588b3811fc9c658
testuzerz123 <testuzerz123@gmail.com>
2026-05-17 09:11:06 +0530

    fix(dead_code): The file defines three handler functions (html, text, json) that render

--- examples/content-negotiation/users.js
+++ examples/content-negotiation/users.js
@@ -1,19 +1,8 @@
-'use strict'
-
-var users = require('./db');
-
-exports.html = function(req, res){
-  res.send('<ul>' + users.map(function(user){
-    return '<li>' + user.name + '</li>';
-  }).join('') + '</ul>');
-};
-
-exports.text = function(req, res){
-  res.send(users.map(function(user){
-    return ' - ' + user.name + '\n';
-  }).join(''));
-};
-
-exports.json = function(req, res){
-  res.json(users);
-};
+// In the route definition where content negotiation is set up, e.g., routes/index.js
+const usersHandlers = require('../examples/content-negotiation/users');
+app.get('/users', (req, res) => {
+  const accept = req.headers.accept || '';
+  if (accept.includes('text/html')) return usersHandlers.html(req, res);
+  if (accept.includes('application/json')) return usersHandlers.json(req, res);
+  return usersHandlers.text(req, res);
+});
