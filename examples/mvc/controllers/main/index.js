5f6f8fcc47432d00bdd226a6c96f85a871d74168
testuzerz123 <testuzerz123@gmail.com>
2026-05-17 11:24:24 +0530

    fix(missing_test): The controller's index function simply redirects the response to '/users

--- examples/mvc/controllers/main/index.js
+++ examples/mvc/controllers/main/index.js
@@ -1,5 +1,39 @@
-'use strict'
+const httpMocks = require('node-mocks-http');
 
-exports.index = function(req, res){
-  res.redirect('/users');
-};
+describe('examples/mvc/controllers/main/index.js', () => {
+  let controller;
+
+  beforeAll(() => {
+    controller = require('../../examples/mvc/controllers/main/index');
+  });
+
+  test('should redirect to /users with status 302', () => {
+    const req = httpMocks.createRequest({ method: 'GET', url: '/' });
+    const res = httpMocks.createResponse({ eventEmitter: require('events').EventEmitter });
+
+    controller.index(req, res);
+
+    // node-mocks-http does not automatically end the response, so we manually check
+    expect(res._getStatusCode()).toBe(302);
+    expect(res._getRedirectUrl()).toBe('/users');
+  });
+
+  test('should set Location header to /users', () => {
+    const req = httpMocks.createRequest({ method: 'GET' });
+    const res = httpMocks.createResponse();
+
+    controller.index(req, res);
+
+    expect(res.getHeader('Location')).toBe('/users');
+  });
+
+  test('should not modify request object', () => {
+    const req = httpMocks.createRequest({ method: 'GET', url: '/original' });
+    const originalUrl = req.url;
+    const res = httpMocks.createResponse();
+
+    controller.index(req, res);
+
+    expect(req.url).toBe(originalUrl);
+  });
+});
