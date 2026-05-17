c19b13040777c810e18a9716646c9766ef29d6ac
sreedarsan0311 <sreedarsan0311@gmail.com>
2026-05-17 19:00:12 +0530

    fix(doc_gap): The file defines two route handlers that return a list of users in HTML

--- examples/content-negotiation/users.js
+++ examples/content-negotiation/users.js
@@ -1,12 +1,23 @@
 /**
- * HTTP handler that returns an HTML unordered list of user names.
+ * Sends an HTML unordered list of user names.
  *
- * @param {import('express').Request} req - The Express request object (unused).
- * @param {import('express').Response} res - The Express response object used to send the HTML.
- * @returns {void}
+ * @param {Object} req Express request object (unused).
+ * @param {Object} res Express response object used to send the HTML.
  */
-exports.html = function(req, res){
-  res.send('<ul>' + users.map(function(user){
+exports.html = function(req, res) {
+  res.send('<ul>' + users.map(function(user) {
     return '<li>' + user.name + '</li>';
   }).join('') + '</ul>');
+};
+
+/**
+ * Sends a plain‑text list of user names, each prefixed with a dash.
+ *
+ * @param {Object} req Express request object (unused).
+ * @param {Object} res Express response object used to send the text.
+ */
+exports.text = function(req, res) {
+  res.send(users.map(function(user) {
+    return ' - ' + user.name + '\n';
+  }).join(''));
 };
