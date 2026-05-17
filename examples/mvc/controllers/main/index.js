b7b625024e38dcfe630eb98d0c14822090d55fc2
testuzerz123 <testuzerz123@gmail.com>
2026-05-17 11:05:51 +0530

    fix(missing_test): The controller simply redirects any request to '/users' using Express's

--- examples/mvc/controllers/main/index.js
+++ examples/mvc/controllers/main/index.js
@@ -1,5 +1,27 @@
-'use strict'
+const request = require('supertest');
+const express = require('express');
+const mainController = require('../../examples/mvc/controllers/main/index');
 
-exports.index = function(req, res){
-  res.redirect('/users');
-};
+describe('main controller', () => {
+  let app;
+
+  beforeEach(() => {
+    app = express();
+    // route that uses the controller
+    app.get('/', mainController.index);
+  });
+
+  test('should redirect to /users with default 302 status', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(302);
+    expect(response.headers.location).toBe('/users');
+  });
+
+  test('should allow custom status code if Express default changes', async () => {
+    // Express's res.redirect defaults to 302, but we assert that the status is in the redirect range
+    const response = await request(app).get('/');
+    expect(response.status).toBeGreaterThanOrEqual(300);
+    expect(response.status).toBeLessThan(400);
+    expect(response.headers.location).toBe('/users');
+  });
+});
