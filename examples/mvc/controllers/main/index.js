7a8cf25bbcbaf89dc615d7b7a47315a121a064db
testuzerz123 <testuzerz123@gmail.com>
2026-05-17 15:11:30 +0530

    fix(missing_test): The controller exports an index function that simply redirects the respo

--- examples/mvc/controllers/main/index.js
+++ examples/mvc/controllers/main/index.js
@@ -1,5 +1,27 @@
-'use strict'
+const path = require('path');
+const controllerPath = path.resolve(__dirname, '../../examples/mvc/controllers/main/index.js');
+const mainController = require(controllerPath);
 
-exports.index = function(req, res){
-  res.redirect('/users');
-};
+describe('examples/mvc/controllers/main/index.js', () => {
+  test('index redirects to /users', () => {
+    const req = {};
+    const res = { redirect: jest.fn() };
+    mainController.index(req, res);
+    expect(res.redirect).toHaveBeenCalledTimes(1);
+    expect(res.redirect).toHaveBeenCalledWith('/users');
+  });
+
+  test('index does not modify request object', () => {
+    const req = { foo: 'bar' };
+    const res = { redirect: jest.fn() };
+    const originalReq = { ...req };
+    mainController.index(req, res);
+    expect(req).toEqual(originalReq);
+  });
+
+  test('index handles missing res.redirect gracefully (throws)', () => {
+    const req = {};
+    const res = {};
+    expect(() => mainController.index(req, res)).toThrow();
+  });
+});
