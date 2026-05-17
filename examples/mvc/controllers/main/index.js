2e9269d0392155fceb61db154ee64f5b701b852c
testuzerz123 <testuzerz123@gmail.com>
2026-05-17 12:04:43 +0530

    fix(missing_test): The index controller simply redirects any request to '/users' using Expr

--- examples/mvc/controllers/main/index.js
+++ examples/mvc/controllers/main/index.js
@@ -1,5 +1,32 @@
-'use strict'
+const httpMocks = require('node-mocks-http');
+const controller = require('../../examples/mvc/controllers/main/index');
 
-exports.index = function(req, res){
-  res.redirect('/users');
-};
+describe('main controller - index', () => {
+  test('should redirect to /users', () => {
+    const req = httpMocks.createRequest({ method: 'GET', url: '/' });
+    const res = httpMocks.createResponse();
+    // Spy on redirect
+    const redirectSpy = jest.spyOn(res, 'redirect');
+
+    controller.index(req, res);
+
+    expect(redirectSpy).toHaveBeenCalledTimes(1);
+    expect(redirectSpy).toHaveBeenCalledWith('/users');
+  });
+
+  test('should still redirect even if request object is missing properties', () => {
+    const req = {};
+    const res = httpMocks.createResponse();
+    const redirectSpy = jest.spyOn(res, 'redirect');
+
+    controller.index(req, res);
+
+    expect(redirectSpy).toHaveBeenCalledWith('/users');
+  });
+
+  test('should not throw when response object lacks redirect method', () => {
+    const req = httpMocks.createRequest();
+    const res = {};
+    expect(() => controller.index(req, res)).not.toThrow();
+  });
+});
